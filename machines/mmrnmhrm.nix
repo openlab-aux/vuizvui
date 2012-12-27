b388718fde0f8aa16d1cb3f11464993046848bf6
aszlig <aszlig@redmoonstudios.org>
2012-12-27 14:13:03 +0100

    mmrnmhrm: Change to btrfs-only filesystem layout.
    So, this was a big adventure during the last days, because as you might have
    guessed from my older configuration this was a single-disk system. And that
    single disk failed, so I had to do data recovery instead of actual useful things
    in upstream NixOS. Fortunately I could recover everything, so nothing is lost...
    just a bit hard to find :-D
    
    Anyway, the new filesystem layout is now without LUKS and LVM, as I really want
    to have the flexibility to change striping/raid behaviour per file and possibly
    encryption as well someday.
    
    Well, as you might have noticed from the previous commit: ecryptfs is now built
    into the kernel and is currently my workaround for encryption until btrfs
    finally gets native crypto support.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- machines/mmrnmhrm.nix
+++ machines/mmrnmhrm.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{config, pkgs, ...}:
 
 with pkgs.lib;
 
@@ -27,30 +27,15 @@ with pkgs.lib;
   in rec {
     kernelPackages = pkgs.linuxPackagesFor linuxAszlig kernelPackages;
 
-    initrd = {
-      luks.enable = true;
-      luks.devices = [
-        { name = "system_crypt";
-          device = "/dev/disk/by-uuid/91fbc437-b191-4c32-add4-6f96b2c953dd";
-          preLVM = true;
-        }
-      ];
-    };
-
-    loader.grub.devices = [
-      "/dev/disk/by-id/ata-WDC_WD10EAVS-00D7B1_WD-WCAU48931237"
-    ];
+    loader.grub.devices = singleton
+      "/dev/disk/by-id/ata-WDC_WD6401AALS-00L3B2_WD-WMASY3263730";
   };
 
   networking.hostName = "mmrnmhrm";
 
   fileSystems = {
-    "/boot" = {
-      label = "boot";
-      fsType = "ext2";
-    };
     "/" = {
-      device = "/dev/system/root";
+      label = "root";
       fsType = "btrfs";
       options = concatStringsSep "," [
         "autodefrag"
@@ -63,7 +48,7 @@ with pkgs.lib;
   };
 
   swapDevices = singleton {
-    device = "/dev/system/swap";
+    label = "swap";
   };
 
   services.xserver.videoDrivers = [ "nouveau" ];
