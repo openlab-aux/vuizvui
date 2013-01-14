cb128deec77fe48e8aa2c6321e6de8de418e7022
aszlig <aszlig@redmoonstudios.org>
2013-01-14 07:58:33 +0100

    overrides: Move blop into a subdirectory.
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- overrides/default.nix
+++ overrides/default.nix
@@ -21,20 +21,7 @@ let
       };
     };
 
-    blop = stdenv.mkDerivation rec {
-      name = "blop-${version}";
-      version = "0.2.8";
-
-      configureFlags = [
-        "--with-ladspa-prefix=${ladspaH}"
-        "--with-ladspa-plugin-dir=$(out)/lib/ladspa"
-      ];
-
-      src = fetchurl {
-        url = "mirror://sourceforge/blop/${name}.tar.gz";
-        sha256 = "02iymw84dml8glyqgx1mxq4fz2fifgi1jca28hx2r3a2mi7i71vy";
-      };
-    };
+    blop = callPackage ./blop { };
 
     libCMT = callPackage ./libcmt { };
 
