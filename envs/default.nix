5cd07981d83f080775d6f144a940e05a4cc7310c
aszlig <aszlig@redmoonstudios.org>
2013-01-14 06:51:45 +0100

    overrides: Move kernel/hetzner/rdwarf to ../envs.
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- envs/default.nix
+++ envs/default.nix
@@ -14,4 +14,32 @@ with pkgs;
       collection = import ../collections/aszlig.nix pkgs;
     in lib.mapAttrsToList genAszligEnv collection;
   };
+
+  kernelEnv = myEnvFun {
+    name = "kernel";
+    extraCmds = ''
+      export NIX_LDFLAGS="$NIX_LDFLAGS -lncurses"
+    '';
+    buildInputs = [
+      stdenv ncurses
+    ];
+  };
+
+  hetznerEnv = myEnvFun {
+    name = "hetzner";
+    buildInputs = [
+      stdenv python
+      pythonPackages.pexpect
+    ];
+  };
+
+  rdwarfEnv = myEnvFun {
+    name = "rdwarf";
+    buildInputs = [
+      stdenv python
+      pythonPackages.numpy
+      pythonPackages.pyaudio
+      pythonPackages.curses
+    ];
+  };
 }
--- overrides/default.nix
+++ overrides/default.nix
@@ -84,16 +84,6 @@ rec {
     checksumType = "mhash";
   };
 
-  kernelEnv = myEnvFun {
-    name = "kernel";
-    extraCmds = ''
-      export NIX_LDFLAGS="$NIX_LDFLAGS -lncurses"
-    '';
-    buildInputs = [
-      stdenv ncurses
-    ];
-  };
-
   testChromiumBuild = let
     buildChannels = [ "stable" "beta" "dev" ];
     buildChromium = chan: chromium.override {
@@ -128,24 +118,6 @@ rec {
       pythonPackages.markdown
       pythonPackages.MySQL_python
       pythonPackages.setuptools
-    ];
-  };
-
-  hetznerEnv = myEnvFun {
-    name = "hetzner";
-    buildInputs = [
-      stdenv python
-      pythonPackages.pexpect
-    ];
-  };
-
-  rdwarfEnv = myEnvFun {
-    name = "rdwarf";
-    buildInputs = [
-      stdenv python
-      pythonPackages.numpy
-      pythonPackages.pyaudio
-      pythonPackages.curses
     ];
   };
 
