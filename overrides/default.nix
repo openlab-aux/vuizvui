5fe9d817e156cf4cc3367133ee06eebafaf43f46
aszlig <aszlig@redmoonstudios.org>
2013-10-29 08:00:12 +0100

    overrides: Switch to Torvalds's Linux repository.
    This is instead of my local clone, which I used because I don't want to
    clone that repos over and over again (even if it's a shallow clone) for
    (re-)building the kernel.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- overrides/default.nix
+++ overrides/default.nix
@@ -88,7 +88,7 @@ let
     kernelSourceAszlig = {
       version = "3.12.0-rc7";
       src = everything.fetchgit {
-        url = /home/aszlig/linux;
+        url = git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git;
         rev = "c9ca72fc568403db192e199b752c9c253e5f5fd9";
         sha256 = "1mv6srfa0n8xi6dfxvmbhjwibqqv1r5kr6wlr93mph7a26m6g9dj";
       };
