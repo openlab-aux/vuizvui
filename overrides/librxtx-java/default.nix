c390a01230935eb3f0ef66b3c2871c34dcf17663
aszlig <aszlig@redmoonstudios.org>
2014-03-17 04:35:48 +0100

    librxtx-java: Provide alternate URL for download.
    The web server at rxtx.qbang.org is down at the moment.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- overrides/librxtx-java/default.nix
+++ overrides/librxtx-java/default.nix
@@ -4,6 +4,14 @@ stdenv.mkDerivation rec {
   name = "rxtx-${version}";
   version = "2.2pre2";
 
+  src = fetchurl {
+    urls = [
+      "http://rxtx.qbang.org/pub/rxtx/${name}.zip"
+      "ftp://ftp.freebsd.org/pub/FreeBSD/ports/distfiles/${name}.zip"
+    ];
+    sha256 = "00sv9604hkq81mshih0fhqfzn4mf01d6rish6vplsi0gfqz3fc1w";
+  };
+
   buildInputs = [ unzip jdk lockdev ];
 
   NIX_CFLAGS_COMPILE = "-DUTS_RELEASE=\"3.8.0\"";
@@ -18,9 +26,4 @@ stdenv.mkDerivation rec {
   preInstall = ''
     mkdir -p "$out/lib/java"
   '';
-
-  src = fetchurl {
-    url = "http://rxtx.qbang.org/pub/rxtx/${name}.zip";
-    sha256 = "00sv9604hkq81mshih0fhqfzn4mf01d6rish6vplsi0gfqz3fc1w";
-  };
 }
