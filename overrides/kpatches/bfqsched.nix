4a470de629d5bf3088b9b62d688d43b8a400f1b1
aszlig <aszlig@redmoonstudios.org>
2014-05-01 23:36:28 +0200

    bfqsched: Update to v7r3.
    This also rebases the patch against latest 3.15-rc3.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- overrides/kpatches/bfqsched.nix
+++ overrides/kpatches/bfqsched.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl }:
 
 let
-  version = "3.14.0-v7r2";
+  version = "3.14.0-v7r3";
 
   baseURL = "http://algo.ing.unimo.it/people/paolo/disk_sched/patches";
 
@@ -12,16 +12,16 @@ let
 
   allPatches = [
     (fetchPatch {
-      name = "0001-block-cgroups-kconfig-build-bits-for-BFQ-v7r2-3.14";
-      sha256 = "04vjygsg4hp81i0f78jkv51s4m7w6k3qxhrpjrasrkgyi3wxh15l";
+      name = "0001-block-cgroups-kconfig-build-bits-for-BFQ-v7r3-3.14";
+      sha256 = "1md84ld7lsvxwmzyd9r5znd66vgdljxvqwj47fzy2idsqyn6gjwi";
     })
     (fetchPatch {
-      name = "0002-block-introduce-the-BFQ-v7r2-I-O-sched-for-3.14";
-      sha256 = "1bz6rzg2fakfhh0grpr7z0nl85bkibl6lcw4wgmh34f3kvq89gxs";
+      name = "0002-block-introduce-the-BFQ-v7r3-I-O-sched-for-3.14";
+      sha256 = "1vavp37gwwlr1zxha2wl9yc1rhllcdb4yyahxkr1iwajwrfm3xwi";
     })
     (fetchPatch {
-      name = "0003-block-bfq-add-Early-Queue-Merge-EQM-to-BFQ-v7r2-for-3.14.0";
-      sha256 = "0dsj6hvk0dh1f3p88zyqm31sj0hczjm8k9bmsqvhn38z0c88qi8v";
+      name = "0003-block-bfq-add-Early-Queue-Merge-EQM-to-BFQ-v7r3-for-3.14.0";
+      sha256 = "1bpparpn3rglrdcwgs6hn8ggwhngrx1cy9rqn2mg9jwdz22lrlhv";
     })
     ./bfqsched-3.15.patch
   ];
