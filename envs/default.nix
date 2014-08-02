c842ed7d1c75a456cdb40066fd3d758f0a776f07
aszlig <aszlig@redmoonstudios.org>
2014-08-02 18:55:31 +0200

    envs: Drop all envs except aszligEnv.
    As written in 886cf96, we don't need them anymore.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- envs/default.nix
+++ envs/default.nix
@@ -15,45 +15,4 @@ with pkgs;
     in lib.mapAttrsToList genAszligEnv collection;
     ignoreCollisions = true;
   };
-
-  catapultEnv = myEnvFun {
-    name = "catapult";
-    buildInputs = [
-      stdenv python pil
-      pythonPackages.matplotlib
-      pythonPackages.django
-      pythonPackages.sqlite3
-      pythonPackages.markdown
-      pythonPackages.MySQL_python
-      pythonPackages.setuptools
-    ];
-  };
-
-  kernelEnv = myEnvFun {
-    name = "kernel";
-    extraCmds = ''
-      export NIX_LDFLAGS="$NIX_LDFLAGS -lncurses"
-    '';
-    buildInputs = [
-      stdenv ncurses
-    ];
-  };
-
-  hetznerEnv = myEnvFun {
-    name = "hetzner";
-    buildInputs = [
-      stdenv python
-      pythonPackages.pexpect
-    ];
-  };
-
-  rdwarfEnv = myEnvFun {
-    name = "rdwarf";
-    buildInputs = [
-      stdenv python
-      pythonPackages.numpy
-      pythonPackages.pyaudio
-      pythonPackages.curses
-    ];
-  };
 }
