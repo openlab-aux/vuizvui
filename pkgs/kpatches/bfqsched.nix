e00ed7a14ef490d7791f0d04e8f1d6d558cc0601
aszlig <aszlig@redmoonstudios.org>
2014-08-25 09:46:40 +0200

    bfqsched: Update patch for kernel version 3.17.
    Includes a patch for 3.17-rc1 from:
    
    https://groups.google.com/forum/#!topic/bfq-iosched/piEzoNMGmk8
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/kpatches/bfqsched.nix
+++ pkgs/kpatches/bfqsched.nix
@@ -2,7 +2,7 @@
 
 let
   bfqVersion = "v7r5";
-  kernelVersion = "3.15";
+  kernelVersion = "3.16";
   version = "${kernelVersion}.0-${bfqVersion}";
 
   baseURL = "http://algo.ing.unimo.it/people/paolo/disk_sched/patches";
@@ -16,19 +16,19 @@ let
     (fetchPatch {
       name = "0001-block-cgroups-kconfig-build-bits-for-BFQ-"
            + "${bfqVersion}-${kernelVersion}";
-      sha256 = "16vrdg9ky7q89sb7gp8as7qslqg1zy1n28cnyshm91qnf49wfh7g";
+      sha256 = "1f93n0hy9vmxpfmkdkss4b711mxly23r1z6n832prq3qph9xzkwm";
     })
     (fetchPatch {
       name = "0002-block-introduce-the-BFQ-"
            + "${bfqVersion}-I-O-sched-for-${kernelVersion}";
-      sha256 = "1gf4y5i9prhg9ls2g8apaiygm538khhpp35dahgv59hr3sfwsghs";
+      sha256 = "1b5dwivas7j56v47pq2wyy4dlvmk4f9y051ja9lila6rqfd4wns4";
     })
     (fetchPatch {
       name = "0003-block-bfq-add-Early-Queue-Merge-EQM-to-BFQ-"
            + "${bfqVersion}-for-${kernelVersion}.0";
-      sha256 = "1q3fn6gl3lhvbqfhaarpv399id9sa32zx6ygqx4x98zmixwrsn9z";
+      sha256 = "0rjcmjigqkfplnsh0mdrjf6w9ng02yjwyql6k5kk39wf1q6vf7ch";
     })
-    ./bfqsched.patch
+    ./bfqsched-3.17.patch
   ];
 
   patch = stdenv.mkDerivation {
