openlab-aux/vuizvui

commit d12dc1d671c12a5154d145b467d7de931e416238

dnyarri, mmrnmhrm: Give RT priority to Synergy.
I can't stand waiting for machine switches just because mmrnmhrm is
currently trying to fart a lot on my nixpart tests.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

machines/dnyarri.nix
@@ -131,6 +131,9 @@ with import ../lib;
   services.synergy.client.enable = true;
   services.synergy.client.serverAddress = "mmrnmhrm";
 
+  systemd.services."synergy-client".CPUSchedulingPolicy = "rr";
+  systemd.services."synergy-client".CPUSchedulingPriority = 50;
+
   services.xserver.videoDrivers = [ "ati" ];
   services.xserver.xrandrHeads = [ "HDMI-0" "DVI-0" ];
 

machines/mmrnmhrm.nix
@@ -49,6 +49,9 @@ with import ../lib;
   services.synergy.server.enable = true;
   services.synergy.server.configFile = ../cfgfiles/synergy.conf;
 
+  systemd.services."synergy-server".CPUSchedulingPolicy = "rr";
+  systemd.services."synergy-server".CPUSchedulingPriority = 50;
+
   services.xserver.videoDrivers = [ "nouveau" ];
   services.xserver.xrandrHeads = [ "DVI-I-1" "VGA-1" ];
 

commit 47c58eb3ef65500f20106306fe9f47a12c18ddc1

Fix evaluation for dnyarri and mmrnmhrm.
Regression is from d12dc1d671c12a5154d145b467d7de931e416238.

I know who - besides of mmrnmhrm - is also farting a lot: My brain.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

machines/dnyarri.nix
@@ -131,8 +131,8 @@ with import ../lib;
   services.synergy.client.enable = true;
   services.synergy.client.serverAddress = "mmrnmhrm";
 
-  systemd.services."synergy-client".CPUSchedulingPolicy = "rr";
-  systemd.services."synergy-client".CPUSchedulingPriority = 50;
+  systemd.services."synergy-client".serviceConfig.CPUSchedulingPolicy = "rr";
+  systemd.services."synergy-client".serviceConfig.CPUSchedulingPriority = 50;
 
   services.xserver.videoDrivers = [ "ati" ];
   services.xserver.xrandrHeads = [ "HDMI-0" "DVI-0" ];

machines/mmrnmhrm.nix
@@ -49,8 +49,8 @@ with import ../lib;
   services.synergy.server.enable = true;
   services.synergy.server.configFile = ../cfgfiles/synergy.conf;
 
-  systemd.services."synergy-server".CPUSchedulingPolicy = "rr";
-  systemd.services."synergy-server".CPUSchedulingPriority = 50;
+  systemd.services."synergy-server".serviceConfig.CPUSchedulingPolicy = "rr";
+  systemd.services."synergy-server".serviceConfig.CPUSchedulingPriority = 50;
 
   services.xserver.videoDrivers = [ "nouveau" ];
   services.xserver.xrandrHeads = [ "DVI-I-1" "VGA-1" ];