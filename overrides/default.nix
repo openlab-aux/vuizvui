d5ca8a15519c4e93237c325aef0280cd75764389
aszlig <aszlig@redmoonstudios.org>
2014-10-29 12:17:31 +0100

    overrides/nixops: Default to read-write-mode.
    Added by another patch, which I'm not sure is worthwhile to include
    upstream.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- overrides/default.nix
+++ overrides/default.nix
@@ -97,10 +97,18 @@ let
       build = getAttr o.stdenv.system release.build;
     in with everything; build.drvAttrs // {
       name = "nixops-1.3git";
-      patches = (build.drvAttrs.patches or []) ++ singleton (fetchpatch {
-        url = "https://github.com/NixOS/nixops/pull/201.diff";
-        sha256 = "1i5yycqayxggg3l1i6wk8lp64lqlxw5nmfya9fcrgmck8ls0rxid";
-      });
+      patches = (build.drvAttrs.patches or []) ++ [
+        (fetchpatch {
+          url = "https://github.com/NixOS/nixops/pull/201.diff";
+          sha256 = "1i5yycqayxggg3l1i6wk8lp64lqlxw5nmfya9fcrgmck8ls0rxid";
+        })
+        (fetchpatch rec {
+          name = "read-write-by-default.diff";
+          url = "https://github.com/aszlig/nixops/compare/"
+              + "NixOS:master...aszlig:${name}";
+          sha256 = "0a1jcqrqfi7dfvlha5r0609bzvin7p7nj523xxcrvwpgp6ag0zsa";
+        })
+      ];
       patchFlags = "--merge -p1";
     };
   };
