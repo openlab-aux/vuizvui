79022512006b349677f3b84eba1c6c3c5fa6f64a
devhell <^@regexmail.net>
2015-02-08 17:26:15 +0000

    services_{eris,titan,skunkworks}: Fix UTF8 fonts, and cleanup

--- services/services_eris.nix
+++ services/services_eris.nix
@@ -67,29 +67,30 @@
     ${pkgs.networkmanagerapplet}/bin/nm-applet &
     ${pkgs.pasystray}/bin/pasystray &
     ${pkgs.xorg.xrdb}/bin/xrdb "${pkgs.writeText "xrdb.conf" ''
-       Xft.dpi:              96
-       Xft.antialias:        true
-       Xft.hinting:          full
-       Xft.hintstyle:        hintslight
-       Xft.rgba:             rgb
-       Xft.lcdfilter:        lcddefault
-       Xft.autohint:         1
-       XTerm.termName:       xterm-256color
-       XTerm*bellIsUrgent:   true
-       XTerm*utf8:           1
-       XTerm*locale:         true
-       XTerm*utf8Title:      true
-       XTerm*utf8Fonts:      1
-       XTerm*utf8Latin1:     true
-       XTerm*dynamicColors:  true
-       XTerm*eightBitInput:  true
-       Xcursor.theme:        Vanilla-DMZ-AA
-       Xcursor.size:         22
-       *.charClass:33:48,35:48,37:48,43:48,45-47:48,61:48,63:48,64:48,95:48,126:48,35:48,58:48
-       XTerm*faceName:       xft:DejaVu Sans Mono for Powerline:pixelsize=11:antialia=true:hinting=true
-       XTerm*cursorColor:    #545f65
-       *background:          #121212
-       *foreground:          #babdb6
+      Xft.dpi:              96
+      Xft.antialias:        true
+      Xft.hinting:          full
+      Xft.hintstyle:        hintslight
+      Xft.rgba:             rgb
+      Xft.lcdfilter:        lcddefault
+      Xft.autohint:         1
+      XTerm.termName:       xterm-256color
+      XTerm*bellIsUrgent:   true
+      XTerm*utf8:           1
+      XTerm*locale:         true
+      XTerm*utf8Title:      true
+      XTerm*utf8Fonts:      1
+      XTerm*utf8Latin1:     true
+      XTerm*dynamicColors:  true
+      XTerm*eightBitInput:  true
+      Xcursor.theme:        Vanilla-DMZ-AA
+      Xcursor.size:         22
+      *.charClass:33:48,35:48,37:48,43:48,45-47:48,61:48,63:48,64:48,95:48,126:48,35:48,58:48
+      XTerm*faceName:       xft:DejaVu Sans Mono for Powerline:pixelsize=11:antialias=true:hinting=true
+      XTerm*faceNameDoublesize: xft:Unifont:pixelsize=12:antialias=true:hinting=true
+      XTerm*cursorColor:    #545f65
+      *background:          #121212
+      *foreground:          #babdb6
     ''}"
   '';
 }
--- services/services_skunkworks.nix
+++ services/services_skunkworks.nix
@@ -76,29 +76,30 @@
     displayManager.sessionCommands = ''
       ${pkgs.xorg.xsetroot}/bin/xsetroot -solid black
       ${pkgs.xorg.xrdb}/bin/xrdb "${pkgs.writeText "xrdb.conf" ''
-         Xft.dpi:              96
-         Xft.antialias:        true
-         Xft.hinting:          full
-         Xft.hintstyle:        hintslight
-         Xft.rgba:             rgb
-         Xft.lcdfilter:        lcddefault
-         Xft.autohint:         1
-         XTerm.termName:       xterm-256color
-         XTerm*bellIsUrgent:   true
-         XTerm*utf8:           1
-         XTerm*locale:         true
-         XTerm*utf8Title:      true
-         XTerm*utf8Fonts:      true
-         XTerm*utf8Latin1:     true
-         XTerm*dynamicColors:  true
-         XTerm*eightBitInput:  true
-         Xcursor.theme:        Vanilla-DMZ-AA
-         Xcursor.size:         22
-         *.charClass:33:48,35:48,37:48,43:48,45-47:48,61:48,63:48,64:48,95:48,126:48,35:48,58:48
-         XTerm*faceName:       xft:DejaVu Sans Mono for Powerline:pixelsize=12:antialias=true:hinting=true
-         XTerm*cursorColor:    #545f65
-         *background:          #121212
-         *foreground:          #babdb6
+        Xft.dpi:              96
+        Xft.antialias:        true
+        Xft.hinting:          full
+        Xft.hintstyle:        hintslight
+        Xft.rgba:             rgb
+        Xft.lcdfilter:        lcddefault
+        Xft.autohint:         1
+        XTerm.termName:       xterm-256color
+        XTerm*bellIsUrgent:   true
+        XTerm*utf8:           1
+        XTerm*locale:         true
+        XTerm*utf8Title:      true
+        XTerm*utf8Fonts:      true
+        XTerm*utf8Latin1:     true
+        XTerm*dynamicColors:  true
+        XTerm*eightBitInput:  true
+        Xcursor.theme:        Vanilla-DMZ-AA
+        Xcursor.size:         22
+        *.charClass:33:48,35:48,37:48,43:48,45-47:48,61:48,63:48,64:48,95:48,126:48,35:48,58:48
+        XTerm*faceName:       xft:DejaVu Sans Mono for Powerline:pixelsize=12:antialias=true:hinting=true
+        XTerm*faceNameDoublesize: xft:Unifont:pixelsize=12:antialias=true:hinting=true
+        XTerm*cursorColor:    #545f65
+        *background:          #121212
+        *foreground:          #babdb6
       ''}"
 
       DISPLAY=:0.1 ${pkgs.windowmaker}/bin/wmaker & 
