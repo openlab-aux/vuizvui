142c5e98def1896077a412e60df2f9e7b0312623
devhell <^@regexmail.net>
2015-02-15 23:51:51 +0000

    pkgs_{eris,titan}: Add reaverwps

--- pkgs/pkgs_eris.nix
+++ pkgs/pkgs_eris.nix
@@ -6,6 +6,7 @@
     kismet
     minicom
     networkmanagerapplet
+    reaverwps
     snort
   ];
 }
--- pkgs/pkgs_titan.nix
+++ pkgs/pkgs_titan.nix
@@ -11,6 +11,7 @@
     kismet
     minicom
     networkmanagerapplet
+    reaverwps
     snort
   ];
 }
