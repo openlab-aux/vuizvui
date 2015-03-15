a1468c63c510c656cd4f3af0ea75b830cab45a66
devhell <^@regexmail.net>
2015-03-15 11:52:40 +0000

    Restructure Phase 2: skunkworks
    This is an attempt at restructuring nixosconf. The goal is to abstract
    all configurations down to one "entry-point" file per machine. In this
    phase all of `hardware-configuration.nix` is refactored and included
    into the machine specific .nix file.
    
    This commit concludes Phase 2 for the machine `skunkworks`.

--- machines/machine_skunkworks.nix
+++ machines/machine_skunkworks.nix
@@ -3,14 +3,43 @@
 {
   imports = [ ./machine_common.nix ];
 
-  boot.loader.grub = {
-    enable = true;
-    version = 2;
-    devices = [
-      "/dev/disk/by-id/ata-ST31500541AS_6XW0NK21"
-      "/dev/disk/by-id/ata-ST31500541AS_6XW0P0CW"
+  boot = {
+    loader.grub = {
+      enable = true;
+      version = 2;
+      devices = [
+        "/dev/disk/by-id/ata-ST31500541AS_6XW0NK21"
+        "/dev/disk/by-id/ata-ST31500541AS_6XW0P0CW"
+      ];
+    };
+
+    initrd = {
+      availableKernelModules = [ "ehci_pci" "ahci" "firewire_ohci" "usbhid" "usb_storage" ];
+      kernelModules = [ "fuse" ];
+    };
+
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+    blacklistedKernelModules = [ "pcspkir" ];
+  };
+
+  fileSystems."/" = {
+    label = "nixos";
+    fsType = "btrfs";
+    options = pkgs.lib.concatStringsSep "," [
+      "autodefrag"
+      "space_cache"
+      "compress=lzo"
+      "noatime"
     ];
   };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/16bd9abd-6af5-4a24-8ea5-58adc51e9641"; }
+    { device = "/dev/disk/by-uuid/279708cb-f9c3-4a37-a064-80ff85a66f88"; }
+  ];
+
+  nix.maxJobs = 8;
 
   i18n = {
     consoleFont = "lat9w16";
