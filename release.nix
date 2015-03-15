8c972eeab3a96d7296930b60a3479fc4de358ae4
devhell <^@regexmail.net>
2015-03-15 13:53:53 +0000

    release.nix: Include titan for hydra to build

--- release.nix
+++ release.nix
@@ -1,4 +1,5 @@
 {
   skunkworks = (import <nixpkgs/nixos> { configuration = ./entry-skunkworks.nix; }).system;
+  titan = (import <nixpkgs/nixos> { configuration = ./entry-titan.nix; }).system;
   eris = (import <nixpkgs/nixos> { configuration = ./entry-eris.nix; }).system;
 }
