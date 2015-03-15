71965394a8f3996c4d010e0e18a9d95e3dbc2865
devhell <^@regexmail.net>
2015-03-15 13:42:45 +0000

    Restructure Phase 2: titan
    This is an attempt at restructuring nixosconf. The goal is to abstract
    all configurations down to one "entry-point" file per machine. In this
    phase all of `hardware-configuration.nix` is refactored and included
    into the machine specific .nix file.
    
    This commit concludes Phase 2 for the machine `titan`.

--- machines/machine_titan.nix
+++ machines/machine_titan.nix
@@ -3,10 +3,44 @@
 {
   imports = [ ./machine_common.nix ];
 
-  boot.loader.gummiboot.enable = true;
-  boot.loader.gummiboot.timeout = 2;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelParams = [ "elevator=noop" ];
+  boot = {
+    loader = {
+      gummiboot = {
+        enable = true;
+        timeout = 2;
+      };
+
+      efi.canTouchEfiVariables = true;
+    };
+
+    initrd = {
+      availableKernelModules = [ "xhci_hcd" "ehci_pci" "ahci" "usb_storage" ];
+      kernelModules = [ "fuse" ];
+    };
+
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+
+    kernelParams = [ "elevator=noop" ];
+  };
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/09d1155f-e7dd-4754-ae01-44da2517d5f0";
+    fsType = "btrfs";
+    options = pkgs.lib.concatStringsSep "," [
+      "autodefrag"
+      "space_cache"
+      "compress=lzo"
+      "noatime"
+      "ssd"
+    ];
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/fecde631-8661-4a0e-88e6-5ce5b551847a"; }
+  ];
+
+  nix.maxJobs = 4;
 
   i18n = {
     consoleFont = "lat9w-16";
