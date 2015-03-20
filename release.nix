098934bb6ba3a7d02588409b31f0aa01a0826ee8
devhell <^@regexmail.net>
2015-03-20 17:04:09 +0000

    release.nix: We don't need to build for i686

--- release.nix
+++ release.nix
@@ -1,3 +1,9 @@
+let
+  supportedSystems = [ "x86_64-linux" ];
+  system = "x86_64-linux";
+
+in
+
 {
   skunkworks = (import <nixpkgs/nixos> { configuration = ./entry-skunkworks.nix; }).system;
   titan = (import <nixpkgs/nixos> { configuration = ./entry-titan.nix; }).system;
