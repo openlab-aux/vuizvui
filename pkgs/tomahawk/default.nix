fb8d28f8766046a8a1792305f42d7c8e73f61bae
aszlig <aszlig@redmoonstudios.org>
2015-03-31 14:57:39 +0200

    tomahawk: Remove dependency an qjson.
    It is included in Qt5 already and the separate one also won't build with
    newer Qt, so let's null it out for echonest and remove it from
    Tomahawk's buildInputs.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/tomahawk/default.nix
+++ pkgs/tomahawk/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchFromGitHub, cmake, pkgconfig, attica, boost, gnutls, libechonest
-, liblastfm, lucenepp, vlc_qt5, qca2, qjson, qt54, qtkeychain, quazip
+, liblastfm, lucenepp, vlc_qt5, qca2, qt54, qtkeychain, quazip
 , kf5_latest, sparsehash, taglib, websocketpp, makeWrapper, ffmpeg_2, v4l_utils
 
 , enableXMPP      ? true,  libjreen     ? null
@@ -19,7 +19,7 @@ let
   });
 
   libechonestQT5 = overrideDerivation ((useQT5 libechonest).override {
-    qjson = useQT5 qjson;
+    qjson = null;
   }) (drv: {
     cmakeFlags = (drv.cmakeFlags or []) ++ [ "-DBUILD_WITH_QT4=OFF" ];
   });
@@ -46,7 +46,7 @@ in stdenv.mkDerivation rec {
     "-DLUCENEPP_LIBRARY_DIR=${lucenepp}/lib"
   ];
 
-  buildInputs = (map useQT5 [ qca2 liblastfm quazip qtkeychain qjson ]) ++ [
+  buildInputs = (map useQT5 [ qca2 liblastfm quazip qtkeychain ]) ++ [
     libechonestQT5 kf5_latest.attica cmake pkgconfig boost gnutls lucenepp
     vlc qt54.base sparsehash taglib websocketpp makeWrapper
   ] ++ stdenv.lib.optional enableXMPP      (useQT5 libjreen)
