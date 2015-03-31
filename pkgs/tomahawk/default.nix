c768bac8ecfa3a151226203f79b53ab77ebb0850
aszlig <aszlig@redmoonstudios.org>
2015-03-31 16:13:40 +0200

    tomahawk: Override QCA sources with Qt5 fork.
    Thanks to @muesli for mentioning this, so we no longer need to patch
    Tomahawk's CMakeList.txt.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/tomahawk/default.nix
+++ pkgs/tomahawk/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, fetchFromGitHub, cmake, pkgconfig, attica, boost, gnutls, libechonest
-, liblastfm, lucenepp, vlc_qt5, qca2, qt54, qtkeychain, quazip
+{ stdenv, fetchFromGitHub, fetchurl, cmake, pkgconfig, attica, boost, gnutls
+, libechonest, liblastfm, lucenepp, vlc_qt5, qca2, qt54, qtkeychain, quazip
 , kf5_latest, sparsehash, taglib, websocketpp, makeWrapper, ffmpeg_2, v4l_utils
 
 , enableXMPP      ? true,  libjreen     ? null
@@ -16,6 +16,14 @@ with stdenv.lib;
 let
   useQT5 = pkg: pkg.override (attrs: {
     ${if attrs ? qt4 then "qt4" else "qt"} = qt54.base;
+  });
+
+  qcaQT5 = overrideDerivation (useQT5 qca2) (drv: rec {
+    name = "qca-qt5-2.1.0.3";
+    src = fetchurl {
+      url = "mirror://kde/stable/qca-qt5/2.1.0.3/src/${name}.tar.xz";
+      sha256 = "1yc9s88q6gzb1jjd34f9h28dnhjb1dnx6wlasf47by4gd9lp00db";
+    };
   });
 
   libechonestQT5 = overrideDerivation ((useQT5 libechonest).override {
@@ -48,10 +56,6 @@ in stdenv.mkDerivation rec {
     sha256 = "07gpf885hxv5xmv3acixm0rhinqilygrw7542nj7s68iigd9sfqd";
   };
 
-  postPatch = ''
-    sed -i -e 's/Qca-qt5/Qca/' CMakeLists.txt
-  '';
-
   cmakeFlags = [
     "-DLUCENEPP_INCLUDE_DIR=${lucenepp}/include"
     "-DLUCENEPP_LIBRARY_DIR=${lucenepp}/lib"
@@ -61,10 +65,10 @@ in stdenv.mkDerivation rec {
     "-DECHONEST_INCLUDE_DIR=${libechonestQT5}/include"
   ];
 
-  buildInputs = (map useQT5 [ qca2 liblastfm ]) ++ [
-    qtkeychainQT5 libechonestQT5 kf5_latest.attica cmake pkgconfig boost gnutls
-    lucenepp vlc qt54.base qt54.svg qt54.tools qt54.x11extras sparsehash taglib
-    websocketpp makeWrapper
+  buildInputs = (map useQT5 [ liblastfm ]) ++ [
+    qcaQT5 qtkeychainQT5 libechonestQT5 kf5_latest.attica cmake pkgconfig boost
+    gnutls lucenepp vlc qt54.base qt54.svg qt54.tools qt54.x11extras sparsehash
+    taglib websocketpp makeWrapper
   ] ++ stdenv.lib.optional enableXMPP      (useQT5 libjreen)
     ++ stdenv.lib.optional enableKDE       (useQT5 kdelibs)
     ++ stdenv.lib.optional enableTelepathy (useQT5 telepathy_qt);
