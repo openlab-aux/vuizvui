openlab-aux/vuizvui

commit a610560bb4b5a05eb480a4dbab3e24c553727047

release.nix: Rename upstream pkgs to pkgsUpstream.
Mainly to make it a bit easier to distinguish, but also because we want
to make the release attribute set recursive, so we don't run into weird
evaluation errors.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

release.nix
@@ -4,9 +4,9 @@
 
 let
   system = "x86_64-linux";
-  pkgs = import <nixpkgs> { inherit system; };
+  pkgsUpstream = import <nixpkgs> { inherit system; };
 
-in with pkgs.lib; with builtins; {
+in with pkgsUpstream.lib; with builtins; {
 
   machines = mapAttrsRecursiveCond (m: !(m ? build)) (path: attrs:
     attrs.build.config.system.build.toplevel
@@ -19,9 +19,9 @@ in with pkgs.lib; with builtins; {
   pkgs = let
     releaseLib = import <nixpkgs/pkgs/top-level/release-lib.nix> {
       inherit supportedSystems;
-      packageSet = attrs: import ./pkgs {
+      packageSet = attrs: (import ./pkgs {
         pkgs = import <nixpkgs> attrs;
-      } // { inherit (pkgs) lib; };
+      }) // { inherit (pkgsUpstream) lib; };
     };
   in with releaseLib; mapTestOn (packagePlatforms releaseLib.pkgs);
 

commit 40d5244cf3842282aef0a1c19c7bdb508677baae

release.nix: Fix evaluation of manual.
Regression introduced by a610560bb4b5a05eb480a4dbab3e24c553727047.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

release.nix
@@ -42,10 +42,10 @@ in with pkgsUpstream.lib; with builtins; {
     filterDoc = filter (opt: isVuizvui opt && opt.visible && !opt.internal);
     optionsXML = toXML (filterDoc (optionAttrSetToDocList modules.options));
     optionsFile = toFile "options.xml" (unsafeDiscardStringContext optionsXML);
-  in pkgs.stdenv.mkDerivation {
+  in pkgsUpstream.stdenv.mkDerivation {
     name = "vuizvui-options";
 
-    buildInputs = singleton pkgs.libxslt;
+    buildInputs = singleton pkgsUpstream.libxslt;
 
     xsltFlags = ''
       --param section.autolabel 1
@@ -73,7 +73,7 @@ in with pkgsUpstream.lib; with builtins; {
       XML
 
       xsltproc -o "$out/manual.html" $xsltFlags -nonet -xinclude \
-        ${pkgs.docbook5_xsl}/xml/xsl/docbook/xhtml/docbook.xsl \
+        ${pkgsUpstream.docbook5_xsl}/xml/xsl/docbook/xhtml/docbook.xsl \
         manual.xml
 
       cp "${<nixpkgs/nixos/doc/manual/style.css>}" "$out/style.css"