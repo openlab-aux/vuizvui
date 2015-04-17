7f43520465453d63ed09f84f2fc1171aa0f06bcc
devhell <^@regexmail.net>
2015-04-17 23:21:36 +0100

    machine_common: Tidy up a bit by abstracting options

--- machines/machine_common.nix
+++ machines/machine_common.nix
@@ -1,59 +1,85 @@
 { config, pkgs, lib, ... }:
 
 {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
 
-  nix.buildCores = 0;
-  nix.useChroot = true;
-  nix.binaryCaches = [ "https://headcounter.org/hydra/" ];
+  nix = {
+    buildCores = 0;
+    useChroot = true;
+    binaryCaches = [ "https://headcounter.org/hydra/" ];
+  };
 
-  time.timeZone = "Europe/London";
+  time = {
+    timeZone = "Europe/London";
+  };
 
-  system.fsPackages = with pkgs; [
-    sshfsFuse
-    fuse
-    cryptsetup
-  ];
+  system = {
+    fsPackages = with pkgs; [
+      sshfsFuse
+      fuse
+      cryptsetup
+    ];
+  };
 
   hardware = {
     enableAllFirmware = true;
     cpu.intel.updateMicrocode = true;
-    opengl.s3tcSupport = true;
-    opengl.driSupport32Bit = true;
-    pulseaudio.enable = true;
-    pulseaudio.systemWide = false;
+    opengl = {
+      s3tcSupport = true;
+      driSupport32Bit = true;
+    };
+    pulseaudio = {
+      enable = true;
+      systemWide = false;
+    };
   };
 
-  security.setuidPrograms = [ ];
+  security = {
+    setuidPrograms = [ ];
+  };
 
-  programs.ssh.startAgent = false;
-  programs.zsh.enable = true;
-  programs.bash.enableCompletion = true;
-  programs.bash.promptInit = ''
-    # Provide a nice prompt.
-    PROMPT_COLOR="1;31m"
-    let $UID && PROMPT_COLOR="1;32m"
-    PS1="\n\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
-    if test "$TERM" = "xterm"; then
-      PS1="\[\033]2;\h:\u:\w\007\]$PS1"
-    fi
-    eval `dircolors ~/.dir_colors`
-  '';
+  programs = {
+    ssh = {
+      startAgent = false;
+    };
+    zsh = {
+      enable = true;
+    };
+    bash = {
+      enableCompletion = true;
+      promptInit = ''
+        # Provide a nice prompt.
+        PROMPT_COLOR="1;31m"
+        let $UID && PROMPT_COLOR="1;32m"
+        PS1="\n\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
+        if test "$TERM" = "xterm"; then
+          PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+        fi
+        eval `dircolors ~/.dir_colors`
+      '';
+    };
+  };
 
-  environment.shells = [ "/run/current-system/sw/bin/zsh" ];
-  environment.sessionVariables.TERM = "xterm-256color";
+  environment = {
+    shells = [ "/run/current-system/sw/bin/zsh" ];
+    sessionVariables.TERM = "xterm-256color";
+  };
 
-  fonts.enableGhostscriptFonts = true;
-  fonts.fontconfig.enable = true;
-  fonts.enableCoreFonts = true;
-  fonts.fonts = [
-    pkgs.ttf_bitstream_vera
-    pkgs.dejavu_fonts
-    pkgs.dosemu_fonts
-    pkgs.proggyfonts
-    pkgs.vistafonts
-    pkgs.terminus_font
-    pkgs.ubuntu_font_family
-    pkgs.unifont
-  ];
+  fonts = {
+    enableGhostscriptFonts = true;
+    fontconfig.enable = true;
+    enableCoreFonts = true;
+    fonts = with pkgs; [
+      ttf_bitstream_vera
+      dejavu_fonts
+      dosemu_fonts
+      proggyfonts
+      vistafonts
+      terminus_font
+      ubuntu_font_family
+      unifont
+    ];
+  };
 }
