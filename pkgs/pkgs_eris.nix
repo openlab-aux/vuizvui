efd869016fdda2e32335b4d94bd7819f64147808
devhell <^@regexmail.net>
2015-05-04 11:55:04 +0100

    pkgs_eris: Add packages and vaapiSupport
    Since eris also has an Intel graphics card we should really take
    advantage of that in mpv. We'll also add some more tools here.

--- pkgs/pkgs_eris.nix
+++ pkgs/pkgs_eris.nix
@@ -3,12 +3,21 @@
 {
   imports = [ ./pkgs_common.nix ];
 
+  nixpkgs.config.mpv = {
+    vaapiSupport = true;
+  };
+
   environment.systemPackages = with pkgs; [
     aircrackng
     kismet
     minicom
     networkmanagerapplet
+    pamixer
+    pmtools
+    pmutils
     reaverwps
     snort
+    xbindkeys
+    xorg.xbacklight
   ];
 }
