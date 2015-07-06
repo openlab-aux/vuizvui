603f0faa8867a1b1b7e5062cfdef4f43e78ff7e0
aszlig <aszlig@redmoonstudios.org>
2015-07-06 22:49:46 +0200

    pkgs/grandpa: Update to latest Git master.
    The current master version includes a few fixes I made for setting
    default values for the Rockfabrik stage setup, which is to be used by my
    machine called "kzerza" (booted solely via USB storage device).
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/grandpa/default.nix
+++ pkgs/grandpa/default.nix
@@ -1,19 +1,20 @@
-{ fetchgit, buildPythonPackage, pythonPackages, cython, gpm }:
+{ fetchFromGitHub, buildPythonPackage, pythonPackages, cython, gpm }:
 
 pythonPackages.buildPythonPackage {
   name = "grandpa-0.5";
   namePrefix = "";
 
-  src = fetchgit {
-    url = "git://github.com/aszlig/GrandPA.git";
-    rev = "41f21f67446b98f4600bc043abe32b95af6dd048";
-    sha256 = "06sgz39ri0ai3x2fck44rczk04pj8zcysbhp97x20ggmqhx5vxyh";
+  src = fetchFromGitHub {
+    owner = "aszlig";
+    repo = "GrandPA";
+    rev = "c8eb1fc40b20610380583444446b3e62b841df07";
+    sha256 = "0mxf2ddqhbhcgblvivi0hslflabdj0qwyx92zbzayzgwlbqz4hss";
   };
 
   doCheck = false;
 
   buildInputs = [ cython gpm ];
   propagatedBuildInputs = with pythonPackages; [
-    bsddb curses pyserial pyglet
+    bsddb curses pyserial
   ];
 }
