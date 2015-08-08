5a60d26a26da27a68f3b65183086bbeab18eda81
aszlig <aszlig@redmoonstudios.org>
2015-08-08 09:41:57 +0200

    bfqsched: Update to version 4.1-v7r8.
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/kpatches/bfqsched.nix
+++ pkgs/kpatches/bfqsched.nix
@@ -1,8 +1,8 @@
 { stdenv, fetchurl }:
 
 let
-  bfqVersion = "v7r7";
-  kernelVersion = "4.0";
+  bfqVersion = "v7r8";
+  kernelVersion = "4.1";
   version = "${kernelVersion}.0-${bfqVersion}";
 
   baseURL = "http://algo.ing.unimo.it/people/paolo/disk_sched/patches";
@@ -16,17 +16,17 @@ let
     (fetchPatch {
       name = "0001-block-cgroups-kconfig-build-bits-for-BFQ-"
            + "${bfqVersion}-${kernelVersion}";
-      sha256 = "0gi4qwk2wn2lr707ybh800fx4ygx5jw8glph2rqgqz2qg5pph4zz";
+      sha256 = "0xb9x502j5s4j31fzplr88i4qik2h6prh2a5gaidk9hy0p4a637c";
     })
     (fetchPatch {
       name = "0002-block-introduce-the-BFQ-"
            + "${bfqVersion}-I-O-sched-for-${kernelVersion}";
-      sha256 = "1sn567adj20mzn2cbg5xppsfgipcihdwk7xd2ndnxw4hnyjhl2ll";
+      sha256 = "0yz8pnifrbrcir6b1r7iz7spz2rwqgy8y4dx904iia62723c9hn5";
     })
     (fetchPatch {
       name = "0003-block-bfq-add-Early-Queue-Merge-EQM-to-BFQ-"
            + "${bfqVersion}-for-${kernelVersion}.0";
-      sha256 = "1fz6rhb8sh4bi3bwb40014gaazlnx0wwv2ksxkxipsg0gnbdzdg8";
+      sha256 = "1n9070kbr9rly6aabjzkfb100ibk2b97mhhssk4awjfzvdmgfc2g";
     })
   ];
 
