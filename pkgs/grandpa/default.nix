1adb23cfc77725b1c7f16f7ed51461a3a32ea199
aszlig <aszlig@redmoonstudios.org>
2015-08-08 19:33:56 +0200

    pkgs/grandpa: Update to latest upstream master.
    Includes changes for the new Rockfabrik dancefloor/stage setup.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/grandpa/default.nix
+++ pkgs/grandpa/default.nix
@@ -7,8 +7,8 @@ pythonPackages.buildPythonPackage {
   src = fetchFromGitHub {
     owner = "aszlig";
     repo = "GrandPA";
-    rev = "c8eb1fc40b20610380583444446b3e62b841df07";
-    sha256 = "0mxf2ddqhbhcgblvivi0hslflabdj0qwyx92zbzayzgwlbqz4hss";
+    rev = "a9bb455a6789c4b9bacd27ab9d3cdb7b141c29d8";
+    sha256 = "15aqpinrwsg53g1aprkqqp1sr5ch4q8zpm982s228i38vdzp8i60";
   };
 
   doCheck = false;
