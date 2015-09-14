faf503c964e7e3a0be2f7b95216576ff70938059
devhell <^@regexmail.net>
2015-09-14 17:12:24 +0100

    overrides: Add workaround for Haskell/Hydra bug
    See: https://github.com/headcounter/deployment/issues/19 for the
    original issue.

--- overrides/overrides.nix
+++ overrides/overrides.nix
@@ -1,7 +1,16 @@
 { config, pkgs, lib, ... }:
 
+with lib;
+
 {
   nixpkgs.config.packageOverrides = pkgs: {
     gnupg = pkgs.gnupg21;
+
+    # XXX: Very ugly workaround see https://github.com/openlab-aux/vuizvui/commit/a93b7583084ff9084d73873d80d8dc428406593c
+    haskellPackages = pkgs.haskellPackages.override {
+      ghc = pkgs.haskellPackages.ghc.overrideDerivation (const {
+        forceRebuild = true;
+      });
+    };
   };
 }
