4a356e6fbf47d74cb672f06ce5cc94b40d09808e
devhell <^@regexmail.net>
2015-10-04 20:02:19 +0100

    users_dev: Add user dev to libvirtd group
    Without this there might be some problems when trying to fire up some
    virtual machines using `libvirtd` instead of `virtualbox`.

--- users/users_dev.nix
+++ users/users_dev.nix
@@ -3,7 +3,7 @@
 {
   users.extraUsers.dev = {
     isNormalUser = true;
-    extraGroups = [ "vboxusers" "wheel" "mpd" "networkmanager"];
+    extraGroups = [ "vboxusers" "wheel" "mpd" "networkmanager" "libvirtd" ];
     uid = 1000;
     shell = "${pkgs.zsh}/bin/zsh";
   };
