a8b6eeb17ea48eaae57de00c9968e31ac7eb9f1b
aszlig <aszlig@redmoonstudios.org>
2015-10-07 13:08:02 +0200

    pkgs/bfqsched: Update to version 4.2.0-v7r9.
    From the changelog:
    
     * IMPROVEMENT: Changed BFQ to use the blkio controller instead of its
                    own controller. BFQ now registers itself as a policy to
                    the blkio controller and implements its hierarchical
                    scheduling support using data structures that already
                    exist in blk-cgroup. The bfqio controller's code is
                    completely removed.
    
     * CODE IMPROVEMENTS: Applied all suggestions from Tejun Heo,
                          received on the last submission to LKML:
                          https://lkml.org/lkml/2014/5/27/314
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/kpatches/bfqsched.nix
+++ pkgs/kpatches/bfqsched.nix
@@ -1,8 +1,8 @@
 { stdenv, fetchurl }:
 
 let
-  bfqVersion = "v7r8";
-  kernelVersion = "4.1";
+  bfqVersion = "v7r9";
+  kernelVersion = "4.2";
   version = "${kernelVersion}.0-${bfqVersion}";
 
   baseURL = "http://algo.ing.unimo.it/people/paolo/disk_sched/patches";
@@ -16,17 +16,17 @@ let
     (fetchPatch {
       name = "0001-block-cgroups-kconfig-build-bits-for-BFQ-"
            + "${bfqVersion}-${kernelVersion}";
-      sha256 = "0xb9x502j5s4j31fzplr88i4qik2h6prh2a5gaidk9hy0p4a637c";
+      sha256 = "1998g05gj2vcwxhccjsa4nch3pyzm2qlc1k9q688f70j19qbnybk";
     })
     (fetchPatch {
       name = "0002-block-introduce-the-BFQ-"
            + "${bfqVersion}-I-O-sched-for-${kernelVersion}";
-      sha256 = "0yz8pnifrbrcir6b1r7iz7spz2rwqgy8y4dx904iia62723c9hn5";
+      sha256 = "1p5xxpzbmch03r7d0wpfr4x1vhy1h7mv2lwck7lq3184mcscqgkl";
     })
     (fetchPatch {
       name = "0003-block-bfq-add-Early-Queue-Merge-EQM-to-BFQ-"
            + "${bfqVersion}-for-${kernelVersion}.0";
-      sha256 = "1n9070kbr9rly6aabjzkfb100ibk2b97mhhssk4awjfzvdmgfc2g";
+      sha256 = "10bzgdz4n2q6inv1w8v0qdhl0pl1gndn1iz97qkxdi0zfabc574d";
     })
   ];
 
