09df9e6095b6dfdc78ecfae51fcb3f34bbb1ff42
aszlig <aszlig@redmoonstudios.org>
2015-10-23 15:29:58 +0200

    pkgs/nixops: Switch to vuizvui branch of my fork.
    Instead of merging all those PRs via the patches attribute, I've now
    created a branch that has all those PRs merged, which are:
    
     * NixOS/nixops#201: Use dedicated SSH keypair for "none" backend
     * NixOS/nixops#348: Fixup and refactor Hetzner backend tests
     * NixOS/nixops#349: hetzner: Don't create /root/.ssh/authorized_keys
     * NixOS/nixops#350: Fix tests for the "none" backend
    
    So our version of NixOps now should now correctly cope with
    users.mutableUsers set to false.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/nixops/default.nix
+++ pkgs/nixops/default.nix
@@ -1,34 +1,19 @@
 { stdenv, fetchFromGitHub, fetchpatch, git }:
 
 let
-  rev = "ac1182c3ccfa77b7c1f46e6c1c9e812c76c50ff0";
-  sha256 = "02271pvc458vpn6a1qjn2amrk1k3z62lrgghjnmyhgqihhbckf72";
+  rev = "85439475872a2fc0336b5dcb02711293f611e004";
+  sha256 = "007jmf0x06bk1jfhgdlaamdnwpda57rpab12xisy7sxmfvafgp4j";
 
   master = stdenv.mkDerivation rec {
     name = "nixops-upstream-patched";
 
     src = fetchFromGitHub {
-      owner = "NixOS";
+      owner = "aszlig";
       repo = "nixops";
       inherit rev sha256;
     };
 
     phases = [ "unpackPhase" "patchPhase" "installPhase" ];
-
-    patches = [
-      (fetchpatch {
-        url = "https://github.com/NixOS/nixops/pull/201.diff";
-        sha256 = "1i5yycqayxggg3l1i6wk8lp64lqlxw5nmfya9fcrgmck8ls0rxid";
-      })
-      (fetchpatch rec {
-        name = "read-write-by-default.diff";
-        url = "https://github.com/aszlig/nixops/compare/"
-            + "NixOS:master...aszlig:${name}";
-        sha256 = "0a1jcqrqfi7dfvlha5r0609bzvin7p7nj523xxcrvwpgp6ag0zsa";
-      })
-    ];
-
-    patchFlags = "--merge -p1";
 
     postPatch = ''
       sed -i -re 's!<nixpkgs([^>]*)>!${import ../../nixpkgs-path.nix}\1!g' \
