openlab-aux/vuizvui

commit 5e1ad35657c05f409a29ddc7a29501fc611111b0

services/vlock: Only include socket when enabled.
Just stumbled over this... even systems that do not define
vuizvui.user.aszlig.services.vlock.enable accidentally got the socket
file, which now is no longer the case.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

modules/user/aszlig/services/vlock/default.nix
@@ -30,26 +30,28 @@ in {
     enable = lib.mkEnableOption "console lock";
   };
 
-  config.systemd.sockets.vlock = {
-    description = "Console Lock Socket";
-    wantedBy = [ "sockets.target" ];
-    socketConfig.ListenStream = "/run/console-lock.sock";
-    socketConfig.Accept = true;
-  };
+  config = lib.mkIf cfg.enable {
+    systemd.sockets.vlock = {
+      description = "Console Lock Socket";
+      wantedBy = [ "sockets.target" ];
+      socketConfig.ListenStream = "/run/console-lock.sock";
+      socketConfig.Accept = true;
+    };
 
-  config.systemd.services."vlock@" = lib.mkIf cfg.enable {
-    description = "Lock All Consoles";
-    serviceConfig.Type = "oneshot";
+    config.systemd.services."vlock@" = {
+      description = "Lock All Consoles";
+      serviceConfig.Type = "oneshot";
 
-    #environment.USER = "%i"; XXX
-    environment.USER = "aszlig";
+      #environment.USER = "%i"; XXX
+      environment.USER = "aszlig";
 
-    script = ''
-      retval=0
-      oldvt="$("${pkgs.kbd}/bin/fgconsole")"
-      "${vlock}/bin/vlock" -asn || retval=$?
-      if [ $retval -ne 0 ]; then "${pkgs.kbd}/bin/chvt" "$oldvt"; fi
-      exit $retval
-    '';
+      script = ''
+        retval=0
+        oldvt="$("${pkgs.kbd}/bin/fgconsole")"
+        "${vlock}/bin/vlock" -asn || retval=$?
+        if [ $retval -ne 0 ]; then "${pkgs.kbd}/bin/chvt" "$oldvt"; fi
+        exit $retval
+      '';
+    };
   };
 }

commit 20225bda26bc4d9c73a7cd536b37b82ee445eb2f

services/vlock: Fix evaluation error.
Regression introduced by 5e1ad35657c05f409a29ddc7a29501fc611111b0.

Geesh, that's what happens if you're already tired out.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

modules/user/aszlig/services/vlock/default.nix
@@ -38,7 +38,7 @@ in {
       socketConfig.Accept = true;
     };
 
-    config.systemd.services."vlock@" = {
+    systemd.services."vlock@" = {
       description = "Lock All Consoles";
       serviceConfig.Type = "oneshot";
 