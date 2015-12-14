2c5b52769214bfb36bdaf5957e32b667f7d87b65
aszlig <aszlig@redmoonstudios.org>
2015-12-14 20:34:21 +0100

    machines/tishtushi: Enable taalo-build.
    It's my primary laptop and it has a very limited bandwidth to the
    internet right now.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- machines/aszlig/tishtushi.nix
+++ machines/aszlig/tishtushi.nix
@@ -9,6 +9,7 @@ let
   diskID = "ata-Hitachi_HTS543232A7A384_E2P31243FGB6PJ";
 in {
   vuizvui.user.aszlig.profiles.workstation.enable = true;
+  vuizvui.user.aszlig.programs.taalo-build.enable = true;
 
   boot = rec {
     kernelPackages = with pkgs; let
