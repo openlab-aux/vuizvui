8551763da3267d1a0e08c13bd902f5bc1c0363df
devhell <^@regexmail.net>
2015-12-21 20:56:28 +0000

    overrides: Add ncmpcpp options
    Enable visualization and clock support.

--- overrides/overrides.nix
+++ overrides/overrides.nix
@@ -11,6 +11,11 @@ with lib;
       enableOfficialBranding = true;
     };
 
+    ncmpcpp = pkgs.ncmpcpp.override {
+      visualizerSupport = true;
+      clockSupport = true;
+    };
+
     # XXX: Very ugly workaround see https://github.com/openlab-aux/vuizvui/commit/a93b7583084ff9084d73873d80d8dc428406593c
     haskellPackages = pkgs.haskellPackages.override {
       ghc = pkgs.haskellPackages.ghc.overrideDerivation (const {
