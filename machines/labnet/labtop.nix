openlab-aux/vuizvui

commit d99fd1ba860dd513508a7460bf139e9ee990a5d3

thinkpad firmware

machines/labnet/labtop.nix
@@ -1,6 +1,28 @@
 { pkgs, lib, ... }:
 
 let
+  greybird = pkgs.stdenv.mkDerivation {
+    name = "greybird-xfce-theme";
+
+    src = pkgs.fetchFromGitHub {
+      repo = "Greybird";
+      owner = "shimmerproject";
+      rev = "61ec18d22780aa87998381599c941e0cf4f7bfb5";
+      sha256 = "03h8hba4lfp337a4drylcplrbggry9gz8dq1f3gjy25fhqkgvq05";
+    };
+
+    phases = [ "unpackPhase" "installPhase" ];
+
+    installPhase = ''
+      mkdir -p "$out/share/themes/Greybird" \
+               "$out/share/themes/Greybird-compact/xfwm4"
+      cp -vrt "$out/share/themes/Greybird" \
+        gtk-* metacity-1 unity xfce-notify-4.0 xfwm4
+      cp -vrt "$out/share/themes/Greybird-compact/xfwm4" \
+        xfwm4_compact/*
+    '';
+  };
+
   modulesPath = "${import ../../nixpkgs-path.nix}/nixos/modules";
 
 in {
@@ -23,6 +45,8 @@ in {
     device = "/dev/disk/by-uuid/754fd3e3-2e04-4028-9363-0c6bb4c54367";
     fsType = "ext4";
   };
+
+  vuizvui.hardware.thinkpad.enable = true;
 
   environment.systemPackages = with pkgs; [
     #repetierhost <- TODO
@@ -62,19 +86,18 @@ in {
     displayManager.auto.enable = true;
     displayManager.auto.user = "openlab";
     desktopManager.xfce.enable = true;
-
-    synaptics.enable = true;
-    synaptics.minSpeed = "0.5";
-    synaptics.accelFactor = "0.01";
+    # synaptics.enable = true;
+    # synaptics.minSpeed = "0.5";
+    # synaptics.accelFactor = "0.01";
   };
 
 
-  hardware.trackpoint = {
-    enable = true;
-    emulateWheel = true;
-    sensitivity = 130;
-    speed = 350;
-  };
+  # hardware.trackpoint = {
+  #   enable = true;
+  #   emulateWheel = true;
+  #   sensitivity = 130;
+  #   speed = 350;
+  # };
 
 
   services.openssh.enable = true;
@@ -82,8 +105,11 @@ in {
   networking.networkmanager.enable = true;
   networking.enableIntel3945ABGFirmware = true;
   networking.hostName = "labtop";
-  networking.firewall.allowedTCPPorts = [ 1337 2342 ];
-  networking.firewall.allowPing = true;
+  networking.firewall = {
+    allowedTCPPorts = [ 1337 2342 ];
+    allowedTCPPortRanges = [ { from = 8000; to = 8005 } ];
+    allowPing = true;
+  };
 
   nix.maxJobs = 2;
 

commit b6cae323e1c5f0098c1b711c9ec86a8028da559d

machines/labtop: Fix syntax error
Regression introduced by d99fd1ba860dd513508a7460bf139e9ee990a5d3.

Fixes the following evaluation error:

error: syntax error, unexpected '}', expecting ';', at labtop.nix:110:55

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

machines/labnet/labtop.nix
@@ -107,7 +107,7 @@ in {
   networking.hostName = "labtop";
   networking.firewall = {
     allowedTCPPorts = [ 1337 2342 ];
-    allowedTCPPortRanges = [ { from = 8000; to = 8005 } ];
+    allowedTCPPortRanges = [ { from = 8000; to = 8005; } ];
     allowPing = true;
   };
 