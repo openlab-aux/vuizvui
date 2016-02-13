openlab-aux/vuizvui

commit 7e255747e0b6edbf2bfd71edf6fea0d47a35ded6

labtop: Switch to GNOME
(test)

machines/labnet/labtop.nix
@@ -62,7 +62,6 @@ in {
     gimp
     git
     gmpc
-    vuizvui.greybird-xfce-theme
     inkscape
     ino
     (libreoffice.overrideDerivation (lib.const { doCheck = false; }))
@@ -85,7 +84,7 @@ in {
 
     displayManager.auto.enable = true;
     displayManager.auto.user = "openlab";
-    desktopManager.xfce.enable = true;
+    desktopManager.gnomeenable = true;
     # synaptics.enable = true;
     # synaptics.minSpeed = "0.5";
     # synaptics.accelFactor = "0.01";

commit bf331e660fc05e4e22f8b4510c75a55494fdcc65

machines/labtop: Fix evaluation error
Regression introduced by 7e255747e0b6edbf2bfd71edf6fea0d47a35ded6.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

machines/labnet/labtop.nix
@@ -84,7 +84,7 @@ in {
 
     displayManager.auto.enable = true;
     displayManager.auto.user = "openlab";
-    desktopManager.gnomeenable = true;
+    desktopManager.gnome.enable = true;
     # synaptics.enable = true;
     # synaptics.minSpeed = "0.5";
     # synaptics.accelFactor = "0.01";