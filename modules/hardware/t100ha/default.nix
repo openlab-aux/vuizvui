0aae52f426a1c145de68ddb852123fc99793f412
aszlig <aszlig@redmoonstudios.org>
2016-02-23 09:10:27 +0100

    hardware/t100ha: Rotate monitor in Xorg as well
    We already rotate the fbcon display, but we need to make sure we also
    apply the rotation to the X server.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- modules/hardware/t100ha/default.nix
+++ modules/hardware/t100ha/default.nix
@@ -12,9 +12,17 @@ in {
     boot.initrd.availableKernelModules = [
       "xhci_pci" "sdhci_acpi" "mmc_block"
     ];
+
     # It's a CherryTrail SoC, so we want to have the latest and greatest:
     boot.kernelPackages = pkgs.linuxPackages_latest;
+
     # By default the console is rotated by 90 degrees to the right.
     boot.kernelParams = [ "fbcon=rotate:3" ];
+    services.xserver.deviceSection = ''
+      Option "monitor-DSI1" "Monitor[0]"
+    '';
+    services.xserver.monitorSection = ''
+      Option "Rotate" "left"
+    '';
   };
 }
