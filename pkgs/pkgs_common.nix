920452f72c73ef43a63c8eb64296d71601c282e0
devhell <^@regexmail.net>
2016-03-15 14:10:59 +0000

    pkgs_common: Readd firefox options
    This readds the enableGTK3 and enableOfficalBranding options. Let's see
    if this builds.

--- pkgs/pkgs_common.nix
+++ pkgs/pkgs_common.nix
@@ -17,6 +17,11 @@
       x11Support = false;
     };
 
+    firefox = {
+      enableGTK3 = true;
+      enableOfficalBranding = true;
+    };
+
     virtualbox = {
       enableExtensionPack = true;
     };
