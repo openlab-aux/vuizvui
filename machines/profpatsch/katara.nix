68860a23992a33e4363c791057f8edb58838d315
Profpatsch <mail@profpatsch.de>
2016-04-03 13:01:33 +0200

    machines/katara: refactor a bit

--- machines/profpatsch/katara.nix
+++ machines/profpatsch/katara.nix
@@ -5,6 +5,21 @@ let
 
   # mytexlive = with pkgs.texlive; combine { inherit minted; }; # inherit scheme-medium minted units collection-bibtexextra; };
 
+  authKeys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJhthfk38lzDvoI7lPqRneI0yBpZEhLDGRBpcXzpPSu+V0YlgrDix5fHhBl+EKfw4aeQNvQNuAky3pDtX+BDK1b7idbz9ZMCExy2a1kBKDVJz/onLSQxiiZMuHlAljVj9iU4uoTOxX3vB85Ok9aZtMP1rByRIWR9e81/km4HdfZTCjFVRLWfvo0s29H7l0fnbG9bb2E6kydlvjnXJnZFXX+KUM16X11lK53ilPdPJdm87VtxeSKZ7GOiBz6q7FHzEd2Zc3CnzgupQiXGSblXrlN22IY3IWfm5S/8RTeQbMLVoH0TncgCeenXH7FU/sXD79ypqQV/WaVVDYMOirsnh/ philip@nyx"];
+ 
+  philip = rec {
+    name = "philip";
+    group = "users";
+          extraGroups = [ "wheel" "networkmanager" "sambashare" ];
+    uid = 1000;
+    createHome = true;
+    home = "/home/philip";
+          passwordFile = "${home}/.config/passwd";
+          # password = "test"; # in case of emergency, break glass
+    shell = "${pkgs.fish}/bin/fish";
+          openssh.authorizedKeys.keys = authKeys;
+    };
+
 in {
 
   config = rec {
@@ -303,22 +318,7 @@ in {
 
     # Nobody wants mutable state. :)
     users.mutableUsers = false;
-    users.users =
-      let authKeys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJhthfk38lzDvoI7lPqRneI0yBpZEhLDGRBpcXzpPSu+V0YlgrDix5fHhBl+EKfw4aeQNvQNuAky3pDtX+BDK1b7idbz9ZMCExy2a1kBKDVJz/onLSQxiiZMuHlAljVj9iU4uoTOxX3vB85Ok9aZtMP1rByRIWR9e81/km4HdfZTCjFVRLWfvo0s29H7l0fnbG9bb2E6kydlvjnXJnZFXX+KUM16X11lK53ilPdPJdm87VtxeSKZ7GOiBz6q7FHzEd2Zc3CnzgupQiXGSblXrlN22IY3IWfm5S/8RTeQbMLVoH0TncgCeenXH7FU/sXD79ypqQV/WaVVDYMOirsnh/ philip@nyx"];
-      in {
-        philip = rec {
-  	name = "philip";
-  	group = "users";
-          extraGroups = [ "wheel" "networkmanager" ];
-  	uid = 1000;
-  	createHome = true;
-  	home = "/home/philip";
-          passwordFile = "${home}/.config/passwd";
-          # password = "test"; # in case of emergency, break glass
-    shell = "${pkgs.fish}/bin/fish";
-          openssh.authorizedKeys.keys = authKeys;
-      };
-    };
+    users.users = { inherit philip; };
 
     ###########
     # Programs
