c2589e43cc3afe0331a8d6bdf25b8ebba96307e5
aszlig <aszlig@redmoonstudios.org>
2016-04-26 19:33:18 +0200

    machines/tyree: Add Thunderbird to system packages
    I'd prefer to use kdepim, but that apparently doesn't seem to be
    packaged yet.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- machines/aszlig/managed/tyree.nix
+++ machines/aszlig/managed/tyree.nix
@@ -9,7 +9,8 @@
 
   environment.systemPackages = with pkgs; [
     cdparanoia chromium figlet gajim gimp htop inkscape kde5.gwenview
-    libreoffice mosh mpv pciutils skype vlc vuizvui.tomahawk wget wine youtubeDL
+    libreoffice mosh mpv pciutils skype thunderbird vlc vuizvui.tomahawk wget
+    wine youtubeDL
   ];
 
   fileSystems."/boot".device = "/dev/disk/by-uuid/A0D5-269D";
