075067bfa2a59c39e960aba92f798cd626cd073d
aszlig <aszlig@redmoonstudios.org>
2016-05-03 01:03:47 +0200

    machines: Add a new callMachines function
    This is particularly needed for the Labtops and it allows to call a full
    attribute set of machines to be incorporated into a single file as one
    big attribute set.
    
    Its functionality is kind of similar to the NixOps network expressions
    by providing default hostnames (in our case with a priority of 900 to
    still make it overridable without pain).
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- machines/default.nix
+++ machines/default.nix
@@ -2,6 +2,17 @@
 
 let
   callMachine = import ../lib/call-machine.nix;
+  callMachines = path: args: let
+    machines = import path;
+  in with builtins; listToAttrs (map (name: {
+    inherit name;
+    value = callMachine machines.${name} ({
+      extraConfig = { lib, ... }: {
+        imports = lib.singleton (args.extraConfig or {});
+        networking.hostName = lib.mkOverride 900 name;
+      };
+    } // removeAttrs args [ "extraConfig" ]);
+  }) (attrNames machines));
   nixpkgs = import (import ../nixpkgs-path.nix) {};
 in {
   aszlig = {
