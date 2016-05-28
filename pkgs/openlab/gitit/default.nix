a4d63956319379be4221091edb8c24bdfb102d51
aszlig <aszlig@redmoonstudios.org>
2016-05-28 18:02:34 +0200

    pkgs/openlab/gitit: Build on Hydra
    Instead of setting meta.platforms, we're setting just "platforms" here,
    because the cabal build expression passes it through as meta.platforms.
    
    Note that I'm currently setting the platforms attribute to just
    x86_64-linux for now, because that is the architecture the workaround
    machine "website-vm" so far has been built for.
    
    Of course, if we'd not set the platforms at all, it would include all
    supported architectures by default.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>
    Cc: @Profpatsch

--- pkgs/openlab/gitit/default.nix
+++ pkgs/openlab/gitit/default.nix
@@ -9,6 +9,7 @@ let hp = haskellPackages.override {
         rev = "3da7c841f9382d0c62242a1b718511acec97e9f7";
         sha256 = "0qhkbvm4ixav4nln3m9845w9m3gzfq5xh4nxp2c9qj4w9p79if7z";
       };
+      platforms = [ "x86_64-linux" ];
     }));
   });
 };
