8e68d2b9ae4ac0f358d80fed9cc99a84da42ef04
aszlig <aszlig@redmoonstudios.org>
2016-06-05 02:14:43 +0200

    modules: Update my GnuPG public key
    Time for a new GnuPG key, this time CCID-only :-)
    
    This is the last commit you'll get using my old key (which will soon be
    revoked as soon as I moved everything over to use the new key), so if
    you're paranoid be sure to check the fingerprint against the signature
    of this very commit.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- modules/user/aszlig/programs/gajim/config.nix
+++ modules/user/aszlig/programs/gajim/config.nix
@@ -389,7 +389,7 @@ in mkConfig {
       is_zeroconf = false;
       keep_alive_every_foo_secs = "55";
       keep_alives_enabled = true;
-      keyid = "8C2DC961";
+      keyid = "4DFD43EC834B6901BDA2BAAC1DE8E48E57DB5436";
       keyname = ''aszlig <"^[0-9]+$"@regexmail.net>'';
       last_archiving_time = "1970-01-01T00:00:00Z";
       last_status_msg = "";
