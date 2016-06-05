openlab-aux/vuizvui

commit fba5c2469bd5e95857c4ffd9db3dd0529f2485f3

programs/taalo-build: Add link to derivation file
This is needed to ensure that the .drv file doesn't get garbage
collected while we're transferring it to taalo.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

modules/user/aszlig/programs/taalo-build/default.nix
@@ -61,8 +61,16 @@ let
 
   taalo-build = pkgs.writeScriptBin "taalo-build" ''
     #!${pkgs.stdenv.shell}
-    drvs="$(nix-instantiate "$@" | cut -d'!' -f1)" || exit 1
-    exec ${backend} $drvs
+    if tmpdir="$("${pkgs.coreutils}/bin/mktemp" -d taalo-build.XXXXXX)"; then
+      trap "rm -rf '$tmpdir'" EXIT
+      drvs="$(nix-instantiate --add-root "$tmpdir/derivation" --indirect "$@" \
+        | cut -d'!' -f1)" || exit 1
+      ${backend} $drvs
+      exit $?
+    else
+      echo "Unable to create temporary directory for build link!" >&2
+      exit 1
+    fi
   '';
 
 in {

commit afa9e7dbec5902c74ed6dceddab224c2bd34a6ad

taalo-build: Fix picking up derivation paths
Regression introduced by fba5c2469bd5e95857c4ffd9db3dd0529f2485f3.

The derivation paths returned by nix-instantiate are usually valid store
paths, except when used with --add-root. In case of the latter, we get
the root symlink back, so we need to do an additional readlink on it.

We also now pass -t to mktemp so that $TMPDIR or /tmp is used instead of
the current working directory.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

modules/user/aszlig/programs/taalo-build/default.nix
@@ -61,11 +61,11 @@ let
 
   taalo-build = pkgs.writeScriptBin "taalo-build" ''
     #!${pkgs.stdenv.shell}
-    if tmpdir="$("${pkgs.coreutils}/bin/mktemp" -d taalo-build.XXXXXX)"; then
+    if tmpdir="$("${pkgs.coreutils}/bin/mktemp" -d -t taalo-build.XXXXXX)"; then
       trap "rm -rf '$tmpdir'" EXIT
       drvs="$(nix-instantiate --add-root "$tmpdir/derivation" --indirect "$@" \
         | cut -d'!' -f1)" || exit 1
-      ${backend} $drvs
+      ${backend} $("${pkgs.coreutils}/bin/readlink" $drvs)
       exit $?
     else
       echo "Unable to create temporary directory for build link!" >&2