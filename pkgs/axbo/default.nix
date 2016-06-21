8faa61ad6d8ff28af2db196c5faf53e0b77ee9e6
aszlig <aszlig@redmoonstudios.org>
2016-06-21 04:08:55 +0200

    pkgs/axbo: Update to latest development branch
    This one explicitly includes a MIT license, so let's actually prefer
    this branch instead of the master brach.
    
    I've also included commons and streamflyer to the list of bundled JARs
    that are needed from the upstream repository. Of course, in the long
    term it would be a good idea to actually package those in <nixpkgs> so
    it's just temporary.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/axbo/default.nix
+++ pkgs/axbo/default.nix
@@ -9,8 +9,10 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "jansolo";
     repo = "aXbo-research";
-    rev = "aXbo-research_${version}";
-    sha256 = "0p2my5bczmwnrs3c0l9wyq3gsc5vydw0nh9n8jkdp9vm77z0kgvd";
+    #rev = "aXbo-research_${version}";
+    # Includes MIT license:
+    rev = "6e6888917b5f200a44509650d6f46ec42c133cdc";
+    sha256 = "0nbyxajl75q80cnyl9c0sjlyk3rmhm7k8w8mksg4lfyh78ynayyc";
   };
 
   sourceRoot = "${src.name}/aXbo-research";
@@ -24,8 +26,10 @@ stdenv.mkDerivation rec {
   extraJars = [
     "commons-beanutils-1.8.3"
     "commons-digester3-3.2"
+    "commons-io-2.4"
     "jcommon-1.0.20"
     "jfreechart-1.0.16"
+    "streamflyer-core-1.0.1"
     "swingx-all-1.6.4"
   ];
 
