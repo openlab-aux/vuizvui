decd0f04f517e832e2479ec2543141399cc87c80
aszlig <aszlig@redmoonstudios.org>
2016-08-01 23:12:32 +0200

    machines/brawndo: Disable containers
    For now the containers-imperative tests are failing and containers
    aren't used on this machine either, so let's just disable them.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- machines/aszlig/managed/brawndo.nix
+++ machines/aszlig/managed/brawndo.nix
@@ -6,6 +6,7 @@ let
 
 in {
   boot = {
+    enableContainers = false;
     initrd.availableKernelModules = [
       "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod"
       "rtsx_pci_sdmmc"
