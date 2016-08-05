adf1cf2895ac31f85571c69ccf6a877580040170
devhell <^@regexmail.net>
2016-08-05 17:02:00 +0100

    overrides: Add LAME support to sox

--- overrides/overrides.nix
+++ overrides/overrides.nix
@@ -11,6 +11,10 @@ with lib;
       clockSupport = true;
     };
 
+    sox = pkgs.sox.override {
+      enableLame = true;
+    };
+
     # XXX: Very ugly workaround see https://github.com/openlab-aux/vuizvui/commit/a93b7583084ff9084d73873d80d8dc428406593c
     haskellPackages = pkgs.haskellPackages.override {
       ghc = pkgs.haskellPackages.ghc.overrideDerivation (const {
