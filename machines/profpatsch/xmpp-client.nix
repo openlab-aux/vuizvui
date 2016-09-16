d2d2cfaf940a7e34780e714ac95733bc738f56ad
Profpatsch <mail@profpatsch.de>
2016-09-16 16:39:11 +0200

    machines/katara: logging for xmpp-client
    Enable logging of xmpp-client output with `script(1)`.

--- machines/profpatsch/xmpp-client.nix
+++ machines/profpatsch/xmpp-client.nix
@@ -4,7 +4,17 @@ home: passwordentry:
 writeScriptBin "xmpp-client" ''
   #!/usr/bin/env bash
   PASS=$(${lib.getBin pass}/bin/pass "${passwordentry}" | head -n1)
-  ${lib.getBin xmpp-client}/bin/xmpp-client \
-    --config-file <(sed "s/@PASS@/$PASS/" ${home}/.config/xmpp-client/config)
+
+  # pipe cofnig with password in fifo
+  TMP=$(mktemp)
+  mkfifo "$TMP"
+  sed "s/@PASS@/$PASS/" ${home}/.config/xmpp-client/config > "$TMP"
+
+
+  # execute the client with logging enabled
+  mkdir -p ${home}/.local/share/xmpp-client
+  LOG=${home}/.local/share/xmpp-client/history
+  CMD="${lib.getBin xmpp-client}/bin/xmpp-client --config-file $TMP"
+  script --append --command "$CMD" "$LOG"
 ''
 
