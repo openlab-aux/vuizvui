7d5c3f5e09e2731b03179591d635a1a5766cd251
sternenseemann <git@lukasepple.de>
2016-09-23 22:51:52 +0200

    pkgs/games/humblebundle: reflect changes in version 0.1.9

--- pkgs/games/humblebundle/pico-8.nix
+++ pkgs/games/humblebundle/pico-8.nix
@@ -1,8 +1,8 @@
-{ stdenv, fetchHumbleBundle, SDL2, unzip }:
+{ stdenv, fetchHumbleBundle, SDL2, unzip, xorg}:
 
 stdenv.mkDerivation rec {
   name = "pico-8-${version}";
-  version = "0.1.8";
+  version = "0.1.9";
 
   src = fetchHumbleBundle {
     name = "pico8_linux";
@@ -11,7 +11,10 @@ stdenv.mkDerivation rec {
       "x86_64-linux" = "64-bit";
       "i686-linux"   = "32-bit";
     }.${stdenv.system};
-    md5 = "5866cf247c3390011fb4e84092c3ac8e";
+    md5 = {
+      "x86_64-linux" = "3c280c31f53443a4bc7f32647cfdc8f6";
+      "i686-linux"   = "54c22267748812952f3a0f6363fedc70";
+    }.${stdenv.system};
   };
 
   unpackCmd = ''
@@ -22,16 +25,16 @@ stdenv.mkDerivation rec {
 
   buildPhase = let
     rpath = stdenv.lib.makeLibraryPath [
-      stdenv.cc.cc SDL2
+      stdenv.cc.cc SDL2 xorg.libXxf86vm
     ];
   in ''
     patchelf \
       --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      --set-rpath "${rpath}" pico8_dyn
+      --set-rpath "${rpath}" pico8
   '';
 
   installPhase = ''
-    install -vD pico8_dyn "$out/share/pico8"
+    install -vD pico8 "$out/share/pico8"
     install -vD pico8.dat "$out/share/pico8.dat"
     install -vD pico-8.txt "$out/share/pico-8.txt"
     install -vD license.txt "$out/share/license.txt"
