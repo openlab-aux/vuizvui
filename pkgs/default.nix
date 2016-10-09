openlab-aux/vuizvui

commit e0abe1dbbda54c1f048b5d38df05e1a3289216a6

pkgs/profpatsch/display-infos: init
A script to display current time & battery.

pkgs/default.nix
@@ -13,6 +13,8 @@ let
     git-detach = callPackage ./git-detach { };
     grandpa = callPackage ./grandpa { };
     greybird-xfce-theme = callPackage ./greybird-xfce-theme { };
+    iec16022 = callPackage ./iec16022 { };
+    multi-iec16022 = callPackage ./multi_iec16022 { };
     jmtpfs = pkgs.jmtpfs.override {
       libmtp = pkgs.libmtp.overrideDerivation (old: {
         patches = old.patches or [] ++ [ ./mtp-jolla.patch ];
@@ -46,6 +48,9 @@ let
     openlab = pkgs.recurseIntoAttrs {
       gitit = callPackage ./openlab/gitit { hlib = pkgs.haskell.lib; };
       stackenblocken = callPackage ./openlab/stackenblocken {};
+    };
+    profpatsch = pkgs.recurseIntoAttrs {
+      display-infos = callPackage ./profpatsch/display-infos {};
     };
   };
 in pkgs // self

pkgs/profpatsch/display-infos/default.nix
@@ -1,0 +1,38 @@
+{ lib, runCommand, python3, libnotify }:
+
+let
+  name = "display-infos-0.1.0";
+  script = builtins.toFile (name + "-script") ''
+    #!@python3@
+
+    import sys
+    import glob
+    import subprocess as sub
+    import os.path as path
+    import statistics as st
+
+    full = 0
+    now  = 0
+    for bat in glob.iglob("/sys/class/power_supply/BAT*"):
+        def readint(fn):
+            with open(fn, 'r') as f:
+                return int(f.read())
+
+        full += readint(path.join(bat, "energy_full"))
+        now  += readint(path.join(bat, "energy_now" ))
+
+    bat = round( now/full, 2 )
+    date = sub.run(["date", "+%d.%m. %a %T"], stdout=sub.PIPE).stdout.strip().decode()
+    notify = "BAT: {}% | {}".format(int(bat*100), date)
+    sub.run(["@notify-send@", notify])
+  '';
+
+in
+  with lib; runCommand "display-infos" {
+    meta.description = "Script to display time & battery";
+  } ''
+    substitute ${script} script \
+      --replace "@python3@" "${getBin python3}/bin/python3" \
+      --replace "@notify-send@" "${getBin libnotify}/bin/notify-send"
+    install -D script $out/bin/display-infos
+  ''

commit 46b657bbf5f579716e192c256dd19eb9f1992ffe

pkgs: Temporarily disable (multi-)?iec16022
Regression introduced in e0abe1dbbda54c1f048b5d38df05e1a3289216a6.

This causes the evaluation to fail since 5 days now, so I'm commenting
it out until @Profpatsch has either provided the expressions or removed
the callPackage references.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>
Cc: @Profpatsch

pkgs/default.nix
@@ -13,8 +13,9 @@ let
     git-detach = callPackage ./git-detach { };
     grandpa = callPackage ./grandpa { };
     greybird-xfce-theme = callPackage ./greybird-xfce-theme { };
-    iec16022 = callPackage ./iec16022 { };
-    multi-iec16022 = callPackage ./multi_iec16022 { };
+    # TODO: Profpatsch needs to include these files!
+    #iec16022 = callPackage ./iec16022 { };
+    #multi-iec16022 = callPackage ./multi_iec16022 { };
     jmtpfs = pkgs.jmtpfs.override {
       libmtp = pkgs.libmtp.overrideDerivation (old: {
         patches = old.patches or [] ++ [ ./mtp-jolla.patch ];