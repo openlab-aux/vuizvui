225ed17457fae8e4a1aa46de1d187177753a1141
aszlig <aszlig@redmoonstudios.org>
2017-01-12 08:14:24 +0100

    aszlig/kernel: Fix up BFQ patch for kernel 4.10
    First of all, using .patch from the GitHub compare view is not going to
    work because the concatenated diffs are in reverse order for use within
    "git apply".
    
    And the second thing why it's not working is that the patch has a hunk
    that changes the version in Makefile to add an extra version -rc1-bfq,
    which would only apply for kernel 4.10-rc1 but not for subsequent
    release canidates.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- modules/user/aszlig/system/kernel.nix
+++ modules/user/aszlig/system/kernel.nix
@@ -13,8 +13,12 @@
           name = "bfq-v8r7.patch";
           url = "https://github.com/linusw/linux-bfq/compare/"
               + "238d1d0f79f619d75c2cc741d6770fb0986aef24..."
-              + "eaa6f84b3c10985b01a1d7ff1a77fb5f43df714d.patch";
-          sha256 = "1ir9m5smd82maipy7jjs4vmfsz18qz51gz3zy9gmmiaznm0i9llx";
+              + "eaa6f84b3c10985b01a1d7ff1a77fb5f43df714d.diff";
+          postFetch = ''
+            "${pkgs.patchutils}/bin/filterdiff" -x '[ab]/Makefile' "$out" > tmp
+            mv tmp "$out"
+          '';
+          sha256 = "1q108w6w5l4qdnicp01c4kb4rdgs2q34vh0gb5436hsj5diai69q";
         };
         extraConfig = (pkgs.vuizvui.kernel.bfqsched.extraConfig or "") + ''
           IOSCHED_BFQ y
