d1015532a6f021b19eae3037b9ed7a2e4719d68d
aszlig <aszlig@redmoonstudios.org>
2017-01-12 07:11:28 +0100

    aszlig/kernel: Update BFQ to v8r7 for 4.10-rc1
    The patch is from https://github.com/linusw/linux-bfq/tree/bfq-v8.
    
    It's a combined patch until the parent of the branch's head, because the
    latest commit is a work-in-progress commit.
    
    I have only tested evaluation and didn't test whether the patch actually
    applies yet, because the machines currently using the BFQ patch are
    broken because the old BFQ patch no longer applies for kernel 4.10.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- modules/user/aszlig/system/kernel.nix
+++ modules/user/aszlig/system/kernel.nix
@@ -7,14 +7,22 @@
 
   config = lib.mkIf config.vuizvui.user.aszlig.system.kernel.enable {
     boot = {
-      kernelPatches = lib.singleton (pkgs.vuizvui.kernel.bfqsched // {
+      kernelPatches = lib.singleton {
+        name = "bfq-v8r7";
+        patch = pkgs.fetchpatch {
+          name = "bfq-v8r7.patch";
+          url = "https://github.com/linusw/linux-bfq/compare/"
+              + "238d1d0f79f619d75c2cc741d6770fb0986aef24..."
+              + "eaa6f84b3c10985b01a1d7ff1a77fb5f43df714d.patch";
+          sha256 = "1ir9m5smd82maipy7jjs4vmfsz18qz51gz3zy9gmmiaznm0i9llx";
+        };
         extraConfig = (pkgs.vuizvui.kernel.bfqsched.extraConfig or "") + ''
           IOSCHED_BFQ y
           DEFAULT_BFQ y
           DEFAULT_CFQ n
           DEFAULT_IOSCHED bfq
         '';
-      });
+      };
 
       kernelPackages = let
         inherit (lib) take splitString replaceStrings;
