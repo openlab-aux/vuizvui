0ec2cd45ff4d6278344071e81ab742d2d157cb81
sternenseemann <git@lukasepple.de>
2017-03-07 15:43:30 +0100

    pkgs/spacecookie: 0.1.0.0 -> 0.2.0.0

--- pkgs/sternenseemann/spacecookie/default.nix
+++ pkgs/sternenseemann/spacecookie/default.nix
@@ -1,24 +1,24 @@
 { mkDerivation, aeson, attoparsec, base, bytestring, containers
-, directory, fetchgit, filepath, hxt-unicode, mtl, network, stdenv
-, transformers, unix
+, directory, fetchgit, filepath, hxt-unicode, mtl, socket, stdenv
+, transformers, unix, fast-logger
 }:
 mkDerivation {
   pname = "spacecookie";
-  version = "0.1.0.0";
+  version = "0.2.0.0";
   src = fetchgit {
     url = "https://github.com/sternenseemann/spacecookie";
-    sha256 = "0qxrrjjlrjkws978h10cwph5cskaa85p1ndxc9phcqqw79ib4xmi";
-    rev = "1d654575c2b77102f876f98fbea7708ae87c9583";
+    sha256 = "1hsanzhxg29alc49rlfny778afn6xznjamnqd8m7a4ynj3iswg42";
+    rev = "39001d0f70891caab774376a48f61b91a66d9f30";
   };
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     attoparsec base bytestring containers directory filepath
-    hxt-unicode mtl network transformers unix
+    hxt-unicode mtl socket transformers unix fast-logger
   ];
   executableHaskellDepends = [
     aeson attoparsec base bytestring containers directory filepath mtl
-    network transformers unix
+    transformers unix
   ];
   description = "gopher server daemon";
   license = stdenv.lib.licenses.gpl3;
