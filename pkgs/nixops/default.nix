a2152f28006cb92b349f404112a50caca5d89ccf
aszlig <aszlig@redmoonstudios.org>
2017-04-19 07:11:31 +0200

    nixops: Update to latest upstream master
    We can now safely drop the patch for NixOS/nixops#508, because it has
    long been applied to master and also partially reverted as well in
    NixOS/nixops@4bbceb294fc50146a4fd47352fba786ee1af6654.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/nixops/default.nix
+++ pkgs/nixops/default.nix
@@ -1,8 +1,8 @@
 { stdenv, fetchFromGitHub, fetchpatch, git }:
 
 let
-  rev = "06e3a4b961b80ada9293632d0ec62493c0740282";
-  sha256 = "0w2b1m9z42g56mw2g2sw5izb7gn0nhlp3v8c96m65r121gdil9fj";
+  rev = "bbf9a792d06c9a60c74dabe2937a9dfda9bff8f7";
+  sha256 = "0a1mx0ngp0zg65r1rx99rina4wbfjyzrziw2z9788v629j58p4jd";
 
   master = stdenv.mkDerivation rec {
     name = "nixops-upstream-patched";
@@ -15,15 +15,9 @@ let
 
     phases = [ "unpackPhase" "patchPhase" "installPhase" ];
 
-    patches = stdenv.lib.singleton (fetchpatch {
-      url = "https://github.com/NixOS/nixops/pull/508.patch";
-      sha256 = "1x0cjjpw7ykavnkz1ndxlkcymp9vx6lkyqhbf9wz5jvplqhpb9z7";
-    });
-
     postPatch = ''
       sed -i -re 's!<nixpkgs([^>]*)>!${import ../../nixpkgs-path.nix}\1!g' \
         release.nix doc/manual/default.nix doc/manual/resource.nix
-      sed -i -e 's/\<sqlite3\>//' release.nix
     '';
 
     installPhase = ''
