028a94c628876c97d048ce5235b89b515f747422
aszlig <aszlig@redmoonstudios.org>
2017-04-19 05:56:23 +0200

    tomahawk: Update to latest master and fix build
    I'm no longer using Tomahawk at the moment, so I'm not very ambitous to
    get this build running in a *clean* way. So this is more or less a "get
    this shit to build no matter what" fix rather than investigating why it
    really fails to discover the dependencies.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/tomahawk/default.nix
+++ pkgs/tomahawk/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchFromGitHub, fetchurl, cmake, pkgconfig, boost, gnutls
 , libechonest, liblastfm, lucenepp, kdeFrameworks, qt5, libsForQt5, sparsehash
-, taglib, websocketpp, ffmpeg_2, v4l_utils
+, taglib, websocketpp, ffmpeg_2, v4l_utils, libtasn1, libidn, p11_kit
 
 , enableXMPP      ? true,  libjreen     ? null
 , enableKDE       ? false, kdelibs      ? null
@@ -52,9 +52,14 @@ in stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "tomahawk-player";
     repo = "tomahawk";
-    rev = "09e9a6e960b7cc29018721075d82ba994dc87ab6";
-    sha256 = "0cvfnf996alvxx010bj7pf6xk14k9ig33ahjp6zyz92bj8136c1w";
+    rev = "97a407f83701ee2343e4826043c311c03fe5675b";
+    sha256 = "1palfsn8kz8q0xb19xhiwy3n993q2kifz9dy1ry5cnys7706l9b8";
   };
+
+  postPatch = ''
+    sed -i -e '/set(QUAZIP_LIB_VERSION_SUFFIX/d' CMakeModules/FindQuaZip.cmake
+    sed -i -e 's,quazip5/,quazip/,' src/libtomahawk/utils/TomahawkUtils.cpp
+  '';
 
   cmakeFlags = [
     "-DLUCENEPP_INCLUDE_DIR=${lucenepp}/include"
@@ -64,6 +69,7 @@ in stdenv.mkDerivation rec {
   nativeBuildInputs = [ cmake pkgconfig kdeFrameworks.extra-cmake-modules ];
 
   buildInputs = [
+    libtasn1 libidn p11_kit
     libsForQt5.attica libsForQt5.qca-qt5 libsForQt5.qtkeychain libsForQt5.quazip
     libsForQt5.vlc qt5.qtbase qt5.qtsvg qt5.qttools qt5.qtwebkit qt5.qtx11extras
   ] ++ map useQT5 [ liblastfm ] ++ [
