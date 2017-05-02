openlab-aux/vuizvui

commit 736e0da1e251df836b1669ffee80cecfd6bbf0e9

machines/katara: add hledger

machines/profpatsch/katara.nix
@@ -148,6 +148,7 @@ in {
         myPkgs.saneGhci      # <s>Glorious</s>Glasgow Haskell Compiler, mostly for ghci
         gimp                 # graphics
         gmpc                 # mpd client and best music player interface in the world
+        hledger              # plain text accounting
         inkscape             # vector graphics
         libreoffice          # a giant ball of C++, that sometimes helps with proprietary shitformats
         lilyterm-git         # terminal emulator, best one around

commit 36cfeeaed596eb9d22780721efb58710a37dfb21

machines/katara: Fix attribute for hledger
Evaluation error introduced by 736e0da1e251df836b1669ffee80cecfd6bbf0e9.

The attribute "hledger" isn't a top-level pkgs attribute but resides
within haskellPackages.

This fixes evaluation, as tested by:

nix-instantiate machines -A profpatsch.katara.build

Signed-off-by: aszlig <aszlig@redmoonstudios.org>
Cc: @Profpatsch

machines/profpatsch/katara.nix
@@ -148,7 +148,7 @@ in {
         myPkgs.saneGhci      # <s>Glorious</s>Glasgow Haskell Compiler, mostly for ghci
         gimp                 # graphics
         gmpc                 # mpd client and best music player interface in the world
-        hledger              # plain text accounting
+        haskellPackages.hledger # plain text accounting
         inkscape             # vector graphics
         libreoffice          # a giant ball of C++, that sometimes helps with proprietary shitformats
         lilyterm-git         # terminal emulator, best one around