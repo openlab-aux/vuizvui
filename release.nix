4ba315934a9d3727a0bc0ff392c706d9bda57ce7
devhell <^@regexmail.net>
2017-06-21 12:28:21 +0000

    release.nix: Reenable titan
    Titan has been reborn. Bigger, stronger, faster.

--- release.nix
+++ release.nix
@@ -8,6 +8,6 @@ in
 
 {
   skunkworks = (import <nixpkgs/nixos> { configuration = ./entry-skunkworks.nix; }).system;
-  #titan = (import <nixpkgs/nixos> { configuration = ./entry-titan.nix; }).system;
+  titan = (import <nixpkgs/nixos> { configuration = ./entry-titan.nix; }).system;
   eris = (import <nixpkgs/nixos> { configuration = ./entry-eris.nix; }).system;
 }
