a911f4231c6c0a4c7d7621873ae452a37d12faf4
aszlig <aszlig@redmoonstudios.org>
2017-06-22 01:45:51 +0200

    devhell: Move users_dev.nix into base profile
    IMHO this module is really too small to factor it out into its own
    profile module, so I'm putting it into the base profile as it's really
    common among all of devhell's machines.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>
    Cc: @devhell

--- modules/user/devhell/profiles/base.nix
+++ modules/user/devhell/profiles/base.nix
@@ -46,6 +46,13 @@ in {
 
     networking.firewall.enable = false;
 
+    users.users.dev = {
+      isNormalUser = true;
+      extraGroups = [ "vboxusers" "wheel" "mpd" "networkmanager" "libvirtd" ];
+      uid = 1000;
+      shell = "${pkgs.zsh}/bin/zsh";
+    };
+
     programs = {
       ssh = {
         startAgent = false;
