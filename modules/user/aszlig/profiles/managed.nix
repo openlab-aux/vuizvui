224a63100f6233fda762c72818cad57173411802
aszlig <aszlig@redmoonstudios.org>
2017-07-26 21:25:59 +0200

    profiles/managed: Add patch for networkd IPv6 fix
    This is exactly the same patch/implementation as I've added for my
    machines in 85f3d5340e53e2624f65248740ed04cb1f5e94e9.
    
    I wanted to delay this a bit until I have tested it by myself, which I
    have by now.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- modules/user/aszlig/profiles/managed.nix
+++ modules/user/aszlig/profiles/managed.nix
@@ -21,6 +21,17 @@ in {
 
     environment.systemPackages = [ pkgs.simple-scan ];
 
+    nixpkgs.overlays = lib.singleton (lib.const (super: {
+      # https://github.com/NixOS/systemd/pull/12
+      systemd = super.systemd.overrideDerivation (drv: {
+        patches = (drv.patches or []) ++ lib.singleton (pkgs.fetchpatch {
+          url = "https://github.com/NixOS/systemd/commit/"
+              + "6554550f35a7976f9110aff94743d3576d5f02dd.patch";
+          sha256 = "07l6wx0pb7pvjx8n9j0rwv5n260crbrfg5rh56l5nfan6biv81cl";
+        });
+      }) // { inherit (super.systemd) udev; };
+    }));
+
     # Printing for the most common printers among the managed machines.
     services.printing.enable = true;
     services.printing.drivers = [
