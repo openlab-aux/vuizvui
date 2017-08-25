openlab-aux/vuizvui

commit 5990a4d62f2d3146ddd03ec2461d45b2102e5f42

devhell: Mergo overrides/pkgs into vuizvui
This introduces another profile module called "packages", which contains
all the package configuration (including overrides) of all the machines
in the devhell namespace.

The machine-specific configuration is now merged into the machine
configurations the same way as we've done previously with the services.

One major difference here is that the haskellPackages workaround is no
longer needed in the package configuration, as it is handled by vuizvui.

Tested this by evaluating all machines and all evaluations succeeded.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>
Cc: @devhell

modules/user/devhell/profiles/packages.nix
@@ -1,0 +1,321 @@
+{ config, pkgs, lib, ... }:
+
+let
+  cfg = config.vuizvui.user.devhell.profiles.packages;
+
+in {
+  options.vuizvui.user.devhell.profiles.packages = {
+    enable = lib.mkEnableOption "Packages profile for devhell";
+  };
+
+  config = lib.mkIf cfg.enable {
+    nixpkgs.config = {
+      pulseaudio = true;
+
+      allowUnfree = true;
+
+      systemd = {
+        enableKDbus = true;
+      };
+
+      conky = {
+        weatherMetarSupport = true;
+        mpdSupport = true;
+        wirelessSupport = true;
+        x11Support = false;
+      };
+
+      firefox = {
+        enableGTK3 = true;
+        enableOfficalBranding = true;
+      };
+
+      virtualbox = {
+        enableExtensionPack = true;
+      };
+
+      mpv = {
+        youtubeSupport = true;
+      };
+
+      nixpkgs.config.packageOverrides = super: {
+        gnupg = super.gnupg21;
+
+        ncmpcpp = super.ncmpcpp.override {
+          visualizerSupport = true;
+          clockSupport = true;
+        };
+
+        sox = super.sox.override {
+          enableLame = true;
+        };
+      };
+    };
+
+    environment.systemPackages = with pkgs; [
+      #attic
+      #emacs
+      #gitAndTools.git-annex
+      #ipfs
+      #john
+      #lxappearance
+      #sleuthkit
+      #texmacs
+      #tribler
+      #vimiv
+      #zotero
+      abcde
+      abook
+      accountsservice
+      antiword
+      apg
+      arandr
+      arc-theme
+      ascii
+      aspell
+      aspellDicts.de
+      aspellDicts.en
+      atftp
+      atom
+      audacity
+      axel
+      bc
+      beets
+      biber
+      bind
+      binutils
+      brotli
+      bup
+      cacert
+      cataclysm-dda
+      cava
+      ccrypt
+      chromaprint
+      chromium
+      cifs_utils
+      cipherscan
+      cmake
+      cmatrix
+      colordiff
+      compton
+      conky
+      cryptsetup
+      ctodo
+      cuetools
+      darkstat
+      dcfldd
+      ddrescue
+      dhcping
+      dmenu
+      dmidecode
+      docker
+      dos2unix
+      dosbox
+      duff
+      dynamic-colors
+      e2fsprogs
+      easytag
+      electrum
+      enhanced-ctorrent
+      ethtool
+      evince
+      fbida
+      fdupes
+      feh
+      ffmpeg-full
+      figlet
+      file
+      firefox
+      flac
+      foremost
+      freerdpUnstable
+      fuse_exfat
+      gajim
+      gcc
+      gdb
+      ghostscript
+      gimp
+      gitAndTools.git-extras
+      gitAndTools.git-remote-hg
+      gitAndTools.git2cl
+      gitAndTools.gitFastExport
+      gitAndTools.gitFull
+      gitAndTools.gitRemoteGcrypt
+      gitAndTools.gitSVN
+      gitAndTools.gitflow
+      gitAndTools.svn2git
+      gitAndTools.tig
+      glxinfo
+      gnome3.dconf
+      gnome3.defaultIconTheme
+      gnome3.gnome_themes_standard
+      gnufdisk
+      gnupg
+      gnupg1compat
+      gource
+      gparted
+      gpgme
+      gpicview
+      gptfdisk
+      graphviz
+      gstreamer
+      handbrake
+      hdparm
+      heimdall
+      hexedit
+      hplipWithPlugin
+      htop
+      i3lock
+      i3status
+      icedtea_web
+      iftop
+      imagemagick
+      impressive
+      inkscape
+      iotop
+      iptraf-ng
+      ipv6calc
+      jfsutils
+      jwhois
+      keepassx
+      keepassx-community
+      keepassx2
+      keybase
+      kpcli
+      lftp
+      libarchive
+      libreoffice
+      lm_sensors
+      lsof
+      lxc
+      lynx
+      macchanger
+      manpages
+      mc
+      mcabber
+      mdp
+      mediainfo
+      mkvtoolnix
+      mmv
+      monkeysAudio
+      mono
+      monodevelop
+      mosh
+      mp3gain
+      mpc_cli
+      mpv
+      mtr
+      ncdu
+      ncmpcpp
+      neovim
+      nethack
+      nethogs
+      netkittftp
+      netrw
+      netsniff-ng
+      nitrogen
+      nix-prefetch-scripts
+      nix-repl
+      nixops
+      nload
+      nmap
+      ntfs3g
+      ntfsprogs
+      ntopng
+      numix-icon-theme
+      obnam
+      openssl
+      p7zip
+      pandoc
+      paperkey
+      pass
+      pasystray
+      pavucontrol
+      pciutils
+      picard
+      posix_man_pages
+      powertop
+      profanity
+      profile-cleaner
+      profile-sync-daemon
+      pv
+      python
+      python2
+      python3
+      python34Packages.hovercraft
+      pythonPackages.jrnl
+      pythonPackages.livestreamer
+      pythonPackages.rainbowstream
+      qemu
+      qrencode
+      recode
+      reiserfsprogs
+      rofi
+      rsync
+      ruby
+      safecopy
+      screen
+      scrot
+      shntool
+      silver-searcher
+      smartmontools
+      sox
+      speedtest-cli
+      spek
+      ssdeep
+      stow
+      strace
+      surfraw
+      taskwarrior
+      telnet
+      testdisk
+      texlive.combined.scheme-small
+      tftp-hpa
+      tldr
+      tmux
+      toilet
+      tomahawk
+      toxic
+      transcode
+      transgui
+      transmission_remote_gtk
+      tree
+      tty-clock
+      udevil
+      units
+      unrar
+      unzip
+      valgrind
+      vanilla-dmz
+      vim_configurable
+      virt-viewer
+      virtinst
+      virtmanager
+      vit
+      vivaldi
+      vlc
+      vlock
+      vnstat
+      vorbisTools
+      vorbisgain
+      w3m
+      wavpack
+      weechat
+      wget
+      which
+      wipe
+      wireshark
+      xfsprogs
+      xlibs.xev
+      xmpp-client
+      xpdf
+      xpra
+      xscreensaver
+      youtube-dl
+      zathura
+      zbar
+      zip
+      zsync
+    ];
+  };
+}

devhell/overrides/overrides.nix
@@ -1,25 +1,0 @@
-{ config, pkgs, lib, ... }:
-
-with lib;
-
-{
-  nixpkgs.config.packageOverrides = pkgs: {
-    gnupg = pkgs.gnupg21;
-
-    ncmpcpp = pkgs.ncmpcpp.override {
-      visualizerSupport = true;
-      clockSupport = true;
-    };
-
-    sox = pkgs.sox.override {
-      enableLame = true;
-    };
-
-    # XXX: Very ugly workaround see https://github.com/openlab-aux/vuizvui/commit/a93b7583084ff9084d73873d80d8dc428406593c
-    haskellPackages = pkgs.haskellPackages.override {
-      ghc = pkgs.haskellPackages.ghc.overrideDerivation (const {
-        forceRebuild = true;
-      });
-    };
-  };
-}

devhell/pkgs/pkgs_common.nix
@@ -1,299 +1,0 @@
-{ config, pkgs, lib, ... }:
-
-{
-  nixpkgs.config = {
-    pulseaudio = true;
-
-    allowUnfree = true;
-
-    systemd = {
-      enableKDbus = true;
-    };
-
-    conky = {
-      weatherMetarSupport = true;
-      mpdSupport = true;
-      wirelessSupport = true;
-      x11Support = false;
-    };
-
-    firefox = {
-      enableGTK3 = true;
-      enableOfficalBranding = true;
-    };
-
-    virtualbox = {
-      enableExtensionPack = true;
-    };
-
-    mpv = {
-      youtubeSupport = true;
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    #attic
-    #emacs
-    #gitAndTools.git-annex
-    #ipfs
-    #john
-    #lxappearance
-    #sleuthkit
-    #texmacs
-    #tribler
-    #vimiv
-    #zotero
-    abcde
-    abook
-    accountsservice
-    antiword
-    apg
-    arandr
-    arc-theme
-    ascii
-    aspell
-    aspellDicts.de
-    aspellDicts.en
-    atftp
-    atom
-    audacity
-    axel
-    bc
-    beets
-    biber
-    bind
-    binutils
-    brotli
-    bup
-    cacert
-    cataclysm-dda
-    cava
-    ccrypt
-    chromaprint
-    chromium
-    cifs_utils
-    cipherscan
-    cmake
-    cmatrix
-    colordiff
-    compton
-    conky
-    cryptsetup
-    ctodo
-    cuetools
-    darkstat
-    dcfldd
-    ddrescue
-    dhcping
-    dmenu
-    dmidecode
-    docker
-    dos2unix
-    dosbox
-    duff
-    dynamic-colors
-    e2fsprogs
-    easytag
-    electrum
-    enhanced-ctorrent
-    ethtool
-    evince
-    fbida
-    fdupes
-    feh
-    ffmpeg-full
-    figlet
-    file
-    firefox
-    flac
-    foremost
-    freerdpUnstable
-    fuse_exfat
-    gajim
-    gcc
-    gdb
-    ghostscript
-    gimp
-    gitAndTools.git-extras
-    gitAndTools.git-remote-hg
-    gitAndTools.git2cl
-    gitAndTools.gitFastExport
-    gitAndTools.gitFull
-    gitAndTools.gitRemoteGcrypt
-    gitAndTools.gitSVN
-    gitAndTools.gitflow
-    gitAndTools.svn2git
-    gitAndTools.tig
-    glxinfo
-    gnome3.dconf
-    gnome3.defaultIconTheme
-    gnome3.gnome_themes_standard
-    gnufdisk
-    gnupg
-    gnupg1compat
-    gource
-    gparted
-    gpgme
-    gpicview
-    gptfdisk
-    graphviz
-    gstreamer
-    handbrake
-    hdparm
-    heimdall
-    hexedit
-    hplipWithPlugin
-    htop
-    i3lock
-    i3status
-    icedtea_web
-    iftop
-    imagemagick
-    impressive
-    inkscape
-    iotop
-    iptraf-ng
-    ipv6calc
-    jfsutils
-    jwhois
-    keepassx
-    keepassx-community
-    keepassx2
-    keybase
-    kpcli
-    lftp
-    libarchive
-    libreoffice
-    lm_sensors
-    lsof
-    lxc
-    lynx
-    macchanger
-    manpages
-    mc
-    mcabber
-    mdp
-    mediainfo
-    mkvtoolnix
-    mmv
-    monkeysAudio
-    mono
-    monodevelop
-    mosh
-    mp3gain
-    mpc_cli
-    mpv
-    mtr
-    ncdu
-    ncmpcpp
-    neovim
-    nethack
-    nethogs
-    netkittftp
-    netrw
-    netsniff-ng
-    nitrogen
-    nix-prefetch-scripts
-    nix-repl
-    nixops
-    nload
-    nmap
-    ntfs3g
-    ntfsprogs
-    ntopng
-    numix-icon-theme
-    obnam
-    openssl
-    p7zip
-    pandoc
-    paperkey
-    pass
-    pasystray
-    pavucontrol
-    pciutils
-    picard
-    posix_man_pages
-    powertop
-    profanity
-    profile-cleaner
-    profile-sync-daemon
-    pv
-    python
-    python2
-    python3
-    python34Packages.hovercraft
-    pythonPackages.jrnl
-    pythonPackages.livestreamer
-    pythonPackages.rainbowstream
-    qemu
-    qrencode
-    recode
-    reiserfsprogs
-    rofi
-    rsync
-    ruby
-    safecopy
-    screen
-    scrot
-    shntool
-    silver-searcher
-    smartmontools
-    sox
-    speedtest-cli
-    spek
-    ssdeep
-    stow
-    strace
-    surfraw
-    taskwarrior
-    telnet
-    testdisk
-    texlive.combined.scheme-small
-    tftp-hpa
-    tldr
-    tmux
-    toilet
-    tomahawk
-    toxic
-    transcode
-    transgui
-    transmission_remote_gtk
-    tree
-    tty-clock
-    udevil
-    units
-    unrar
-    unzip
-    valgrind
-    vanilla-dmz
-    vim_configurable
-    virt-viewer
-    virtinst
-    virtmanager
-    vit
-    vivaldi
-    vlc
-    vlock
-    vnstat
-    vorbisTools
-    vorbisgain
-    w3m
-    wavpack
-    weechat
-    wget
-    which
-    wipe
-    wireshark
-    xfsprogs
-    xlibs.xev
-    xmpp-client
-    xpdf
-    xpra
-    xscreensaver
-    youtube-dl
-    zathura
-    zbar
-    zip
-    zsync
-  ];
-}

devhell/pkgs/pkgs_eris.nix
@@ -1,29 +1,0 @@
-{ config, pkgs, lib, ... }:
-
-{
-  imports = [ ./pkgs_common.nix ];
-
-  nixpkgs.config.mpv = {
-    vaapiSupport = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    terminator
-    claws-mail
-    aircrackng
-    horst
-    kismet
-    minicom
-    networkmanagerapplet
-    pamixer
-    pmtools
-    pmutils
-    reaverwps
-    snort
-    wavemon
-    xbindkeys
-    xorg.xbacklight
-    thunderbird
-    iw
-  ];
-}

devhell/pkgs/pkgs_skunkworks.nix
@@ -1,31 +1,0 @@
-{ config, pkgs, lib, ... }:
-
-{
-  imports = [ ./pkgs_common.nix ];
-
-  nixpkgs.config = {
-    chromium = {
-      enablePepperFlash = true;
-    };
-
-    mpv = {
-      bs2bSupport = true;
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    abook
-    canto-curses
-    cli-visualizer
-    cmus
-    #ipfs
-    handbrake
-    hplip
-    mutt-with-sidebar
-    nzbget
-    #scummvm
-    slrn
-    twister
-    urlview
-  ];
-}

devhell/pkgs/pkgs_titan.nix
@@ -1,48 +1,0 @@
-{ config, pkgs, lib, ... }:
-
-{
-  imports = [ ./pkgs_common.nix ];
-
-  nixpkgs.config.mpv = {
-    vaapiSupport = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    #cura
-    #openjdk8
-    #skype
-    aircrackng
-    calibre
-    cdrtools
-    claws-mail
-    connmanui
-    dvdplusrwtools
-    glxinfo
-    horst
-    ipmitool
-    ipmiutil
-    ipmiview
-    kismet
-    libva
-    libvdpau-va-gl
-    minicom
-    msmtp
-    mutt
-    networkmanagerapplet
-    notmuch
-    offlineimap
-    pamixer
-    pmtools
-    pmutils
-    pythonPackages.alot
-    reaverwps
-    snort
-    thunderbird
-    vaapiVdpau
-    vdpauinfo
-    wavemon
-    wirelesstools
-    xbindkeys
-    xorg.xbacklight
-  ];
-}

machines/devhell/eris.nix
@@ -199,4 +199,30 @@
       #DEVICES_TO_ENABLE_ON_WWAN_DISCONNECT=""
     '';
   };
+
+  #### Machine-specific packages configuration ####
+
+  vuizvui.user.devhell.profiles.packages.enable = true;
+
+  nixpkgs.config.mpv.vaapiSupport = true;
+
+  environment.systemPackages = with pkgs; [
+    terminator
+    claws-mail
+    aircrackng
+    horst
+    kismet
+    minicom
+    networkmanagerapplet
+    pamixer
+    pmtools
+    pmutils
+    reaverwps
+    snort
+    wavemon
+    xbindkeys
+    xorg.xbacklight
+    thunderbird
+    iw
+  ];
 }

machines/devhell/skunkworks.nix
@@ -243,4 +243,27 @@
       DISPLAY=:0.1 ${pkgs.windowmaker}/bin/wmaker &
     '';
   };
+
+  #### Machine-specific packages configuration ####
+
+  vuizvui.user.devhell.profiles.packages.enable = true;
+
+  nixpkgs.config.chromium.enablePepperFlash = true;
+  nixpkgs.config.mpv.bs2bSupport = true;
+
+  environment.systemPackages = with pkgs; [
+    abook
+    canto-curses
+    cli-visualizer
+    cmus
+    #ipfs
+    handbrake
+    hplip
+    mutt-with-sidebar
+    nzbget
+    #scummvm
+    slrn
+    twister
+    urlview
+  ];
 }

commit cab045f4f50a3e80eedbdf8953e10cd53f029b43

devhell/profiles: Fix package overrides
Regression introduced by 5990a4d62f2d3146ddd03ec2461d45b2102e5f42.

In this commit, I copied over the nixpkgs.config.packageOverrides
attribute, but didn't trim off the "nixpkgs.config", so the
packageOverrides attribute didn't get evaluated correctly.

While converting everything to overlays in 0feae9218e6f922366665e6515bc,
I noticed that for @devhell's profile the store paths were different. In
hindsight, this is pretty obvious given that overrides didn't apply *at*
*all* but with the move to nixpkgs.overlays they suddenly applied.

So this moves the overrides to overlays instead and drops the override
on gnupg, because gnupg is already gnupg21 in upstream <nixpkgs>.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>
Cc: @devhell

modules/user/devhell/profiles/packages.nix
@@ -9,6 +9,17 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
+    nixpkgs.overlays = lib.singleton (lib.const (super: {
+      ncmpcpp = super.ncmpcpp.override {
+        visualizerSupport = true;
+        clockSupport = true;
+      };
+
+      sox = super.sox.override {
+        enableLame = true;
+      };
+    }));
+
     nixpkgs.config = {
       pulseaudio = true;
 
@@ -36,19 +47,6 @@ in {
 
       mpv = {
         youtubeSupport = true;
-      };
-
-      nixpkgs.config.packageOverrides = super: {
-        gnupg = super.gnupg21;
-
-        ncmpcpp = super.ncmpcpp.override {
-          visualizerSupport = true;
-          clockSupport = true;
-        };
-
-        sox = super.sox.override {
-          enableLame = true;
-        };
       };
     };
 