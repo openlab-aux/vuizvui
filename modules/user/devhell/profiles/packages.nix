openlab-aux/vuizvui

commit 761856c84d8abae87aa1cd3b2a0f70f9f1bf4397

devhell/packages: Add glances

modules/user/devhell/profiles/packages.nix
@@ -143,6 +143,7 @@ in {
       gitAndTools.gitflow
       gitAndTools.svn2git
       gitAndTools.tig
+      glances
       glxinfo
       gnome3.dconf
       gnome3.defaultIconTheme

commit d3188f2051563bb077a0d54b9750be7e8307417b

devhell/packages: Fix attribute for glances
Eval error introduced by 761856c84d8abae87aa1cd3b2a0f70f9f1bf4397.

It's actually in pythonPackages.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>
Cc: @devhell

modules/user/devhell/profiles/packages.nix
@@ -143,7 +143,7 @@ in {
       gitAndTools.gitflow
       gitAndTools.svn2git
       gitAndTools.tig
-      glances
+      pythonPackages.glances
       glxinfo
       gnome3.dconf
       gnome3.defaultIconTheme