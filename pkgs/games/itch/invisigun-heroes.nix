58ea45433adaa030e70d604bb26d635f8c179b43
aszlig <aszlig@redmoonstudios.org>
2017-09-24 17:10:28 +0200

    games/invisigun-heroes: Update to version 1.5.50
    New features:
    
     * NET: Join & invite your Steam friends through the in-game lobby menu
            or Steam friends list
     * GAME: Experimental fallback for unsupported controllers & generic USB
             gamepads
     * GAME: Added an achievement for completing the training
     * UI: Your Steam nickname will show at the top left of the title screen
     * UI: New lobby chat message: "I'm going to spectate this match."
     * PREFS: Set your own data export path in the "config.json" file in the
              game directory
    
    Updates:
    
     * GAME: The shield is only lost when dealt actual damage (not via
             stuns, Yeti breath, etc)
     * GAME: Lower chance of character repetition when choosing random
             heroes
     * GAME: More obvious shocked/soft-stun effect and audio
     * GAME: Bubble Suit removed
     * NET: Online play requires the latest version if there's an update
            available
     * AETHER: Aether is immobile for 0.3s after a ground pound
     * AETHER: Aether's spam cooldown increased from 0.5s to 0.7s
     * AETHER: Aether's pound has a circular radius of 2 units from his
               center
     * AETHER: Pound hard stuns for 1.25s
     * AETHER: Pound pushback force is uniform
     * CARMEN: Destroyed block rubble remains for 3s instead of 0.5s
     * PROTEUS: Tagged players are slowed more (multiplier changed from
                0.65x to 0.45x)
     * PROTEUS: The Scanner Drone can't be self-destroyed while
                pulse-revealing players
     * IRIS: Spam allowed reduced from 3 to 2
     * EPI: Epi has an ability cooldown of 2s if the ghost is killed (no
            self-imposed cooldown)
     * UI: "Join Public Match" is now "Quick Match" with an icon update
     * UI: Searching for a public match in "Any" region is faster
     * UI: More fun title screen news
    
    Fixes
    
     * GAME: Fixed projectiles having no velocity if split while being
             reflected
     * GAME: Fixed controller lockup in lobby when switching from previously
             chosen player slots
     * GAME: Fixed perma-visibility after teleporting while revealed
     * GAME: Fixed disappearing revealer if it's pursuing a teleporting
             player
     * GAME: Fixed possibility that a Beast Tamer pit beast could spawn
             outside the map
     * AETHER: Aether no longer gets stunned if walking into a player that
               he pounded
     * EPI: Fixed real Epi teleporting to ghost Epi if inputs are triggered
            twice in a frame
     * UI: Fixed some lobby UI initialization before an online match is
           created
     * UI: Fixed edge cases that could cause a client to be stuck "waiting
           for host" in the lobby
     * UI: Fixed pause menu "Return to Lobby" button for Challenge Mode
     * UI: Fixed color & bloom values in field guide
    
    Full upstream change log:
    
    http://www.sombr.com/pub/SIG/tools/ReleaseNotes.php
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/games/itch/invisigun-heroes.nix
+++ pkgs/games/itch/invisigun-heroes.nix
@@ -3,13 +3,13 @@
 buildUnity rec {
   name = "invisigun-heroes";
   fullName = "Invisigun Heroes";
-  version = "1.5.30";
+  version = "1.5.50";
 
   src = fetchItch {
     name = "${name}-${version}.zip";
     gameId = 25561;
     uploadId = 208583;
     version = "v${version}";
-    sha256 = "07iskccdmygnx70naaa3fcac1ayrhmq82cypddsnihc3gkw7rwrd";
+    sha256 = "0gyk683sa8gkx5dl78jac905mhfv1kfjdslpczh2pm2fmd5n651x";
   };
 }
