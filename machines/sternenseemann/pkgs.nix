7998dac8b47b581314cef515948959a1eeb5f8a6
sternenseemann <git@lukasepple.de>
2017-10-17 18:05:35 +0200

    machines/fliewatuet: more texlive packages

--- machines/sternenseemann/pkgs.nix
+++ machines/sternenseemann/pkgs.nix
@@ -4,7 +4,7 @@ let
 
   mpv = pkgs.mpv.override { scripts = [ pkgs.mpvScripts.convert ]; };
 
-  texlive = with pkgs.texlive; combine { inherit scheme-medium minted units collection-bibtexextra wrapfig libertine enumitem dashrule; };
+  texlive = with pkgs.texlive; combine { inherit scheme-medium minted units collection-bibtexextra wrapfig libertine enumitem dashrule ifmtarg xstring; };
 
   urxvt = pkgs.rxvt_unicode-with-plugins.override { plugins = [ pkgs.urxvt_perls ]; };
 
