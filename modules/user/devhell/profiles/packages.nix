04787ae4295a816bc827bfc4d0bb7a9bd98075dc
devhell <devhell@mailfresser.de>
2017-10-23 19:54:19 +0100

    profiles/packages: Add polybar override
    I'm using i3 and mpd, so make them work in polybar.

--- modules/user/devhell/profiles/packages.nix
+++ modules/user/devhell/profiles/packages.nix
@@ -15,6 +15,11 @@ in {
         clockSupport = true;
       };
 
+      polybar = super.polybar.override {
+        i3Support = true;
+        mpdSupport = true;
+      };
+
       sox = super.sox.override {
         enableLame = true;
       };
