6ea9f3f1b66ad63051875627ef6ad58a0aa7a857
aszlig <aszlig@redmoonstudios.org>
2017-10-26 14:25:51 +0200

    games/invisigun-heroes: Update to version 1.5.70
    New features:
    
     * New powerup: Cluster Shot
     * New powerup: Guardian
    
    Updates:
    
     * GAME: Blizzards vary in intensity, with max intensity slighty reduced
     * MAPS: Adjusted Zone Control area on The Long Run
     * GFX: Updated frozen player visuals
     * GFX: More efficient pooling & performance for sprite trails (dash,
            jump, etc)
     * SELENE: Added a subtle jump trail
     * UI: Updated some sprites to reduce buzzing on contrast-sensitive
           monitors
     * UI: Updated discord URL to the verified official address:
           discord.gg/invisigun
     * UI: Minor update to Epi's character image
    
    Fixes:
    
     * GAME: Fixed being able to act on an already-existing shot after
             picking up Split Shot
     * GAME: Fixed resistance & diagonal shot interactions
     * GAME: More reliable slow-motion timing for end-of-round moments
     * AI: Fixed several path-finding errors for bots
     * AI: Fixed bots staying in place momentarily while soft-stunned
     * SELENE: Fixed buffered shots still firing when landing on another
               player
     * RONIN: Slashing a player shouldn't remove their shield
     * MAPS: Fixed asymmetrical issue on Snowergrown
    
    Full upstream change log:
    
    https://s3-us-west-1.amazonaws.com/sombr-invisigun/messages/ReleaseNotes.txt
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/games/itch/invisigun-heroes.nix
+++ pkgs/games/itch/invisigun-heroes.nix
@@ -3,13 +3,13 @@
 buildUnity rec {
   name = "invisigun-heroes";
   fullName = "Invisigun Heroes";
-  version = "1.5.50";
+  version = "1.5.70";
 
   src = fetchItch {
     name = "${name}-${version}.zip";
     gameId = 25561;
     uploadId = 208583;
     version = "v${version}";
-    sha256 = "0gyk683sa8gkx5dl78jac905mhfv1kfjdslpczh2pm2fmd5n651x";
+    sha256 = "149g3bsgyqal3m1h2rh5h0m3dc39ky5n81y8plx3xg9r2b1sfhsn";
   };
 }
