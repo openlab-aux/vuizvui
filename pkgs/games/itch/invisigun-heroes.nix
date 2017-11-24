2af3b6a0891d18f2a07f36e54e61047e57e31d32
aszlig <aszlig@nix.build>
2017-11-24 02:24:57 +0100

    games/invisigun-heroes: Add missing sandbox path
    For replays and custom phrases, Invisigun Heroes writes them to
    "$HOME/Invisigun Heroes", so we need to bind-mount that directory to the
    chroot as well.
    
    Signed-off-by: aszlig <aszlig@nix.build>
    Cc: @Profpatsch

--- pkgs/games/itch/invisigun-heroes.nix
+++ pkgs/games/itch/invisigun-heroes.nix
@@ -13,4 +13,6 @@ buildUnity rec {
     version = "v${version}";
     sha256 = "149g3bsgyqal3m1h2rh5h0m3dc39ky5n81y8plx3xg9r2b1sfhsn";
   };
+
+  sandbox.paths.required = [ "$HOME/Invisigun Heroes" ];
 }
