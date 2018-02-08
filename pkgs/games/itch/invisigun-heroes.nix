df76a5c2d09709ba404c65dd361e59bba1a6be37
aszlig <aszlig@nix.build>
2018-02-08 20:11:10 +0100

    games/invisigun-heroes: Update to version 1.6.15
    New features:
    
     * New Cerebrus map: Back to the Future
    
    Updates:
    
     * RONIN: Slashed players will incur 0.25s of hard stun / push back, and
              0.35s of soft stun
     * PROTEUS: Scanner Drone won't continue to shock dead players
     * PROTEUS: Next Scanner Drone has a 3s cooldown from the time the last
                was destroyed (if shot down)
     * CRONUS: Drop target movement speed increased from 0.17s to 0.15s per
               tile
     * CRONUS: Bomb hit time & lingering fire increased from 1s to 1.5s
     * AETHER: Ground pound hit window is active for 0.25s instead of 1
               frame
     * GAME: Arx-515 teleporters are disabled for 1.25s while being used
     * GAME: Reduced visibility time after firing from 0.75s to 0.5s
     * GAME: Slightly reduced replay recording memory usage
     * GAME: Improved presentation of players visible due to Revealer
             powerup
     * GAME: Players are snapped to the nearest tile when firing
             perpendicular to movement
     * GFX: Removed FX when revealer ends, which had the potential to reveal
            real Epi's position
     * GFX: Clarified readability of some boundaries on Aqueduct
     * GFX: Powerup parachute drops are slightly transparent until near the
            ground
     * UI: Hero Profile demos properly show ability cooldown indicators
    
    Fixes:
    
     * EPI: Fixed cluster shots from ghost Epi breaking shields
     * CRONUS: Fixed target disappearing on clients after attempting a drop
               over a disallowed area
     * CRONUS: Better drop target local simulation and feel for remote
               clients
     * MAPS: Added missing fence posts around holes on Conundrum
     * GAME: Slightly increased physics step rate to prevent some
             fast-moving collision misses
     * GAME: Fixed bumping builder blocks destruction after the reflection
             shield is already gone
     * GAME: Fixed potential to get shot in your original spot after
             teleporting online
     * GFX: Fixed projectile collision visuals' positions due to high-speed
            physics
     * GFX: Fixed projectile collision visuals' positions on network clients
     * GFX: Fixed exported GIF speeds due to vsync differences
     * UI: Fixed occasional disappearing in-match pause menu
     * UI: Fixed Proteus hero profile demo
     * UI: Fixed insane speed when changing controller setup types using
           analog sticks
     * PREFS: custom config file will no longer be reset during updates
              (after this release)
     * PREFS: custom config file has option to disable fullscreen FX for
              strobe-sensitive players
    
    Full upstream change log:
    
    https://s3-us-west-1.amazonaws.com/sombr-invisigun/messages/ReleaseNotes.txt
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/games/itch/invisigun-heroes.nix
+++ pkgs/games/itch/invisigun-heroes.nix
@@ -4,14 +4,14 @@ buildUnity rec {
   name = "invisigun-heroes";
   fullName = "Invisigun Heroes";
   saveDir = "Sombr Studio/Invisigun Heroes";
-  version = "1.6.1";
+  version = "1.6.15";
 
   src = fetchItch {
     name = "${name}-${version}.zip";
     gameId = 25561;
     uploadId = 208583;
     version = "v${version}";
-    sha256 = "1jvdqrr7rysvgmyn3zyjyydwl8hwrdisis4wznpjhl02nplmhnqx";
+    sha256 = "0a8lf5w7xb3pzakf4b3adnk1bi7mzqmxvjamci6a6snazhyghrgp";
   };
 
   sandbox.paths.required = [ "$HOME/Invisigun Heroes" ];
