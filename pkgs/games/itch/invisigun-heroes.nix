08ff07627c05911ac491f15e0b98b2bc6f664278
aszlig <aszlig@nix.build>
2018-03-02 17:20:12 +0100

    games/invisigun-heroes: Update to version 1.6.15
    New features:
    
     * Localization: Italian
     * New Cerebrus map: Summit
     * New Cerebrus map: Yinyang
     * New Cerebrus map: Polar Express
    
    Updates:
    
     * CRONUS: Bombs no longer show nearby player indicator
     * CRONUS: Bomb impact area powers up the longer the target is left out
     * CRONUS: Bombs briefly show the impact area when a target is marked
     * CRONUS: Bombs leave soot on the floor which makes players leave a
               footprint trail
     * PROTEUS: Scanner drone shocks tagged players 2 times instead of 3
     * PROTEUS: Scanner drone re-deploy cooldown also applies to
                self-destruction
     * UI: Simplified lobby host info; better readability and presentation
     * UI: "Join Code" is now known as a "Join Key"
     * UI: Post-round scoreboard shows Steam nicknames
     * UI: Post-match stats table headers changed to icons for localization
           space
     * UI: Reduced controller vibration in some UI scenes
     * GFX: Better tile transitions in maps with very different adjacent
            surface types
     * GFX: Improved Selene's victory pose sprite
     * SFX: Subtle audio cue when you first start moving to another tile, as
            input confirmation
    
    Fixes:
    
     * CRONUS: Fixed an issue where the center bomb tiles spawned
               simultaneously instead of staggered
     * CRONUS: Fixed bomb trails damaging scanner drones by touching their
               scan radius
     * GAME: Fixed revealer carrying over to the next round
     * CARRIER: Fixed carrier ball interactions while players are
                teleporting
     * UI: Fixed being able to quickly back out of scenes while they are
           transitioning in
     * UI: Fixed "Player Disconnected" popups when remote clients try to
           join an active match
     * UI: Fixed spam cooldown indicator still visible when a player dies
     * UI: Fixed blank lobby region flag when joining a match in "ANY"
           region
     * UI: Fixed flag icon showing in lobby when connected via IP
     * UI: Long controller names are truncated in lobby player selectors
     * GFX: Fixed pit beasts sorting issues with sand piles
     * GFX: Fixed y-sorting of Beast Tamer coins
     * GFX: Fixed y-sorting of some fence posts on Ice Prison
     * GFX: Fixed map objects showing proper bump FX over network
     * SFX: Fixed music fade out when exiting challenge mode
    
    Full upstream change log:
    
    https://s3-us-west-1.amazonaws.com/sombr-invisigun/messages/ReleaseNotes.txt
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/games/itch/invisigun-heroes.nix
+++ pkgs/games/itch/invisigun-heroes.nix
@@ -4,14 +4,14 @@ buildUnity rec {
   name = "invisigun-heroes";
   fullName = "Invisigun Heroes";
   saveDir = "Sombr Studio/Invisigun Heroes";
-  version = "1.6.15";
+  version = "1.6.25";
 
   src = fetchItch {
     name = "${name}-${version}.zip";
     gameId = 25561;
     uploadId = 208583;
     version = "v${version}";
-    sha256 = "0a8lf5w7xb3pzakf4b3adnk1bi7mzqmxvjamci6a6snazhyghrgp";
+    sha256 = "0kd6z70m85p0iyhipcgxw8x43h662jiwk4r3s1q94nn9zpndwj5s";
   };
 
   sandbox.paths.required = [ "$HOME/Invisigun Heroes" ];
