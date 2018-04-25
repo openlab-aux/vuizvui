2a12b296d563df7e210dd178803bd96dafdfe755
aszlig <aszlig@nix.build>
2018-04-25 13:32:53 +0200

    profiles/managed: Add boot.cleanTmpDir
    A lot of trash has accumulated in /tmp on some of the machines, so let's
    clean it up.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- modules/user/aszlig/profiles/managed.nix
+++ modules/user/aszlig/profiles/managed.nix
@@ -20,6 +20,7 @@ in {
   config = mkIf cfg.enable {
     vuizvui.system.kernel.bfq.enable = true;
 
+    boot.cleanTmpDir = true;
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
