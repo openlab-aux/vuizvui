a67e4eaff437235965757fa34724cd291d3c10b4
devhell <devhell@mailfresser.de>
2018-05-04 17:22:05 +0100

    machines/titan: Enable CUPS
    Additionally let's enable Brother HL-1110 drivers.

--- machines/devhell/titan.nix
+++ machines/devhell/titan.nix
@@ -91,7 +91,10 @@
   services = {
     tftpd.enable = false;
     gnome3.gnome-keyring.enable = true;
-    printing.enable = false;
+    printing = {
+      enable = true;
+      drivers = [ pkgs.gutenprint pkgs.hplip pkgs.cups-brother-hl1110 ];
+    };
     dnscrypt-proxy.enable = false;
   };
 
