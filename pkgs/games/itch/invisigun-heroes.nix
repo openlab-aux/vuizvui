1ffd55e697636d75598b2223cf8c3ed750d047a9
aszlig <aszlig@nix.build>
2018-05-24 10:12:23 +0200

    games/invisigun-heroes: Update to version 1.6.71
    The hotfix release 1.6.71 contains the following fixes:
    
      * UI: Fixed sorting of UI dialogs when in-match
      * UI: Fixed missing Chinese translation for training menu label
      * UI: Fixed resolution "free" label value change when changing
            languages
    
    New features in version 1.6.70:
    
      * New localization: French
      * New localization: Chinese
      * UI: Language selector is presented on first launch
      * UI: Some title screen layout adjustments for localization (complete
            reskin later)
      * CRONUS: Finally has a unique victory pose (like the rest of the
                cast)
    
    Fixes in version 1.6.70:
    
      * GAME: GIF export files are properly closed when canceling
      * GAME: Fixed empty powerup crate falling once on clients if all
              powerups are disabled
      * GAME: Fixed not being able to split shots on clients if projectile
              is already destroyed on the host
      * GAME: Fixed possibility for two projectiles to create a blast
              barrier instead of hitting objects
      * GAME: Fixed missing left ember spawner on The Catacombs
      * GAME: Random map is refreshed when returning to the lobby from a
              match
      * GFX: Fixed the tint of Epi's ghost in his victory pose
      * UI: "Keyboard" is now properly localized on player selectors
      * UI: Fixed potential for game to stall if localized text parsing
            fails in some edge cases
      * UI: Better spacing in GIF end cards for multi-line Steam usernames
      * UI: Fixed Connect button being selected after removing IP digits
            using the GUI
      * UI: Various minor layout fixes for different locales
      * UI Fixed ready up indicator minor scaling issues
      * AUDIO: Fixed replays always playing SFX at max volume
      * EPI: Fixed ghost Epi getting hit instead of real Epi if standing in
             the same spot
    
    Full upstream change log:
    
    https://s3-us-west-1.amazonaws.com/sombr-invisigun/messages/ReleaseNotes.txt
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/games/itch/invisigun-heroes.nix
+++ pkgs/games/itch/invisigun-heroes.nix
@@ -4,14 +4,14 @@ buildUnity rec {
   name = "invisigun-heroes";
   fullName = "Invisigun Heroes";
   saveDir = "Sombr Studio/Invisigun Heroes";
-  version = "1.6.50";
+  version = "1.6.71";
 
   src = fetchItch {
     name = "${name}-${version}.zip";
     gameId = 25561;
     uploadId = 208583;
     version = "v${version}";
-    sha256 = "1p5zx8xs0gjsimkwli23cw41r55vy7dy9rlrqiwk9lvm9si4rn9i";
+    sha256 = "16bcyypigk0nm7ckdfxc9hkwv8dznqs936qmkzbjb22va2j4ip0s";
   };
 
   sandbox.paths.required = [ "$HOME/Invisigun Heroes" ];
