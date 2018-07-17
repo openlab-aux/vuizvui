3917a5e3c4b7784bf535eafb25e0a47662c0978a
aszlig <aszlig@nix.build>
2018-07-17 02:59:32 +0200

    monogame-patcher: Switch to MSBuild
    I really would have preferred a simple Makefile, but with that we can't
    use buildDotnetPackage and we also need to take care of butchering the
    dependencies manually.
    
    So I moved everything to src/ and added a csproj file to clean up most
    of the cruft and just use buildDotnetPackage with minimal attributes.
    
    In addition to that I also added assembly info, so that the command line
    help will show the proper author name, copyright, yaddayadda...
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/games/build-support/monogame-patcher/default.nix
+++ pkgs/games/build-support/monogame-patcher/default.nix
@@ -1,31 +1,24 @@
-{ lib, runCommand, makeWrapper, fetchNuGet, mono, dotnetPackages }:
+{ buildDotnetPackage, fetchNuGet }:
 
-runCommand "monogame-patcher" {
-  nativeBuildInputs = [ mono makeWrapper ];
+buildDotnetPackage {
+  baseName = "monogame-patcher";
+  version = "0.1.0";
 
-  src = ./patcher.cs;
+  src = ./src;
 
-  cecil = "${fetchNuGet {
-    baseName = "Mono.Cecil";
-    version = "0.10-beta7";
-    sha256 = "03bina3llcnylrfrvp5psnwrfn757j7zch5r360rpdn7gmcjjcpl";
-    outputFiles = [ "lib/net40/*" ];
-  }}/lib/dotnet/Mono.Cecil";
+  buildInputs = [
+    (fetchNuGet {
+      baseName = "Mono.Cecil";
+      version = "0.10-beta7";
+      sha256 = "03bina3llcnylrfrvp5psnwrfn757j7zch5r360rpdn7gmcjjcpl";
+      outputFiles = [ "lib/net40/*" ];
+    })
 
-  cliparser = "${fetchNuGet {
-    baseName = "CommandLineParser";
-    version = "2.2.1";
-    sha256 = "0wf8mzr16d2ni008m60rrk738v8ypk74llk6g8mlyx7rrlchnxaf";
-    outputFiles = [ "lib/net45/*" ];
-  }}/lib/dotnet/CommandLineParser";
-
-} ''
-  mkdir -p "$out/bin" "$out/libexec/monogame-patcher"
-  mcs "$src" -out:"$out/libexec/monogame-patcher/patcher.exe" \
-    -lib:"$cecil" -lib:"$cliparser" \
-    -r:Mono.Cecil -r:Mono.Cecil.Rocks -r:CommandLine
-
-  makeWrapper ${lib.escapeShellArg "${mono}/bin/mono"} "$out/bin/$name" \
-    --set MONO_PATH "$cecil:$cliparser" \
-    --add-flags "$out/libexec/monogame-patcher/patcher.exe"
-''
+    (fetchNuGet {
+      baseName = "CommandLineParser";
+      version = "2.2.1";
+      sha256 = "0wf8mzr16d2ni008m60rrk738v8ypk74llk6g8mlyx7rrlchnxaf";
+      outputFiles = [ "lib/net45/*" ];
+    })
+  ];
+}
---
+++ pkgs/games/build-support/monogame-patcher/src/assembly-info.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("monogame-patcher")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyDescription("Patches common annoyances in Mono Assemblies")]
+[assembly: AssemblyCopyright("(c) 2018 aszlig")]
