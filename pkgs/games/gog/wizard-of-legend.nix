e9bb11e89fb728fe2127f5ac24a15df7b3c393ad
aszlig <aszlig@nix.build>
2018-07-31 01:41:25 +0200

    games/wizard-of-legend: Update to version 1.033b
    Upstream fixes:
    
      * Bug with Magsphere spell not collecting projectiles as it should
      * Bug with Frost Feint not freezing enemies when the decoy was struck
      * Bug that caused Air Burst Dash effect to not show up
      * Bug where Takeout Box would grant max shield in post boss PvP
        battles
      * Armor of Greed causing players to lose gold during post boss PvP
        battles
      * Bug that would end the game on double-ko situations during post boss
        PvP battles
      * Bug with loading screen not showing 2nd player on the game board
      * Artifacting on the loading screen
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/games/gog/wizard-of-legend.nix
+++ pkgs/games/gog/wizard-of-legend.nix
@@ -4,12 +4,12 @@ buildUnity {
   name = "wizard-of-legend";
   fullName = "WizardOfLegend";
   saveDir = "Contingent99/Wizard of Legend";
-  version = "1.033";
+  version = "1.033b";
 
   src = fetchGog {
     productId = 2061814323;
     downloadName = "en3installer0";
-    sha256 = "1gbj0znfw87n2kq76vbchb3r2pyrl0wmdcjrvyr11b9igvqakv48";
+    sha256 = "192fhway7ij5f4fh0vb1204f3yg3fxz08fvqlg03gskjs9krcbcz";
   };
 
   unpackCmd = "${unzip}/bin/unzip -qq \"$curSrc\" 'data/noarch/game/*' || :";
