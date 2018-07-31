9fa25489b136a798334a6b774f1077164b82d59c
aszlig <aszlig@nix.build>
2018-07-31 16:49:09 +0200

    games: Add names for setup hooks
    The name "hook" is a not very good derivation name to distinguish it
    from other hooks, so let's actually name them.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/games/build-support/setup-hooks/default.nix
+++ pkgs/games/build-support/setup-hooks/default.nix
@@ -2,10 +2,12 @@
 
 {
   fixFmodHook = makeSetupHook {
+    name = "fix-fmod-hook";
     deps = [];
   } ./fix-fmod.sh;
 
   gogUnpackHook = makeSetupHook {
+    name = "gog-unpack-hook";
     deps = [ libarchive innoextract ];
   } ./gog-unpack.sh;
 }
