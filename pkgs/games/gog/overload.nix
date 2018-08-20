fb0729e1709c087a2f7931253d1d3fd426f33bf0
aszlig <aszlig@nix.build>
2018-08-20 19:38:08 +0200

    games/overload: Update to version 1.0.1854
    New & Changed Features:
    
      * Added a new multiplayer level: Terminal.
      * Added new graphics options (see second page of Advanced Graphics
        Options).
      * Added particle effects to make switches look more interactive.
      * Added platform identifier icons for MP.
      * Added some text to clarify successive NG+ runs.
      * Hacked "Just Practicing" achievement so it will be awarded for
        unlocking Insane+ on all CM levels.
      * Updated Rewired input manager to version 1.1.18.0. Provides support
        for 3Dconnexion SpaceMouse Compact and SpaceMouse Wireless.
      * Added 30 Hz option to V-Sync setting.
      * Private Matches now show password in the lobby now (unless it's
        hidden).
    
    Fixes:
    
      * Fixed an issue with warpers and chunked-out areas.
      * Fixed count-based Achievements on GOG Galaxy.
      * Fixed issue in CM where a robot could be a Variant AND a Super.
      * Fixed an issue with fabricated bots being invisible in some cases.
      * Alien secret doors no longer trigger when Flak 'targets' them.
      * Fixed a graphical glitch with Super Powerups after save/load.
      * Fixed an issue where completing Ymirus on NG+ wouldn't properly
        update saved NG+ values.
      * Fixed an issue where the Super Op warning message could block a the
        final message in a Secret Level.
      * Fixed a problem with some bots not activating properly after loading
        a saved game.
      * Added translations for cheat messages and other text.
      * Fixed a couple of minor text issues, including the spelling of
        "Initiated."
    
    Updates/Fixes in Level Editor Build 34:
    
      * User level [format] improvements
      * Automatic chunking is now controlled by a export-time flag and not
        an ifdef
      * Support for reference_guid in Entities of an .overload for
        overriding with inline prefabs. Use "$INTERNAL$:" as a reference to
        the entity internally to the overload file
      * Bumped user level version number to 4
      * Added LocalPosition and LocalRotation to the ITransformComponent
        interface
      * Added UnityEngine.BoneWeight as a native serialized type
      * Added CreateInlinePrefab as a new command for user level files
      * User level support for loading asset bundles for OSX and Linux
        platforms (in addition to the existing Windows)
      * Updated UnityEngine.Mesh support for colors32, boneWeights, and
        bindposes
      * More emulated Unity matrix4x4 support and BoneWeight
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/games/gog/overload.nix
+++ pkgs/games/gog/overload.nix
@@ -4,11 +4,11 @@ buildUnity {
   name = "overload";
   fullName = "Overload";
   saveDir = "Revival/Overload";
-  version = "1.0.1839";
+  version = "1.0.1854";
 
   src = fetchGog {
     productId = 1309632191;
     downloadName = "en3installer0";
-    sha256 = "1r42ll6k2xif405rp85gn3sbhacrhf1kkpqx2ahp8j5f9alscdxm";
+    sha256 = "1qyd78xzd39763dmrb5rb8g0v0qi45jjkb9id9gjvvmh41m0731i";
   };
 }
