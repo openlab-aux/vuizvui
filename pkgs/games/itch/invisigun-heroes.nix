3afb1cfeb336d0b2209ebd86c9318dac8cdf6525
aszlig <aszlig@nix.build>
2018-09-02 06:17:26 +0200

    games/invisigun-heroes: Update to version 1.6.81
    New content:
    
      * New Cerebrus map: Chaos Pit
    
    Updates:
    
      * SYSTEM: This update will require DirectX 11 or OpenGL on Windows
                (DirectX 9 won't be supported)
      * GAME: Player visibility time after using their ability now includes
              the brief spam window time
      * UI: Ability recharge bar flashes during the short post-cooldown spam
            window time
      * UI: Game base resolution changed from 484x272 to 480x270 (resolution
            preferences will be reset)
      * UI: Some Chinese localization edits
      * UI: Improved progress scene performance
      * UI: Optimized screen fading transitions
      * UI: When exiting the lobby, you can choose to go back to match setup
            or the title screen
      * UI: Bulletin items on title screen will open associated URLs in your
            browser when activated
    
    Fixes:
    
      * EPI: Fix edge case that could cause ghost Epi's position to be out
             of sync online
      * GAME: Fix edge case that could cause a downward spiral if save data
              is corrupt
      * GAME: Cronus bombs' soot shouldn't affect players with Silent Steps
      * GAME: Fix potential for minecarts to bump objects on opposite side
              of a shot hitting them
      * UI: Fix error that prevented all victory phrases from showing up
            (custom or default)
      * UI: Fix loss of UI focus if spamming save/cancel buttons in options
            sub panels
      * UI: Fix timing edge case that could cause the cooldown indicator to
            not show online
      * UI: Fix edge case that could leave a dialog box onscreen
      * NET: Fix edge case that leaves the client in lobby-limbo if the host
             disconnects
      * NET: Fix remote clients not skipping tips screen when playing
             consecutive online matches
      * NET: Handled some edge cases when there is no internet connection
             present
    
    Full upstream changelog:
    
    https://s3-us-west-1.amazonaws.com/sombr-invisigun/messages/ReleaseNotes.txt
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/games/itch/invisigun-heroes.nix
+++ pkgs/games/itch/invisigun-heroes.nix
@@ -4,14 +4,14 @@ buildUnity rec {
   name = "invisigun-heroes";
   fullName = "Invisigun Heroes";
   saveDir = "Sombr Studio/Invisigun Heroes";
-  version = "1.6.71";
+  version = "1.6.81";
 
   src = fetchItch {
     name = "${name}-${version}.zip";
     gameId = 25561;
     uploadId = 208583;
     version = "v${version}";
-    sha256 = "16bcyypigk0nm7ckdfxc9hkwv8dznqs936qmkzbjb22va2j4ip0s";
+    sha256 = "0g3z5yy1c2s2vszs31sc7vlzw2x3pvsw9vnzcb3k9593623iw2hj";
   };
 
   sandbox.paths.required = [ "$HOME/Invisigun Heroes" ];
