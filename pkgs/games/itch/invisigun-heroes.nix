9afa3656f208842bcae5fc58b15ed7fde562a224
aszlig <aszlig@nix.build>
2018-11-17 15:56:08 +0100

    invisigun-heroes: Update to version 1.6.101
    Upstream changes since version 1.6.100:
    
      * UI: Lobby chat messages don't reset the auto countdown timer for
            Guest Edition matches
      * NET: Increased connection and idle timeouts to prevent
             disconnections in some high latency cases
    
    Full upstream changelog:
    
    https://s3-us-west-1.amazonaws.com/sombr-invisigun/messages/ReleaseNotes.txt
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/games/itch/invisigun-heroes.nix
+++ pkgs/games/itch/invisigun-heroes.nix
@@ -4,14 +4,14 @@ buildUnity rec {
   name = "invisigun-heroes";
   fullName = "Invisigun Heroes";
   saveDir = "Sombr Studio/Invisigun Heroes";
-  version = "1.6.100";
+  version = "1.6.101";
 
   src = fetchItch {
     name = "${name}-${version}.zip";
     gameId = 25561;
     uploadId = 208583;
     version = "v${version}";
-    sha256 = "14drg7phbgd7l8zrmcw5njrvs0alilayrdd8xinljzz0i3wmdnhg";
+    sha256 = "0l3dqkda9xxvlx5jbb5s3gp8yblzvp2k2wjsg8h9xx860c5nj3cy";
   };
 
   sandbox.paths.required = [ "$HOME/Invisigun Heroes" ];
