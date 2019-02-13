8201859a65c9ceedea4b1743d1bfc2207b4e11bc
aszlig <aszlig@nix.build>
2019-02-13 19:15:27 +0100

    monogame-patcher: Fix SHA256 hashes for deps
    I checked the archives against the previous versions and the contents
    match, however the archives themselves had differences with the file
    order so the hashes are different now.
    
    In the long term, we probably should use fetchzip instead so we don't
    get a hash mismatch if none of the actual contents change.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/games/build-support/monogame-patcher/default.nix
+++ pkgs/games/build-support/monogame-patcher/default.nix
@@ -10,14 +10,14 @@ buildDotnetPackage {
     (fetchNuGet {
       baseName = "Mono.Cecil";
       version = "0.10-beta7";
-      sha256 = "03bina3llcnylrfrvp5psnwrfn757j7zch5r360rpdn7gmcjjcpl";
+      sha256 = "1ngjxk3wbmdwgsbdpy9yjwgc0ii8xxa78i0h57dia2rjn0gr7bw0";
       outputFiles = [ "lib/net40/*" ];
     })
 
     (fetchNuGet {
       baseName = "CommandLineParser";
       version = "2.2.1";
-      sha256 = "0wf8mzr16d2ni008m60rrk738v8ypk74llk6g8mlyx7rrlchnxaf";
+      sha256 = "02zqp98lzjv4rpjf7jl0hvhda41dlh0dc29axaapq9glk0hbmjzg";
       outputFiles = [ "lib/net45/*" ];
     })
   ];
