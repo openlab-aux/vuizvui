04bbb9966ba29fca3026a606adaa42a8a415523b
devhell <devhell@mailfresser.de>
2019-06-29 22:08:22 +0100

    profiles/base: Allow mpd to access pulseaudio
    What would we do without Arch Linux' wiki, I wonder.

--- modules/user/devhell/profiles/base.nix
+++ modules/user/devhell/profiles/base.nix
@@ -41,6 +41,7 @@ in {
       pulseaudio = {
         enable = true;
         systemWide = false;
+        extraConfig = "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1";
       };
     };
 
