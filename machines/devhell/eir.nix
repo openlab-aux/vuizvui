7fbb403a6a3ab03164c17efe46d1b73318be9511
devhell <devhell@mailfresser.de>
2019-07-10 18:45:17 +0100

    machines/{hildr,eir}: Enable localtime service
    When one travels with one's laptop, one does not wish to be disturbed by
    having to manually configure a TZ. How plebish.

--- machines/devhell/eir.nix
+++ machines/devhell/eir.nix
@@ -83,6 +83,7 @@
   vuizvui.user.devhell.profiles.services.enable = true;
 
   services = {
+    localtime.enable = true;
     thermald.enable = true;
     tftpd.enable = false;
     gnome3.gnome-keyring.enable = true;
