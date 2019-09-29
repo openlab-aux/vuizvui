a997e4b18b56f5d57ce6883b255a637243db1045
Guillaume Maudoux <layus.on@gmail.com>
2019-09-30 01:22:36 +0200

    baba-is-you: fix $HOME usage with update

--- pkgs/games/humblebundle/baba-is-you.nix
+++ pkgs/games/humblebundle/baba-is-you.nix
@@ -2,19 +2,19 @@
 
 buildGame rec {
   name = "baba-is-you-unstable-${version}";
-  version = "2019-03-31"; # guessed from mar31 in file name.
+  version = "2019-08-07"; # guessed from aug07 in file name.
 
-  # https://dl.humble.com/hempulioy/BIY_linux_mar31.tar.gz
   src = fetchHumbleBundle {
+    name = "BIY_linux_aug07.tar.gz";
     machineName = "babaisyou_odtbg_linux_oR9qb";
     downloadName = "Download";
     suffix = "tar.gz";
-    md5 = "61893c3ead0dc1823115ad83dcb9aee0";
+    md5 = "3694afc5579cdaad7448c9744aa8d063";
   };
 
   buildInputs = [ makeWrapper libGL ];
 
-  sandbox.paths.required = [ "$HOME/Baba_Is_You" ];
+  sandbox.paths.required = [ "$XDG_DATA_HOME/Baba_Is_You" ];
 
   installPhase = ''
     mkdir -p "$out/bin" "$out/share/baba-is-you"
