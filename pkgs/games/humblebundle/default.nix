7bb7982491e995c3cf81360c1a905507a8d1b07b
Guillaume Maudoux <layus.on@gmail.com>
2019-09-30 10:52:30 +0200

    Update pkgs/games/humblebundle/default.nix
    Co-Authored-By: aszlig <aszlig@redmoonstudios.org>

--- pkgs/games/humblebundle/default.nix
+++ pkgs/games/humblebundle/default.nix
@@ -30,7 +30,7 @@ let
     owlboy = callPackage ./owlboy.nix {};
     pico-8 = callPackage ./pico-8.nix {};
     rocketbirds = callPackage ./rocketbirds.nix {};
-    spacechem = callPackage ./spacechem.nix {};
+    spacechem = callPackage_i686 ./spacechem.nix {};
     spaz = callPackage ./spaz.nix {};
     starbound = callPackage ./starbound.nix {};
     swordsandsoldiers = callPackage ./swordsandsoldiers.nix {};
