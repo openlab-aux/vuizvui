openlab-aux/vuizvui

commit e9975c9a39cb4e654d9132de4b952f51174a0926

pkgs/profpatsch: writeExecline, writeExeclineLocal
similar to writeScript, but writes an execline instead. Should be
upstreams to `lib.writers` sometimes.

pkgs/profpatsch/default.nix
@@ -105,6 +105,10 @@ let
         testing.drvSeqL tests (itLocal name args execline);
     };
 
+  writeExeclineFns = import ./write-execline.nix {
+    inherit pkgs execlineb-with-builtins;
+  };
+
 
 in rec {
   inherit (nixperiments)
@@ -153,6 +157,8 @@ in rec {
 
   inherit (runExeclineFns)
     runExecline runExeclineLocal;
+  inherit (writeExeclineFns)
+    writeExecline writeExeclineBin;
 
   inherit getBins;
 

pkgs/profpatsch/execline/write-execline.nix
@@ -1,0 +1,18 @@
+{ pkgs, execlineb-with-builtins }:
+let
+  escape = ./escape.nix { inherit (pkgs) lib; };
+
+  # Write a list of execline argv parameters to an execline script.
+  # Everything is escaped correctly.
+  # TODO upstream into nixpkgs
+  writeExeclineCommon = writer: name: { readNArgs ? 0 }: argList: writer name ''
+    #!${execlineb-with-builtins}/bin/execlineb -s${toString readNArgs}
+    export EXECLINE_STRICT 2
+    ${escape.escapeExecline argList}
+  '';
+  writeExecline = writeExeclineCommon pkgs.writeScript;
+  writeExeclineBin = writeExeclineCommon pkgs.writeScriptBin;
+
+in {
+  inherit writeExecline writeExeclineBin;
+}

commit 5716d0546978256fa3705f2059682f869a6a273a

pkgs/profpatsch: Fix eval error due to wrong path
Introduced in commit e9975c9a39cb4e654d9132de4b952f51174a0926.

The write-execline.nix file is inside the execline/ directory but the
import doesn't reflect that, so since there isn't any write-execline.nix
in other locations, I assume that this is what the author actually
intended.

Signed-off-by: aszlig <aszlig@nix.build>
Cc: @Profpatsch

pkgs/profpatsch/default.nix
@@ -105,7 +105,7 @@ let
         testing.drvSeqL tests (itLocal name args execline);
     };
 
-  writeExeclineFns = import ./write-execline.nix {
+  writeExeclineFns = import ./execline/write-execline.nix {
     inherit pkgs execlineb-with-builtins;
   };
 