7938e7e27c787ad0f5372a74b8fc349f006fda09
aszlig <aszlig@nix.build>
2019-10-12 10:56:46 +0200

    vim: Add syntax highlighting/indent for PureScript
    This probably needs a bit of refinement when it comes to indentation,
    but at least we get proper syntax highlighting, which is the main point
    here.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/aszlig/vim/default.nix
+++ pkgs/aszlig/vim/default.nix
@@ -356,6 +356,13 @@ let
         install -vD -m 0644 "$downloadedFile" "$out/syntax/xt.vim"
       '';
     };
+
+    purescript = fetchFromGitHub {
+      owner = "purescript-contrib";
+      repo = "purescript-vim";
+      rev = "67ca4dc4a0291e5d8c8da48bffc0f3d2c9739e7f";
+      sha256 = "1insh39hzbynr6qxb215qxhpifl5m8i5i0d09a3b6v679i7s11i8";
+    };
   };
 
   generic = ''
