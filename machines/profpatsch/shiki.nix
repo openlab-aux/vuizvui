openlab-aux/vuizvui

commit 057b0a46b542267dc4c8a3a61427923bb2797a50

machines/profpatsch/shiki: use dhall from binary release
The hackage (and thus nixpkgs) version is sadly very much outdated.
Luckily, Justin Woo provides the statically linked binaries from the
upstream project.

This won’t work for patches or using the library, but at the moment
it’s okay.

machines/profpatsch/shiki.nix
@@ -176,8 +176,8 @@ in {
         pkgs.vuizvui.profpatsch.watch-server # restart server on code change
         pkgs.vuizvui.profpatsch.until        # restart until cmd succeeds
         execline
-        dhall
-        myPkgs.dhall-flycheck
+        pkgs.vuizvui.profpatsch.dhall
+        pkgs.vuizvui.profpatsch.dhall-flycheck
       ];
       documentation = [
         # mustache-spec NOT IN 16.09

machines/profpatsch/pkgs.nix
@@ -102,14 +102,6 @@ let
         --replace 'notify-send' '${notify-send}'
     '';
 
-  dhall-flycheck = pkgs.vuizvui.profpatsch.haskellPackages.callPackage
-    (import "${pkgs.fetchFromGitHub {
-      owner = "Profpatsch";
-      repo = "dhall-flycheck";
-      rev = "0db095732820cd27eccbe9ece97500dd292353de";
-      sha256 = "05n76b781fgm5n7kqq1gpqfzshjfpadahxryxqrfvs3sjn3a6bvp";
-    }}/dhall-flycheck.nix") {};
-
 in
 { inherit
     nix
@@ -123,6 +115,5 @@ in
     saneGhci
     /*searx*/
     pyrnotify
-    dhall-flycheck
     ;
 }

pkgs/profpatsch/utils-hs/default.nix
@@ -58,17 +58,6 @@ let
             dependent-sum prettyprinter (hlib.doJailbreak ref-tf)
           ]);
         });
-
-        dhall-nix = hlib.justStaticExecutables (hlib.overrideCabal hsuper.dhall-nix (old: {
-          src = fetchFromGitHub {
-            owner = "Profpatsch";
-            repo = "dhall-nix";
-            # manual update to dhall @0.19
-            rev = "feae0ce5b2ecf4daeeae15c39f427f126c33da7c";
-            sha256 = "1kdsbnj681lf65dsdclcrzj4cab1hh0v22n2140386zvwmawyp6r";
-          };
-          broken = false;
-        }));
       });
     };
 

commit 69750084815bf994b859819f1dadab83ba0830c6

profpatsch/shiki: Temp-disable dhall{-flycheck}
Eval error introduced by 057b0a46b542267dc4c8a3a61427923bb2797a50.

It looks like that commit is probably missing some files, because I
haven't been able to find anything that would remotely match the dhall
and dhall-flycheck attributes.

The former would be available in nixpkgs, but since the commit in
question actually *changed* the attribute so that it no longer points to
the nixpkgs one. The latter (dhall-flycheck) isn't available in nixpkgs
at all.

To ensure that channels evaluate properly, I temporarily commented out
these attributes.

Signed-off-by: aszlig <aszlig@nix.build>
Cc: @Profpatsch

machines/profpatsch/shiki.nix
@@ -176,8 +176,9 @@ in {
         pkgs.vuizvui.profpatsch.watch-server # restart server on code change
         pkgs.vuizvui.profpatsch.until        # restart until cmd succeeds
         execline
-        pkgs.vuizvui.profpatsch.dhall
-        pkgs.vuizvui.profpatsch.dhall-flycheck
+        # XXX: Breaks channel evaluation because these attributes do not exist.
+        #pkgs.vuizvui.profpatsch.dhall
+        #pkgs.vuizvui.profpatsch.dhall-flycheck
       ];
       documentation = [
         # mustache-spec NOT IN 16.09