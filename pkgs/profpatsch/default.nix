00fce07dba2f80f94f1715f27bdb91b67a65ec89
Profpatsch <mail@profpatsch.de>
2020-02-10 14:57:29 +0100

    pkgs/profpatsch: add dhall from easy-dhall-nix
    It’s the easiest way to get recent statically compiled dhall
    executables.

--- pkgs/profpatsch/default.nix
+++ pkgs/profpatsch/default.nix
@@ -162,4 +162,14 @@ in rec {
     inherit symlink;
   };
 
+
+  easy-dhall-nix = (import (pkgs.fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-dhall-nix";
+    rev = "7e063af9bd6f6bd4b4be00dd0a8bff272893fc44";
+    sha256 = "0c47zlxhpjm1hddhxsqy86mkyy7hlym1rjmnapc3b7x7bkchr8za";
+  }) { inherit pkgs; });
+
+  dhall = easy-dhall-nix.dhall-simple;
+  dhall-nix = easy-dhall-nix.dhall-nix-simple;
 }
