4eb43849cff17c1da74a82b719ef0b9bc43f2808
aszlig <aszlig@nix.build>
2020-02-10 01:20:22 +0100

    invisigun-heroes: Update to version 1.7.16
    Lots of changes have been introduced in version 1.7.6, so here is a
    small summary:
    
      * New "The Hero's Journey" mode, which allows to discover the
        abilities of each hero along with challenges and puzzles.
      * Three new heroes: Djaan-Khe, Zephyr, Violet
      * Challenge mode has been removed (replaced by "The Hero's Journey").
      * Guest Edition has been removed.
    
    The full changelog can be found here:
    
    http://www.sombr.com/pub/SIG/changelog/
    
    While Invisigun Heroes in general went pretty smooth when it comes to
    packaging on our side, this time unfortunately we needed to patch
    something.
    
    There is now a new "config.json", which is written inside the game's
    data directory. Of course, since the data directory resides in the Nix
    store, I've relocated the file to be written into the the config
    directory ($XDG_CONFIG_HOME/unity3d/Sombr Studio/Invisigun Reloaded) of
    the game.
    
    Note that the config directory has also changed from "Invisigun Heroes"
    to "Invisigun Reloaded", so if you don't want to play the old version,
    you can delete the "Invisigun Heroes" directory.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/games/itch/invisigun-heroes.nix
+++ pkgs/games/itch/invisigun-heroes.nix
@@ -1,18 +1,43 @@
-{ buildUnity, fetchItch }:
+{ buildUnity, fetchItch, mono, monogamePatcher, strace }:
 
 buildUnity rec {
   name = "invisigun-heroes";
-  fullName = "Invisigun Heroes";
-  saveDir = "Sombr Studio/Invisigun Heroes";
-  version = "1.6.101";
+  fullName = "Invisigun";
+  saveDir = "Sombr Studio/Invisigun Reloaded";
+  version = "1.7.16";
 
   src = fetchItch {
     name = "${name}-${version}.zip";
     gameId = 25561;
     uploadId = 208583;
     version = "v${version}";
-    sha256 = "0l3dqkda9xxvlx5jbb5s3gp8yblzvp2k2wjsg8h9xx860c5nj3cy";
+    sha256 = "1flwc5wvw84s53my8v8n402iz6izjs4d4ppffajdv9cg1vs3nbpl";
   };
+
+  nativeBuildInputs = [ mono monogamePatcher ];
+
+  buildPhase = ''
+    cat > nix-support.cs <<EOF
+    using UnityEngine;
+
+    public class NixSupport {
+      public static string GetFullPathStub(string _ignore) {
+        return Application.persistentDataPath;
+      }
+    }
+    EOF
+
+    mcs nix-support.cs -target:library \
+      -r:Invisigun_Data/Managed/UnityEngine.CoreModule \
+      -out:Invisigun_Data/Managed/NixSupport.dll
+
+    monogame-patcher replace-call \
+      -i Invisigun_Data/Managed/Assembly-CSharp.dll \
+      -a Invisigun_Data/Managed/NixSupport.dll \
+      'System.String System.IO.Path::GetFullPath(System.String)' \
+      'System.String NixSupport::GetFullPathStub(System.String)' \
+      FileManagerAdapter_Desktop::ApplicationPath
+  '';
 
   sandbox.paths.required = [ "$HOME/Invisigun Heroes" ];
 }
