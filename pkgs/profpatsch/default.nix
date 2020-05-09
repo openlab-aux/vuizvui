9b695406857562d8b1a24ef96794f83c5f13c68f
Profpatsch <mail@profpatsch.de>
2020-05-09 21:02:22 +0200

    pkgs/profpatsch: update easy-dhall-nix, add dhall-json

--- pkgs/profpatsch/default.nix
+++ pkgs/profpatsch/default.nix
@@ -164,12 +164,13 @@ in rec {
   easy-dhall-nix = (import (pkgs.fetchFromGitHub {
     owner = "justinwoo";
     repo = "easy-dhall-nix";
-    rev = "7e063af9bd6f6bd4b4be00dd0a8bff272893fc44";
-    sha256 = "0c47zlxhpjm1hddhxsqy86mkyy7hlym1rjmnapc3b7x7bkchr8za";
+    rev = "27edaa0c769b9c876f11c50c9ab542061d07def5";
+    sha256 = "0w2ckl8fifnaq12ycqqa9ldqjsys3jbblbg7f1vkqa272y89zl08";
   }) { inherit pkgs; });
 
   dhall = easy-dhall-nix.dhall-simple;
   dhall-nix = easy-dhall-nix.dhall-nix-simple;
+  dhall-json = easy-dhall-nix.dhall-json-simple;
 
   dhall-flycheck = haskellPackages.callPackage
     (import "${pkgs.fetchFromGitHub {
