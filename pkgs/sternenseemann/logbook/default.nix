e71d8d665a7450369096712de3d606c8ac6268ff
sternenseemann <git@lukasepple.de>
2020-05-21 04:09:39 +0200

    pkgs/sternenseemann/logbook: 0.2 → 0.3

--- pkgs/sternenseemann/logbook/default.nix
+++ pkgs/sternenseemann/logbook/default.nix
@@ -1,24 +1,22 @@
-{ lib, buildDunePackage, fetchFromGitHub, fetchpatch
+{ lib, buildDunePackage, fetchFromGitHub
 , ocaml_lwt, jingoo, ptime, angstrom, astring, cow}:
 
 buildDunePackage rec {
-  version = "0.2";
+  version = "0.3";
   pname = "logbook";
 
   src = fetchFromGitHub {
     owner  = "sternenseemann";
     repo   = pname;
     rev    = "v${version}";
-    sha256 = "1x202546ivs46m1193bzlnkpi3mxjjs3c1hcrlnb21v5x4fjnd4m";
+    sha256 = "05iwma3vv4v2b3nkb1n1fkfj1gbixw04r8rxdnm468zhf7i7z9gj";
   };
+
+  # fix for angstrom < 0.14
+  postPatch = "sed -i s/~consume:All// src/logbook.ml";
 
   buildInputs = [ cow ocaml_lwt jingoo ];
   propagatedBuildInputs = [ ptime angstrom astring ];
-
-  patches = [ (fetchpatch {
-    url = "https://github.com/sternenseemann/logbook/commit/d4ee40ada4bba55505cc55ab653e69fa5c6406e6.diff";
-    sha256 = "0dj6q2h1pxcniypx14if4gnfq7bbghsig9g51n7z2mad0fsji4sa";
-  }) ];
 
   useDune2 = true;
 
