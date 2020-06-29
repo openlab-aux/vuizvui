bfee3147aaf52163a14b934fc4fdbb9f3ef34652
Profpatsch <mail@profpatsch.de>
2020-06-29 02:54:44 +0200

    machines/legosi: add v6 address

--- machines/profpatsch/legosi.nix
+++ machines/profpatsch/legosi.nix
@@ -46,6 +46,18 @@ in {
           80 443
         ];
       };
+
+      interfaces.ens3 = {
+        ipv6.addresses = [{
+          address = "2a01:4f8:c0c:70aa::1";
+          prefixLength = 64;
+        }];
+      };
+
+      defaultGateway6 = {
+        address = "fe80::1";
+        interface = "ens3";
+      };
     };
 
 
