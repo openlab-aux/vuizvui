9ac9166d6a9398bbf31b6e3d0f30a835226c854c
aszlig <aszlig@nix.build>
2020-11-02 21:26:37 +0100

    flake.nix: Expose packages as legacyPackages
    While the "packages" within Nix Flakes does make a few things a bit
    simpler it also only allows us to export a flat structure.
    
    However, in Vuizvui we want to use namespaces for the corresponding
    package sets, since different packages could have different overrides
    depending on the namespace of the corresponding author.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- flake.nix
+++ flake.nix
@@ -1,8 +1,12 @@
 {
   description = "Vuizvui";
 
-  outputs = _: {
+  outputs = { self, nixpkgs }: {
     nixosModules.vuizvui = import modules/module-list.nix;
     overlay = _: pkgs: { vuizvui = import ./pkgs { inherit pkgs; }; };
+
+    legacyPackages = nixpkgs.lib.mapAttrs (_: pkgs: let
+      inherit (pkgs.extend self.overlay) vuizvui;
+    in vuizvui) nixpkgs.legacyPackages;
   };
 }
