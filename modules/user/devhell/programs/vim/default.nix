7af5fc86eb261dab2c21f20fab25dc087adb861d
devhell <devhell@mailfresser.de>
2020-11-12 13:43:36 +0000

    programs/vim: Rework vim
    Now it's pretty much the way I want it.

--- modules/user/devhell/programs/vim/default.nix
+++ modules/user/devhell/programs/vim/default.nix
@@ -7,6 +7,7 @@ let
     name = "vim";
     vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
       start = [
+        ale
         awesome-vim-colorschemes
         delimitMate
         dracula-vim
@@ -16,6 +17,8 @@ let
         lightline-vim
         limelight-vim
         sleuth
+        tabular
+        vim-lastplace
         vim-nix
         vim-signify
       ];
@@ -42,13 +45,22 @@ let
       set backspace=indent,eol,start
       set nu
       set cursorline
+      set laststatus=2
+      set signcolumn=number
+      set timeout timeoutlen=5000 ttimeoutlen=100
+
+      filetype plugin indent on
+      colorscheme tender
+
+      " signify Settings
+      set updatetime=100
 
       " indentLine Settings
       let g:indentLine_char_list = ['|', '¦', '┆', '┊']
       let g:indentLine_setColors=1
       let g:indentLine_fileTypeExclude=['help', 'main', 'vimfiler', 'json']
-      let g:better_whitespace_filetypes_blacklist = ['diff', 'gitcommit', 'unite',
-      \ 'qf', 'help', 'markdown', 'leaderGuide']
+      let g:better_whitespace_filetypes_blacklist = ['diff', 'gitcommit',
+      \ 'help', 'markdown', 'leaderGuide']
 
       " goyo Settings
       autocmd! User GoyoEnter Limelight
@@ -57,19 +69,16 @@ let
       " lightline Settings
       let g:lightline = {'colorscheme': 'deus'}
       set noshowmode
-
-      " Load a Colorscheme
-      colorscheme tender
     '';
   };
 
-      in {
-        options.vuizvui.user.devhell.programs.vim = {
-          enable = lib.mkEnableOption "devhell's Vim";
-        };
+in {
+  options.vuizvui.user.devhell.programs.vim = {
+    enable = lib.mkEnableOption "devhell's Vim";
+  };
 
-        config = lib.mkIf cfg.enable {
-          environment.variables.EDITOR = "vim";
-          environment.systemPackages = [ customVim ];
-        };
-      }
+  config = lib.mkIf cfg.enable {
+    environment.variables.EDITOR = "vim";
+    environment.systemPackages = [ customVim ];
+  };
+}
