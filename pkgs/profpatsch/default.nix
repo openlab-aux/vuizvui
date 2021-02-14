6af2184cfd1a613fea2a40f5b02642fddbd6d727
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-02-14 15:32:17 +0100

    pkgs/profpatsch/el-semicolon: build and run tests using testRustSimple

--- pkgs/profpatsch/default.nix
+++ pkgs/profpatsch/default.nix
@@ -256,9 +256,8 @@ in rec {
     record-get
     ;
 
-  inherit (import ./execline/default.nix { inherit pkgs writeRustSimpleLib rust-deps; })
+  inherit (import ./execline/default.nix { inherit pkgs writeRustSimpleLib testRustSimple rust-deps; })
     el-semicolon
-    el-semicolon-tests
     el-exec
     el-substitute
     ;
