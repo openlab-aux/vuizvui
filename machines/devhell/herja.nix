6ec330c003ec476ca0a411c22c568bb25e7e2efc
devhell <devhell@mailfresser.de>
2021-02-25 15:56:39 +0000

    machines/herja: add rocm-opencl-icd
    I don't know what I'm doing, but it's fun.

--- machines/devhell/herja.nix
+++ machines/devhell/herja.nix
@@ -33,7 +33,12 @@
     };
     opengl = {
       enable = true;
-      extraPackages = [ pkgs.libvdpau-va-gl pkgs.vaapiVdpau pkgs.amdvlk ];
+      extraPackages = with pkgs; [
+        libvdpau-va-gl
+        vaapiVdpau
+        amdvlk
+        rocm-opencl-icd
+      ];
     };
   };
 
