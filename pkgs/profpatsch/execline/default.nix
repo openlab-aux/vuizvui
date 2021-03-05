36993716d88a25e5e5e4ee06f652cc04a010c349
Profpatsch <mail@profpatsch.de>
2021-03-06 00:11:34 +0100

    pkgs/profpatsch/execline: add missing execline dep

--- pkgs/profpatsch/execline/default.nix
+++ pkgs/profpatsch/execline/default.nix
@@ -19,7 +19,7 @@ let
 
   el-substitute = writeRustSimpleLib "el_substitute" {
     dependencies = [ rust-deps.libc rust-deps.errno ];
-    buildInputs = [ pkgs.skalibs ];
+    buildInputs = [ pkgs.skalibs pkgs.execline ];
     release = false;
     verbose = true;
   } ./el_substitute.rs;
