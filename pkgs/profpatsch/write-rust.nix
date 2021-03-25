165a0b1a67b5000fc3d2fcba1936d8595a24eed8
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-03-25 23:02:24 +0100

    fix(profpatsch/write-rust): fail on rust test failure
    Port of <https://cl.tvl.fyi/c/depot/+/2657>. Original message:
    
    `forstdin` iterates over the tests in the test directory, and by
    default it does *not* fail if an inner loop returns an error, unless
    `-o okcodes` is given, a list of exit codes that indicate success.
    
    Now it fails if a loop returns ≠ 0.

--- pkgs/profpatsch/write-rust.nix
+++ pkgs/profpatsch/write-rust.nix
@@ -52,7 +52,7 @@ let
         "importas" "out" "out"
         "if" [
           "pipeline" [ bins.ls "${crate true}/tests" ]
-          "forstdin" "test"
+          "forstdin" "-o0" "test"
           "importas" "test" "test"
           "${crate true}/tests/$test"
         ]
