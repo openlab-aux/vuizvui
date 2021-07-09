openlab-aux/vuizvui

commit 709e45c2a8c231201d3f67c6a954021ca2a5f796

modules/git: Switch to RFC-0042
In RFC-0042[1], the structural settings are exposed via a "settings"
attribute, while in our module it's called "config". To make this less
ambiguous (since there is already a "config" attribute passed to
modules) and more in line with best practices, I renamed it accordingly.

Additionally, the configuration file is now generated via *defining*
another (read-only) option, which can be used by other modules to
reference the path. The previous way this has been done was using the
apply attribute to mkOption, which makes it really hard to access the
original attributes for these settings.

[1]: https://git.io/JcXmU

Signed-off-by: aszlig <aszlig@nix.build>

modules/user/aszlig/programs/git/default.nix
@@ -4,30 +4,6 @@ let
   inherit (lib) types;
   cfg = config.vuizvui.user.aszlig.programs.git;
 
-  genConf = attrs: let
-    escStr = s: "\"${lib.escape [ "\"" "\\" ] s}\"";
-    mkVal = v: if lib.isBool v && v  then "true"
-          else if lib.isBool v && !v then "false"
-          else escStr (toString v);
-    mkLine = key: val: "${key} = ${mkVal val}";
-
-    joinLines = lib.concatStringsSep "\n";
-    filterNull = lib.filterAttrs (lib.const (v: !(isNull v)));
-
-    mkSection = sect: subsect: vals: ''
-      [${sect}${lib.optionalString (subsect != null) " ${escStr subsect}"}]
-      ${joinLines (lib.mapAttrsToList mkLine (filterNull vals))}
-    '';
-
-    mkConf = sect: content: let
-      subs = lib.filterAttrs (lib.const lib.isAttrs) content;
-      nonSubs = lib.filterAttrs (lib.const (s: !lib.isAttrs s)) content;
-      maybePlain = lib.optional (nonSubs != {});
-      plainSects = lib.singleton (mkSection sect null nonSubs);
-    in lib.mapAttrsToList (mkSection sect) subs ++ maybePlain plainSects;
-
-    lines = lib.flatten (lib.mapAttrsToList mkConf attrs);
-  in pkgs.writeText "gitconfig" (joinLines lines);
 
   gitPatched = pkgs.gitFull.overrideAttrs (git: {
     makeFlags = let
@@ -41,9 +17,15 @@ in {
   options.vuizvui.user.aszlig.programs.git = {
     enable = lib.mkEnableOption "Git";
 
-    config = lib.mkOption {
-      description = "System-wide default config for Git";
+    configFile = lib.mkOption {
+      type = types.path;
+      readOnly = true;
+      description = ''
+        The path to the system-wide configuration file.
+      '';
+    };
 
+    settings = lib.mkOption {
       type = let
         scalar = types.oneOf [ types.bool types.int types.str ];
         section = types.either (types.attrsOf scalar) scalar;
@@ -56,11 +38,41 @@ in {
         guitool.foobar.noconsole = true;
       };
 
-      apply = genConf;
+      description = ''
+        System-wide Git options as described in <citerefentry>
+          <refentrytitle>git-config</refentrytitle>
+          <manvolnum>1</manvolnum>
+        </citerefentry>.
+      '';
     };
   };
 
   config = lib.mkIf cfg.enable {
+    vuizvui.user.aszlig.programs.git.configFile = let
+      escStr = s: "\"${lib.escape [ "\"" "\\" ] s}\"";
+      mkVal = v: if lib.isBool v && v  then "true"
+            else if lib.isBool v && !v then "false"
+            else escStr (toString v);
+      mkLine = key: val: "${key} = ${mkVal val}";
+
+      joinLines = lib.concatStringsSep "\n";
+      filterNull = lib.filterAttrs (lib.const (v: !(isNull v)));
+
+      mkSection = sect: subsect: vals: ''
+        [${sect}${lib.optionalString (subsect != null) " ${escStr subsect}"}]
+        ${joinLines (lib.mapAttrsToList mkLine (filterNull vals))}
+      '';
+
+      mkConf = sect: content: let
+        subs = lib.filterAttrs (lib.const lib.isAttrs) content;
+        nonSubs = lib.filterAttrs (lib.const (s: !lib.isAttrs s)) content;
+        maybePlain = lib.optional (nonSubs != {});
+        plainSects = lib.singleton (mkSection sect null nonSubs);
+      in lib.mapAttrsToList (mkSection sect) subs ++ maybePlain plainSects;
+
+      lines = lib.flatten (lib.mapAttrsToList mkConf cfg.settings);
+    in pkgs.writeText "gitconfig" (joinLines lines);
+
     environment.systemPackages = [
       gitPatched
       pkgs.gitAndTools.git-remote-hg

modules/user/aszlig/profiles/workstation/default.nix
@@ -78,7 +78,7 @@ in {
     vuizvui.user.aszlig.programs.mpv.enable = true;
 
     vuizvui.user.aszlig.programs.git.enable = true;
-    vuizvui.user.aszlig.programs.git.config = {
+    vuizvui.user.aszlig.programs.git.settings = {
       color.ui = "auto";
       merge.tool = "vimdiff3";
       user.email = "aszlig@nix.build";

commit a8218af7daa5dc0d41b8d523daf3743f9320b5be

modules/git: Fix reference to Git config file
Regression introduced in 709e45c2a8c231201d3f67c6a954021ca2a5f796.

With that commit I renamed the "config" option to be called "settings",
but not only that, the actual config *file* is now to be found in the
"configFile" option.

Unfortunately I forgot to change that reference, which caused an
evaluation error.

Signed-off-by: aszlig <aszlig@nix.build>

modules/user/aszlig/programs/git/default.nix
@@ -8,7 +8,7 @@ let
   gitPatched = pkgs.gitFull.overrideAttrs (git: {
     makeFlags = let
       oldFlags = git.makeFlags or [];
-      newVal = "ETC_GITCONFIG=${cfg.config}";
+      newVal = "ETC_GITCONFIG=${cfg.configFile}";
     in if lib.isList oldFlags
        then oldFlags ++ [ newVal ]
        else "${oldFlags} ${newVal}";