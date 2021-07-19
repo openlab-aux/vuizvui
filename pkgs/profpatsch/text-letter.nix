caaea0bec59a4c978f349f1b6b2f7354fe579a0a
Profpatsch <mail@profpatsch.de>
2021-07-19 10:34:45 +0200

    pkgs/profpatsch/text-letter: file from stdin

--- pkgs/profpatsch/text-letter.nix
+++ pkgs/profpatsch/text-letter.nix
@@ -13,7 +13,6 @@ let
   } (./text-letter.rs);
 
   text-letter = writeExecline "write-letter" {} [
-    "pipeline" [ bins.cat "/home/philip/kot/work/pa/krankenkasse/2021-05-18-tk-anschreiben-assurance-maladie.toml" ]
     mustache-interpol
   ];
 
