e686e800d47af13550f3f2166d410d76a2de50eb
aszlig <aszlig@nix.build>
2022-03-24 23:43:20 +0100

    psi: Update to latest upstream master
    The version from 2021 certainly is way too old for my desire to crave
    new bugs, so let's update it for no particular reason other than gut
    feeling.
    
    Oh, well and of course I saw a bunch of OMEMO plugin fixes, so maybe if
    you search hard enough you'll find compelling reasons... :-D
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/aszlig/psi/default.nix
+++ pkgs/aszlig/psi/default.nix
@@ -18,12 +18,12 @@ let
 
   usrsctp = stdenv.mkDerivation {
     pname = "usrsctp";
-    version = "git20210517";
+    version = "git20220218";
 
     src = fetchFromGitHub {
       owner = "sctplab";
       repo = "usrsctp";
-      rev = "e984d7f3c1b13d0b0582497b385c93f0e8d89fb3";
+      rev = "b29c220387d78a48d78186628d4653c89da5dd30";
       sha256 = "0rnhbl80l7gv6r9fqja333l8g06h6cj6d422f6wkx22hfxcphxvf";
     };
 
@@ -32,21 +32,21 @@ let
 
 in stdenv.mkDerivation rec {
   name = "psi-${version}";
-  version = "2.0git20210604aszlig";
+  version = "2.0git20220315aszlig";
 
   src = fetchFromGitHub {
     owner = "psi-im";
     repo = "psi";
-    rev = "ac2d3dc43e5183c8cd897f12b6643de22b5a6a53";
-    sha256 = "0dpp79mmacv14haiscdra3f1znkfzrikw3m9ybq6vbbmsbs860j0";
+    rev = "e7958dad81c870da52ad4c6d8beb8e60d736f73c";
+    hash = "sha256-qhoqmdVHN6aZLMkRSBoJ+lDcIizNM/TyHWi/Ez5lj1g";
     fetchSubmodules = true;
   };
 
   plugins = fetchFromGitHub {
     owner = "psi-im";
     repo = "plugins";
-    rev = "d4824a5c9c13cba0ae03dd265d95b5b921baf001";
-    sha256 = "1wk0ppfzadzq64hh34jr2mc7bw21bw61zd5dwk18sjr107b9khqw";
+    rev = "ddfcbe1cfdb3afc221547bd4d658c51342cd084b";
+    hash = "sha256-UEdYAGyRxdLKx7GfXN7bnu9NRRJuEeZYrtVciNnl52E";
   };
 
   patches = [
