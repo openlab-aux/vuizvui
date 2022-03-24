openlab-aux/vuizvui

commit 25a077f90a0005b519db071a6b5b4d20bd6d2d45

programs/zsh: Add syntax highlighting
I'm already using zsh-fast-syntax-highlighting since weeks via the
configuration.nix and I actually forgot why I used this implementation
rather than one of the others out there.

However, since I'm also using Nushell[1] on a regular basis, I got quite
used to syntax highlighting so that's why I added it to zsh as well.

[1]: https://www.nushell.sh/

Signed-off-by: aszlig <aszlig@nix.build>

modules/user/aszlig/programs/zsh/default.nix
@@ -56,7 +56,48 @@ in {
       "interactive_comments"
     ];
 
-    programs.zsh.interactiveShellInit = mkAfter ''
+    programs.zsh.interactiveShellInit = let
+      # This is to make sure that all themes use either color names or RGB
+      # colors, otherwise they're not displayed correctly on 24bit color terms.
+      highlighter = pkgs.zsh-fast-syntax-highlighting.overrideAttrs (drv: {
+        fixThemeColors = pkgs.writers.writePython3 "fix-theme-colors" {
+          libraries = [ pkgs.python3Packages.plumbum ];
+          flakeIgnore = [ "E111" "E121" "E302" "E305" ];
+        } ''
+          import re
+          from glob import glob
+          from plumbum.colorlib.styles import Color
+          from configparser import RawConfigParser
+
+          def fix_color(color: str) -> str:
+            if color[:2] in ('fg', 'bg') and color[2:3] in ':=':
+              return color[:3] + fix_color(color[3:])
+            return Color(int(color)).hex_code if color.isdigit() else color
+
+          mainfile = re.sub(
+            r'^(:\s+\''${FAST_HIGHLIGHT_STYLES\[[^]]+\]:=)([^}]+)',
+            lambda m: m.group(1) + fix_color(m.group(2)),
+            open('fast-highlight').read(),
+            flags=re.MULTILINE
+          )
+          open('fast-highlight', 'w').write(mainfile)
+
+          for themefile in glob('themes/*.ini'):
+            parser = RawConfigParser(strict=False)
+            parser.read(themefile)
+
+            for name, section in parser.items():
+              for key, color in section.items():
+                section[key] = ','.join(fix_color(c) for c in color.split(','))
+
+            with open(themefile, 'w') as fp:
+              parser.write(fp)
+        '';
+        postPatch = (drv.postPatch or "") + ''
+          "$fixThemeColors"
+        '';
+      });
+    in mkAfter ''
       export HISTFILE=~/.histfile
       export HISTSIZE=100000
       export SAVEHIST=100000
@@ -113,6 +154,10 @@ in {
       zstyle ':completion:*' verbose true
 
       autoload -Uz zmv
+
+      source ${lib.escapeShellArg "${highlighter}/${
+        "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh"
+      }"}
     '';
 
     programs.zsh.promptInit = ''

commit 18bb985d439b7435d993708685e0536d32aa67e7

modules/zsh: Fix evaluation error and clean up
Regression introduced by 25a077f90a0005b519db071a6b5b4d20bd6d2d45.

I usually let Vim perform a syntax check on the Nix file I'm editing,
which usually displays errors. However, given that the zsh module is one
of my older modules where I did a big "with lib;" over the whole scope,
a nix-instantiate --parse didn't find the missing pkgs argument.

So apart from just fixing the error, I removed the "with lib;" and also
moved from the old lib.overrideDerivation to package.overrideAttrs.

Signed-off-by: aszlig <aszlig@nix.build>

modules/user/aszlig/programs/zsh/default.nix
@@ -8,10 +8,10 @@ let
 
 in {
   options.vuizvui.user.aszlig.programs.zsh = {
-    enable = mkEnableOption "zsh";
+    enable = lib.mkEnableOption "zsh";
 
-    machineColor = mkOption {
-      type = types.enum [
+    machineColor = lib.mkOption {
+      type = lib.types.enum [
         "black" "red" "green" "yellow" "blue" "magenta" "cyan" "white"
       ];
       default = "red";
@@ -22,15 +22,15 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     environment.shellInit = ''
       export EDITOR="vim"
       export EMAIL="aszlig@nix.build"
     '';
 
-    nixpkgs.overlays = singleton (lib.const (super: {
-      zsh = overrideDerivation super.zsh (o: {
-        postConfigure = (o.postConfigure or "") + ''
+    nixpkgs.overlays = lib.singleton (lib.const (super: {
+      zsh = super.zsh.overrideAttrs (drv: {
+        postConfigure = (drv.postConfigure or "") + ''
           sed -i -e '/^name=zsh\/newuser/d' config.modules
         '';
       });
@@ -97,7 +97,7 @@ in {
           "$fixThemeColors"
         '';
       });
-    in mkAfter ''
+    in lib.mkAfter ''
       export HISTFILE=~/.histfile
       export HISTSIZE=100000
       export SAVEHIST=100000