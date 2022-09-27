8ab81a08767eb608c90230c652d059690310e783
sternenseemann <sternenseemann@systemli.org>
2022-09-27 17:52:27 +0200

    machines/sternenseemann/ludwig: workarounds initial i686 failures

--- machines/sternenseemann/ludwig.nix
+++ machines/sternenseemann/ludwig.nix
@@ -11,9 +11,37 @@
   config = {
     hardware.bluetooth.enable = false;
 
-    nixpkgs.localSystem = {
-      system = "i686-linux";
-      config = "i686-unknown-linux-gnu";
+    nixpkgs = {
+      localSystem = {
+        system = "i686-linux";
+        config = "i686-unknown-linux-gnu";
+      };
+
+      overlays = [
+        (self: super: {
+          tlp = super.tlp.overrideAttrs (_: {
+            # Avoid crash in pandoc which is a check dependency of tlp
+            # https://gitlab.haskell.org/ghc/ghc/-/issues/22002
+            doCheck = false;
+          });
+
+          upower = super.upower.overrideAttrs (_: {
+            # https://gitlab.freedesktop.org/upower/upower/-/issues/214
+            doCheck = false;
+          });
+
+          spidermonkey_91 = super.spidermonkey_91.overrideAttrs (old: {
+            # Work around spidermonkey not compiling with newer versions of glibc
+            # https://bugzilla.mozilla.org/show_bug.cgi?id=1729459#
+            patches = old.patches or [] ++ [
+              (pkgs.fetchpatch {
+                url = "https://bug1729459.bmoattachments.org/attachment.cgi?id=9250378";
+                sha256 = "00qzvzd4sjyr142lhfpw8wq2k88llh22xp60w5js821w6q9x3pf1";
+              })
+            ];
+          });
+        })
+      ];
     };
 
     system.stateVersion = "22.05";
