770619fee4e91a74ba0a6ad83c176af2f1a5e9c6
sternenseemann <sternenseemann@systemli.org>
2022-09-28 11:31:28 +0200

    pkgs/tvl: fix eval

--- pkgs/tvl/default.nix
+++ pkgs/tvl/default.nix
@@ -1,7 +1,7 @@
 { tvlSrc ? builtins.fetchGit {
     name = "tvl-depot";
     url = "https://code.tvl.fyi";
-    rev = "b29ef90636cfe0ce21aaa95301105079f4709fed" # 2022-09-28
+    rev = "b29ef90636cfe0ce21aaa95301105079f4709fed"; # 2022-09-28
     ref = "canon";
   }
 , pkgs
