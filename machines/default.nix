5476424196697ad3e9de44aa1ec2dc0ca175f11b
devhell <devhell@mailfresser.de>
2022-12-24 17:03:55 +0000

    machines/default: remove sigrun and hildr
    They are current decomissioned, so no need to waste precious hydra
    resources.

--- machines/default.nix
+++ machines/default.nix
@@ -12,8 +12,6 @@ with import ../lib;
   };
   devhell = {
     eir       = callMachine devhell/eir.nix {};
-    sigrun    = callMachine devhell/sigrun.nix {};
-    hildr     = callMachine devhell/hildr.nix {};
     gunnr     = callMachine devhell/gunnr.nix {};
     herja     = callMachine devhell/herja.nix {};
   };
