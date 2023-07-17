1b23a0862e8edf57799ff792d71fe14c9a4154e6
aszlig <aszlig@nix.build>
2023-07-17 08:09:04 +0200

    games/monogame-patcher: Fix evaluation
    The evaluation error is:
    
      error: function 'anonymous lambda' called without required argument 'pname'
    
    This is because the "baseName" attributes of the dotnet tooling have
    changed to the more common "pname" attributes that we use throughout
    nixpkgs.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/games/build-support/monogame-patcher/default.nix
+++ pkgs/games/build-support/monogame-patcher/default.nix
@@ -1,21 +1,21 @@
 { lib, buildDotnetPackage, fetchNuGet }:
 
 buildDotnetPackage {
-  baseName = "monogame-patcher";
+  pname = "monogame-patcher";
   version = "0.1.0";
 
   src = lib.cleanSource ./src;
 
   buildInputs = [
     (fetchNuGet {
-      baseName = "Mono.Cecil";
+      pname = "Mono.Cecil";
       version = "0.10-beta7";
       sha256 = "1ngjxk3wbmdwgsbdpy9yjwgc0ii8xxa78i0h57dia2rjn0gr7bw0";
       outputFiles = [ "lib/net40/*" ];
     })
 
     (fetchNuGet {
-      baseName = "CommandLineParser";
+      pname = "CommandLineParser";
       version = "2.2.1";
       sha256 = "02zqp98lzjv4rpjf7jl0hvhda41dlh0dc29axaapq9glk0hbmjzg";
       outputFiles = [ "lib/net45/*" ];
