2cf463b899b1622ca0cb747a7a9e140554a0365c
aszlig <aszlig@nix.build>
2024-03-25 13:12:13 +0100

    pkgs/aszlig: Remove refs to axbo and librxtx_java
    I've removed the directory in 061b9452ca81f2e96373973d4e675f40e92fd7dd,
    but failed to remove the actual callPackage references.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- pkgs/aszlig/default.nix
+++ pkgs/aszlig/default.nix
@@ -4,12 +4,10 @@
 
 {
   aacolorize = callPackage ./aacolorize { };
-  axbo = callPackage ./axbo { };
   firefox = callPackage ./firefox { };
   git-detach = callPackage ./git-detach { };
   gopass = callPackage ./gopass { inherit gopass; };
   gpodder = callPackage ./gpodder { inherit gpodder; };
-  librxtx_java = callPackage ./librxtx-java { };
   lockdev = callPackage ./lockdev { };
   mutt = callPackage ./mutt { inherit mutt; };
   nlast = callPackage ./nlast { };
