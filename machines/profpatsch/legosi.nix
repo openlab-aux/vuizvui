b014e36cd50e3f6086493e09b10a818d95e36db8
Profpatsch <mail@profpatsch.de>
2024-07-20 15:17:33 +0200

    machines/profpatsch/legosi: remove extra interfaces

--- machines/profpatsch/legosi.nix
+++ machines/profpatsch/legosi.nix
@@ -67,17 +67,6 @@ in {
         };
       };
 
-      interfaces.ens3 = {
-        # ipv6.addresses = [{
-        #   address = "2a01:4f8:c0c:70aa::1";
-        #   prefixLength = 64;
-        # }];
-      };
-
-      # defaultGateway6 = {
-      #   address = "fe80::1";
-      #   interface = "ens3";
-      # };
     };
 
     services.tailscale = {
