8b17c37828f9242e82ffb5dbeffeed8acf33a812
Profpatsch <mail@profpatsch.de>
2024-07-20 15:10:15 +0200

    machines/profpatsch: get rid of toml experiment
    It was an … idea. But lol.

--- machines/profpatsch/base-server.nix
+++ machines/profpatsch/base-server.nix
@@ -1,50 +1,40 @@
 { config, pkgs, lib, ... }:
 
 let
-  cfgImports = (import ../../pkgs/profpatsch/nixos-toml-modules.nix { inherit lib; }).readAnyToml ./base-server.toml
-    config;
+  cfg = config.vuizvui.user.profpatsch.server;
 
 in
 {
-  inherit (cfgImports) imports;
+  imports = [ ./base.nix ];
 
-  # TODO: cannot read options from pkgs because it would lead to an infinite recursion
-  # in the module system, since the pkgs passed into this module already requires all options.
-  options = ((import ../../pkgs/profpatsch/nixos-toml-modules.nix { inherit lib; }).readAnyToml ./base-server-options.toml).options
-    ;
-
-  config =
-    cfgImports.config
-    # TODO: how to handle a reference to pkgs?
-    // {
-      # This can’t be in base.nix, because the workstations
-      # have gitFull which leads to env collisions.
-      environment.systemPackages = [ pkgs.git ];
-      };
+  options = {
+    vuizvui.user.profpatsch.server.sshPort = lib.mkOption {
+      description = "ssh port";
+      default = 7001;
+      type = lib.types.port;
+    };
+  };
 
 
-  # options.vuizvui.user.profpatsch.server.sshPort = lib.traceValSeqN 3 (lib.mkOption {
-  #   description = "ssh port";
-  #   # TODO: replace with types.intBetween https://github.com/NixOS/nixpkgs/pull/27239
-  #   type = with lib.types; addCheck int (x: x >= 0 && x <= 65535);
-  #   default = 6879;
-  # });
+  config = {
+    programs.mosh.enable = true;
 
-  # config = {
+    services.openssh = {
+      enable = true;
+      listenAddresses = [{
+        addr = "0.0.0.0";
+        port = cfg.sshPort;
+      }];
+    };
 
-  #   programs.mosh.enable = true;
+    networking.enableIPv6 = false;
 
-  #   services.openssh = {
-  #     enable = true;
-  #     listenAddresses = [ { addr = "0.0.0.0"; port = cfg.sshPort; } ];
-  #   };
+    networking.firewall = {
+      enable = true;
+      allowPing = true;
 
-  #   networking.firewall = {
-  #     enable = true;
-  #     allowPing = true;
-  #     allowedTCPPorts = [ cfg.sshPort ];
-  #   };
+      allowedTCPPorts = [ cfg.sshPort ];
+    };
 
-  # };
-
+  };
 }
