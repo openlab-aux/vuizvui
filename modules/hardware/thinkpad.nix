a08d7ba5048ec761740757268348e3a54a6e0165
Profpatsch <mail@profpatsch.de>
2024-10-23 16:00:35 +0200

    modules/hardware/thinkpad: add selection option for CPU type
    Newer thinkpads also come with AMD, in which case we don’t want to
    enable intel-specfic options.

--- modules/hardware/thinkpad.nix
+++ modules/hardware/thinkpad.nix
@@ -6,38 +6,53 @@ in
 {
   options.vuizvui.hardware.thinkpad = {
     enable = lib.mkEnableOption "thinkpad support";
-  };
 
-  config = lib.mkIf cfg.enable {
-
-    # We need to update the Intel microcode on every update,
-    # otherwise there can be problems with newers kernels.
-    hardware.cpu.intel.updateMicrocode = lib.mkDefault true;
-
-    # read acpi stats (e.g. battery)
-    environment.systemPackages = [ pkgs.acpi ];
-
-    # for wifi
-    hardware.enableRedistributableFirmware = lib.mkDefault true;
-
-    hardware.trackpoint = lib.mkDefault {
-      enable = true;
-      emulateWheel = true;
-      speed = 250;
-      sensitivity = 140;
-    };
-
-    # TLP Linux Advanced Power Management
-    services.tlp.enable = lib.mkDefault true;
-    boot = {
-      # acpi_call is required for some tlp features, e.g. discharge/recalibrate
-      kernelModules = [
-        "acpi_call"
-      ];
-
-      extraModulePackages = [
-        config.boot.kernelPackages.acpi_call
-      ];
+    cpuType = lib.mkOption {
+      type = lib.types.enum [ "intel" "amd" ];
+      default = "intel";
+      description = "The CPU type of the ThinkPad";
     };
   };
+
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    (lib.mkIf (cfg.cpuType == "intel") {
+      # We need to update the Intel microcode on every update,
+      # otherwise there can be problems with newer kernels.
+      hardware.cpu.intel.updateMicrocode = lib.mkDefault true;
+
+    })
+    (lib.mkIf (cfg.cpuType == "amd") {
+      # We need to update the AMD microcode on every update,
+      # otherwise there can be problems with newer kernels.
+      hardware.cpu.amd.updateMicrocode = lib.mkDefault true;
+    })
+    {
+      # read acpi stats (e.g. battery)
+      environment.systemPackages = [ pkgs.acpi ];
+
+      # for wifi & cpu microcode (amd)
+      hardware.enableRedistributableFirmware = lib.mkDefault true;
+
+      hardware.trackpoint = lib.mkDefault {
+        enable = true;
+        emulateWheel = true;
+        speed = 250;
+        sensitivity = 140;
+      };
+
+      # TLP Linux Advanced Power Management
+      services.tlp.enable = lib.mkDefault true;
+
+      boot = {
+        # acpi_call is required for some tlp features, e.g. discharge/recalibrate
+        kernelModules = [
+          "acpi_call"
+        ];
+
+        extraModulePackages = [
+          config.boot.kernelPackages.acpi_call
+        ];
+      };
+    }
+  ]);
 }
