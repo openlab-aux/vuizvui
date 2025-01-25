e679681699d5f6e41898aaace59bc99bb15d976c
sternenseemann <sternenseemann@systemli.org>
2025-01-25 13:26:10 +0100

    modules/sternenseemann/desktop: drop numbered workspace shortcuts

--- modules/user/sternenseemann/profiles/desktop.nix
+++ modules/user/sternenseemann/profiles/desktop.nix
@@ -300,25 +300,6 @@ in
          Mod+Shift+WheelScrollDown      { focus-column-right; }
          Mod+Shift+WheelScrollUp        { focus-column-left; }
 
-         Mod+1 { focus-workspace 1; }
-         Mod+2 { focus-workspace 2; }
-         Mod+3 { focus-workspace 3; }
-         Mod+4 { focus-workspace 4; }
-         Mod+5 { focus-workspace 5; }
-         Mod+6 { focus-workspace 6; }
-         Mod+7 { focus-workspace 7; }
-         Mod+8 { focus-workspace 8; }
-         Mod+9 { focus-workspace 9; }
-         Mod+Shift+1 { move-column-to-workspace 1; }
-         Mod+Shift+2 { move-column-to-workspace 2; }
-         Mod+Shift+3 { move-column-to-workspace 3; }
-         Mod+Shift+4 { move-column-to-workspace 4; }
-         Mod+Shift+5 { move-column-to-workspace 5; }
-         Mod+Shift+6 { move-column-to-workspace 6; }
-         Mod+Shift+7 { move-column-to-workspace 7; }
-         Mod+Shift+8 { move-column-to-workspace 8; }
-         Mod+Shift+9 { move-column-to-workspace 9; }
-
          Mod+Mod5+r { consume-window-into-column; } // Mod+) (paredit slurp)
          Mod+Mod5+e { expel-window-from-column; }   // Mod+} (paredit barf)
 
