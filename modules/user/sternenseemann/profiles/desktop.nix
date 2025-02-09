09a3e5ba1f55e96085ba421ef6e8ad0b6cb63c98
sternenseemann <sternenseemann@systemli.org>
2025-02-09 15:05:22 +0100

    modules/sternenseemann/desktop: bump xwayland-satellite to master
    I've done this while debugging a clipboard sync issue I was having which
    turned out to be unrelated to xwayland satellite [1]. However, I may as
    well keep this around: In case anything else is wrong, I'll know I'm
    close to HEAD and can directly report an issue.
    
    [1]: https://code.tvl.fyi/commit/users/sterni/acme?id=ba2d2d3eea278413ef1ec075c27cddbf4729574c

--- modules/user/sternenseemann/profiles/desktop.nix
+++ modules/user/sternenseemann/profiles/desktop.nix
@@ -15,6 +15,24 @@ let
 
   inherit (pkgs) niri;
 
+  xwayland-satellite = pkgs.xwayland-satellite.overrideAttrs (oldAttrs: rec {
+    version = "0.5-unstable-2025-01-25";
+    src = lib.warnIf
+      (lib.versionOlder "0.5" oldAttrs.version)
+      "modules/user/sternenseemann/profiles/desktop: consider dropping xwayland-satellite override"
+      pkgs.fetchFromGitHub {
+        owner = "Supreeeme";
+        repo = "xwayland-satellite";
+        sha256 = "0pzwmzwirlsxkcgyy3lk8b91kjz77sbp4483cwwapf89zz02i0y0";
+        rev = "3944c9a0e40e5629f16ad023bbc90dac80d35a0f";
+      };
+    cargoDeps = oldAttrs.cargoDeps.overrideAttrs (_: {
+      name = "xwayland-satellite-${version}-vendor.tar.gz";
+      outputHash = "sha256:0nnkb0299w3kzfnjimb4blisgzs74vivj11yrjkpzi6j7prvyfrp";
+      inherit src;
+    });
+  });
+
   gammastep = pkgs.gammastep.override {
     withRandr = false;
     withGeolocation = false;
@@ -37,7 +55,7 @@ let
       // (getBins pkgs.niri [ "niri" ])
       // (getBins pkgs.jq [ "jq" ])
       // (getBins pkgs.systemd [ "systemctl" ])
-      // (getBins pkgs.xwayland-satellite [ "xwayland-satellite" ])
+      // (getBins xwayland-satellite [ "xwayland-satellite" ])
       // (getBins gammastep [ "gammastep" ])
       // {
         niri-focus-any-window = pkgs.writeShellScript "niri-focus-any-window" ''
