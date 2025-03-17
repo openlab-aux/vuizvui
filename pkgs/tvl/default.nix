16f7573bfb76dac96ddf78853c2d9f9fcbcf6e32
sternenseemann <sternenseemann@systemli.org>
2025-03-17 12:33:30 +0100

    pkgs/tvl: 2025-02-09 -> 2025-03-16
    Should cope with Emacs < 30 being marked as insecure

--- pkgs/tvl/default.nix
+++ pkgs/tvl/default.nix
@@ -1,7 +1,7 @@
 { tvlSrc ? builtins.fetchGit {
     name = "tvl-depot";
     url = "https://code.tvl.fyi";
-    rev = "5f17df85487cb4536c49c3dab8c17b873eeb6dd9"; # 2025-02-09
+    rev = "5f17df85487cb4536c49c3dab8c17b873eeb6dd9"; # 2025-03-16
     ref = "canon";
   }
 , pkgs
