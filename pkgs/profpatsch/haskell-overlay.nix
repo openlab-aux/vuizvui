02c2cb948b5a5063f4fb4f7bfdb890cb9db44622
Profpatsch <mail@profpatsch.de>
2025-10-15 19:05:57 +0200

    pkgs/profpatsch: fix purenix to compile with purs 0.15.15

--- pkgs/profpatsch/haskell-overlay.nix
+++ pkgs/profpatsch/haskell-overlay.nix
@@ -7,16 +7,17 @@ in pkgs.haskellPackages.override {
   overrides = self: super: {
 
     # https://github.com/NixOS/nixpkgs/pull/328896
-    purescript = pkgs.lib.pipe super.purescript [
-      (hlib.appendPatches [./purescript-import-fix.patch ])
-      hlib.unmarkBroken
-    ];
+    #purescript = pkgs.lib.pipe super.purescript [
+    #  #(hlib.appendPatches [./purescript-import-fix.patch ])
+    #  hlib.unmarkBroken
+    #];
     purenix = pkgs.lib.pipe super.purenix [
-      (hlib.appendPatches [
-        ./purenix-import-fix.patch
-        ./purenix-purescript-0_15_12.patch
-      ])
-      hlib.unmarkBroken
+	    (hlib.overrideCabal (drv: {
+	      patches = [
+            # ./purenix-import-fix.patch
+            ./0001-chore-adjust-to-purescript-0.15.15-corefn-changes.patch
+          ];
+	    }))
     ];
   };
 }
