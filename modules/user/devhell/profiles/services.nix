5cb7bcd5348a61b99d9ec5d6f018a42498cffc24
devhell <devhell@mailfresser.de>
2025-10-26 01:12:40 +0000

    profiles/services: reflect tuigreet change

--- modules/user/devhell/profiles/services.nix
+++ modules/user/devhell/profiles/services.nix
@@ -31,7 +31,7 @@ in {
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd niri-session";
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd niri-session";
           user = "greeter";
           vt = 2;
         };
