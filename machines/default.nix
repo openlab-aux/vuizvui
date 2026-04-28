openlab-aux/vuizvui

commit 4d74ae8863083576c4b498dbda49184a44725981

chore(users/Profpatsch): ✨ remove profpatsch machines and packages
Machine configs for rolery, haku, legosi, and leguin have been migrated
to the Profpatsch home-repo (kot/Profpatsch). All profpatsch-specific
NixOS modules, packages, and machine configurations are now maintained
there and built via nixosConfigurations in its flake.nix.

Removed from vuizvui:
- machines/profpatsch/ (all four machines + base configs)
- modules/user/profpatsch/ (all NixOS modules)
- pkgs/profpatsch: backlight, blight, deploy, dhallsh, display-infos,
  git-commit-index, gpg-private-offline-key, home-repo, home-repo-src,
  jaeger, micro, nix-http-serve, query-album-streams, sfttime,
  show-qr-code, special-packages, television, text-letter, utils-hs,
  youtube2audiopodcast, and related patches

Retained in vuizvui: nman, utils, execline, s6, netencode, and other
packages still used by sternenseemann and shared infrastructure.

🤖 Generated with [Claude Code](https://claude.ai/code)

Co-Authored-By: Claude <noreply@anthropic.com>

machines/default.nix
@@ -1,28 +1,5 @@
 with import ../lib;
 
-let
-  # Module selection for profpatsch machines (nixpkgs stable compatibility)
-  profpatschModules = {
-    _moduleSelection = [
-      # Core modules (needed for vuizvui basics)
-      "./core/common.nix"
-      "./core/licensing.nix"
-      "./core/tests.nix"
-
-      "./hardware/thinkpad.nix"
-      "./hardware/t14s.nix"
-      "./hardware/tolino.nix"
-
-      "./programs/gnupg"
-      "./programs/fish/fasd.nix"
-
-      "./services/upower-minimal.nix"
-
-      # All profpatsch user modules
-      "./user/profpatsch/"
-    ];
-  };
-
 in
 {
   aszlig = {
@@ -34,12 +11,6 @@ in
     eir       = callMachine devhell/eir.nix {};
     gunnr     = callMachine devhell/gunnr.nix {};
     herja     = callMachine devhell/herja.nix {};
-  };
-  profpatsch = {
-    rolery = callMachine ./profpatsch/rolery.nix profpatschModules;
-    haku = callMachine ./profpatsch/haku.nix profpatschModules;
-    legosi = callMachine ./profpatsch/legosi.nix profpatschModules;
-    leguin = callMachine ./profpatsch/leguin.nix profpatschModules;
   };
   sternenseemann = {
     wolfgang = callMachine ./sternenseemann/wolfgang.nix {};

pkgs/profpatsch/backlight/default.nix
@@ -1,17 +1,0 @@
-{ stdenv, pkgs, ...}:
-
-stdenv.mkDerivation rec {
-  name = "backlight";
-
-  src = ./backlight.py;
-  phases = [ "installPhase" "fixupPhase" ];
-
-  buildInputs = [ pkgs.python3 ];
-
-  installPhase = ''
-    install -D ${src} $out/bin/backlight
-    substituteInPlace $out/bin/backlight \
-      --replace '"xbacklight"' '"${pkgs.acpilight}/bin/xbacklight"'
-  '';
-
-}

pkgs/profpatsch/default.nix
@@ -31,17 +31,13 @@ let
 
   writeExeclineFns = callPackage ./execline/write-execline.nix {};
 
-  homeRepoSrc = import ./home-repo-src.nix;
-
-  homeRepo = import ./home-repo.nix { inherit pkgs homeRepoSrc; };
-
   writeRust = import ./write-rust.nix {
     inherit pkgs getBins; inherit (runExeclineFns) runExeclineLocal; inherit (nixperiments) drvSeqL;
   };
 
   # All utility functions and libraries that packages might need
   utilityArgs = {
-    inherit stdenv lib pkgs sternenseemann lazy-packages homeRepo;
+    inherit stdenv lib pkgs sternenseemann lazy-packages;
     inherit exactSource;
     inherit (nixperiments) match script drvSeq drvSeqL withTests filterSourceGitignoreWith readGitignoreFile;
     inherit (runExeclineFns) runExecline runExeclineLocal runExeclineLocalNoSeqL;
@@ -60,39 +56,9 @@ in readTree.fix (self: let
     };
   };
 
-  # Additional packages that need special handling (not auto-discovered)
-  specialPackages = rec {
-    # Re-exports from homeRepo
-    inherit (homeRepo.users.Profpatsch) lyric alacritty;
-
-    # .nix files that aren't auto-discovered because default.nix exists in root
-    rust-deps = import ./rust-deps.nix { inherit (pkgs) buildRustCrate; };
-
-    # Standalone package files (import with utilityArgs + pkgs + discovered packages)
-    standaloneArgs = utilityArgs // pkgs // {
-      runblock = discovered.execline.runblock;
-      profpatsch = self;
-    };
-    deploy = (import ./deploy.nix standaloneArgs).deploy;
-    blight = import ./blight.nix standaloneArgs;
-
-    # Re-exports from homeRepo (migrated from vuizvui)
-    inherit (homeRepo.users.Profpatsch) read-qr-code read-qr-code-from-camera e sandbox;
-    xdg-open = homeRepo.users.Profpatsch.xdg-open;
-    xrandr = homeRepo.users.Profpatsch.xrandr;
-    nix-run = homeRepo.users.Profpatsch.nix-tools.nix-run;
-    nix-run-bin = homeRepo.users.Profpatsch.nix-tools.nix-run-bin;
-    nix-eval = homeRepo.users.Profpatsch.nix-tools.nix-eval;
-
-    # Packages that need complex setup
-    droopy = import ./special-packages/droopy.nix { inherit pkgs; };
-    gitit = import ./special-packages/gitit.nix { inherit pkgs; };
-
-  };
-
-in discovered // specialPackages // {
-  # Re-export for backward compatibility (used by machine configs and other packages)
-  inherit homeRepo getBins;
+in discovered // {
+  # Re-export for use by other packages
+  inherit getBins;
   inherit (writeExeclineFns) writeExecline writeExeclineBin;
   inherit (writeRust) writeRustSimple writeRustSimpleBin writeRustSimpleLib;
 })

pkgs/profpatsch/display-infos/default.nix
@@ -1,85 +1,0 @@
-{ lib, pkgs, profpatsch, ... }:
-
-let
-  name = "display-infos-0.1.0";
-  script = pkgs.writeText (name + "-script") ''
-    #!@python3@
-
-    import sys
-    import glob
-    import subprocess as sub
-    import os.path as path
-    import statistics as st
-    import jc
-
-    def readint(fn):
-        with open(fn, 'r') as f:
-            return int(f.read())
-
-    def seconds_to_sft(secs):
-        p = sub.Popen(["@bc@", "-l"], stdin=sub.PIPE, stdout=sub.PIPE)
-        (sft, _) = p.communicate(input="scale=2; obase=16; {} / 86400\n".format(secs).encode())
-        p.terminate()
-        return str(sft.strip().decode())
-
-    def get_5_min_load():
-        with open('/proc/loadavg', 'r') as f:
-            return f.read().split(' ')[1]
-
-    charging = readint("/sys/class/power_supply/AC/online")
-
-    full = 0
-    now  = 0
-    # this is "to charged" if charging and "to empty" if not
-    seconds_remaining = 0
-    for bat in glob.iglob("/sys/class/power_supply/BAT*"):
-
-        # these files might be different for different ACPI/battery providers
-        # see the full list in acpi.c of the acpi(1) tool
-        # unit: who knows
-        full += readint(path.join(bat, "energy_full"))
-        now  += readint(path.join(bat, "energy_now" ))
-        # in unit?/hours, hopefully the same unit as above
-        # ACPI is a garbage fire
-        current_rate = readint(path.join(bat, "power_now"))
-
-        if current_rate == 0:
-          continue
-        elif charging:
-          seconds_remaining += 3600 * (full - now) / current_rate
-        else:
-          seconds_remaining += 3600 * now / current_rate
-
-    bat = round( now/full, 2 )
-    ac = "⚡ " if charging else ""
-    sft_remaining = seconds_to_sft(seconds_remaining)
-    date = sub.run(["date", "+%d.%m. KW%V %a %T"], stdout=sub.PIPE).stdout.strip().decode()
-    dottime = sub.run(["date", "--utc", "+%H·%M"], stdout=sub.PIPE).stdout.strip().decode()
-    sftdate = sub.run(["@sfttime@"], stdout=sub.PIPE).stdout.strip().decode()
-    load = get_5_min_load()
-    free_mem_gibi = jc.parse('free', sub.check_output(['free', '--gibi'], text=True))[0]['available']
-    notify = "BAT: {percent}% {ac}{charge}{{{load}, {free_mem_gibi}G}} | {date} | {sftdate} | {dottime}".format(
-      percent = int(bat*100),
-      ac = ac,
-      charge = "{} ".format(sft_remaining) if seconds_remaining else "",
-      load = load,
-      free_mem_gibi = free_mem_gibi,
-      date = date,
-      sftdate = sftdate,
-      dottime = dottime
-    )
-    print(notify)
-  '';
-
-  python = pkgs.python3.withPackages (pp: [ pp.jc ]);
-
-in
-  with lib; pkgs.runCommandLocal "display-infos" {
-    meta.description = "Script to display time & battery";
-  } ''
-    substitute ${script} script \
-      --replace "@python3@" "${getBin python}/bin/python3" \
-      --replace "@bc@" "${getBin pkgs.bc}/bin/bc" \
-      --replace "@sfttime@" "${getBin profpatsch.sfttime}/bin/sfttime"
-    install -D script $out/bin/display-infos
-  ''

pkgs/profpatsch/git-commit-index/default.nix
@@ -1,69 +1,0 @@
-{ cdb, git, writeShellScriptBin, symlinkJoin, lib, script, runCommandLocal, ... }:
-
-let
-  pathPlus = ''PATH="$PATH:${lib.makeBinPath [cdb git]}"'';
-
-  mkIndex = script.withOptions {
-    name = "git-commit-index-create";
-    synopsis = "Create a git commit index for all .git directories, recursively.";
-
-    options = {
-      index = {
-        description = "Location of the index. Must not exist.";
-        checks = [ script.optionChecks.emptyPath ];
-      };
-    };
-
-    extraArgs = {
-      description = "List of root directories to recursively search";
-      name = "DIRS";
-      checks = [ script.argsChecks.allAreDirs ];
-    };
-
-    script = ''
-      ${pathPlus}
-      source ${./lib.sh}
-      mkdir "$index"
-      for root in "$@"; do
-        for repo in $(findRepos "$root"); do
-          genIndex "$index" "$repo"
-        done
-      done
-    '';
-  };
-
-  queryIndex = script.withOptions {
-    name = "git-commit-index-query";
-
-    synopsis = "Search a git commit index for a rev, return the repository that rev is in.";
-
-    options = {
-      index = {
-        description = "Location of the populated index.";
-        checks = [ script.optionChecks.isDir ];
-      };
-      rev = {
-        description = "Full git rev hash to look up";
-        # TODO: check that it is a commit hash?
-        checks = [];
-      };
-    };
-
-    script = ''
-      ${pathPlus}
-      source ${./lib.sh}
-
-      query "$index" "$rev"
-    '';
-  };
-
-  # magitOpenCommit = writeShellScriptBin "git-commit-index-magit-open" ''
-  #   ${pathPlus}
-# emacsclient -e '(let ((default-directory "/home/philip/kot/work/tweag/lorri"))
-#                                    (magit-mode-setup #\'magit-revision-mode "43a72bc220dae8f34bd0d889f2b1b1ce2a6092b7" nil nil nil))'
-
-in
-  runCommandLocal "git-commit-index" {} ''
-    install -D ${mkIndex} $out/bin/git-commit-index-create
-    install -D ${queryIndex} $out/bin/git-commit-index-query
-  ''

pkgs/profpatsch/gpg-private-offline-key/default.nix
@@ -1,58 +1,0 @@
-{ pkgs, writeExecline, profpatsch, ... }:
-
-let
-# split
-
-  inherit (profpatsch.utils) getBins;
-  bins = getBins pkgs.coreutils [ "split" "mktemp" "rm" "rmdir" ]
-      // getBins pkgs.lr [ "lr" ]
-      // getBins pkgs.xe [ "xe" ]
-      // getBins pkgs.qrencode [ "qrencode" ]
-      // getBins pkgs.zbar [ "zbarimg" ]
-      // getBins pkgs.sane-backends [ "scanimg" ];
-
-  qr-code-props = {
-    # second highest redundancy level
-    level = "Q";
-    # max amount of bytes that level Q can encode
-    bytes = "1700";
-  };
-
-  # Takes a private GPG key encoded with paperkey on stdin
-  # and execs into the argv for each qr code image (on stdin).
-  print-qr-codes = writeExecline "print-qr-codes" {} [
-    split-stdin
-    "pipeline" [
-      bins.qrencode
-      "--level=${qr-code-props.level}"
-      "--dpi=300"
-      "-o-"
-    ]
-    "$@"
-  ];
-
-  split-stdin = pkgs.writers.writePython3 "split-stdin" {} ''
-    import sys
-    import subprocess
-
-    while True:
-        str = sys.stdin.buffer.read(${qr-code-props.bytes})
-        if str == "":
-            break
-        ret = subprocess.run(sys.argv[1:], input=str)
-        code = ret.returncode
-        if code != 0:
-            sys.exit(code if code > 0 else 128 - code)
-  '';
-
-  test = writeExecline "test12" {} [
-    "foreground" [ print-qr-codes "feh" "-" ]
-  ];
-
-  
-# scanning:
-# for i in (seq 0 9); scanimage --device=\'fujitsu:ScanSnap iX500:1527308\' --mode=gray --resolution=100 --format=png | zbarimg -Sdisable -Sqrcode.enable --raw - | head --bytes=-1; end > out
-
-in {
-  inherit print-qr-codes;
-}

pkgs/profpatsch/jaeger/default.nix
@@ -1,18 +1,0 @@
-{ pkgs, ... }:
-let
-  drv =
-    pkgs.stdenv.mkDerivation {
-      pname = "jaeger";
-      version = "1.49.0";
-      src = pkgs.fetchurl {
-        url = "https://github.com/jaegertracing/jaeger/releases/download/v1.49.0/jaeger-1.49.0-linux-amd64.tar.gz";
-        hash = "sha256-QhxISDlk/t431EesgVkHWTe7yiw2B+yyfq//GLP0As4=";
-      };
-      phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
-      installPhase = ''
-        mkdir -p $out/bin
-        install ./jaeger-all-in-one $out/bin
-      '';
-    };
-in
-drv

pkgs/profpatsch/micro/default.nix
@@ -1,96 +1,0 @@
-# COPIED from nixpkgs for https://github.com/zyedidia/micro/pull/3868
-{
-  lib,
-  pkgs,
-  # Deprecated options
-  # Remove them as soon as possible
-  withXclip ? null,
-  withWlClipboard ? null,
-  withWlclip ? null,
-  ...
-}:
-
-let
-  self = pkgs.buildGoModule {
-    pname = "micro";
-    version = "2.0.15";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "zyedidia";
-      repo = "micro";
-      rev = "v2.0.15";
-      hash = "sha256-4C6TtMU6PIYX7lO+o4GRVnIsKnYJxjAqPdoOyAwi7Gc=";
-    };
-
-    vendorHash = "sha256-Pn3C/Te47qXeqgB9UPynB8aPtnwnjrqq/ZE9bA7JRVs=";
-
-    nativeBuildInputs = [ pkgs.installShellFiles ];
-
-    outputs = [
-      "out"
-      "man"
-    ];
-
-    subPackages = [ "cmd/micro" ];
-
-    ldflags =
-      let
-        t = "github.com/zyedidia/micro/v2/internal";
-      in
-      [
-        "-s"
-        "-w"
-        "-X ${t}/util.Version=${self.version}"
-        "-X ${t}/util.CommitHash=${self.src.rev}"
-      ];
-
-    strictDeps = true;
-
-    preBuild = ''
-      GOOS= GOARCH= go generate ./runtime
-    '';
-
-    postInstall = ''
-      installManPage assets/packaging/micro.1
-      install -Dm444 assets/packaging/micro.desktop $out/share/applications/micro.desktop
-      install -Dm644 assets/micro-logo-mark.svg $out/share/icons/hicolor/scalable/apps/micro.svg
-    '';
-
-    passthru = {
-      tests = lib.packagesFromDirectoryRecursive {
-        callPackage = pkgs.callPackage;
-        directory = ./tests;
-      };
-      wrapper = pkgs.callPackage ./wrapper.nix { micro = self; };
-    };
-
-    meta = {
-      homepage = "https://micro-editor.github.io";
-      changelog = "https://github.com/zyedidia/micro/releases/";
-      description = "Modern and intuitive terminal-based text editor";
-      longDescription = ''
-        micro is a terminal-based text editor that aims to be easy to use and
-        intuitive, while also taking advantage of the capabilities of modern
-        terminals.
-
-        As its name indicates, micro aims to be somewhat of a successor to the
-        nano editor by being easy to install and use. It strives to be enjoyable
-        as a full-time editor for people who prefer to work in a terminal, or
-        those who regularly edit files over SSH.
-      '';
-      license = lib.licenses.mit;
-      mainProgram = "micro";
-      maintainers = with lib.maintainers; [
-        pbsds
-      ];
-    };
-  };
-in
-lib.warnIf (withXclip != null || withWlClipboard != null || withWlclip != null) ''
-  The options `withXclip`, `withWlClipboard`, `withWlclip` were removed. If
-  you are seeking for clipboard support, please consider the following
-  packages:
-  - `micro-with-wl-clipboard`
-  - `micro-with-xclip`
-  - `micro-full`
-'' self

commit cc8ccc7372b0facb86b3ed112702465d8650faca

machines: Fix syntax error
Introduced by 4d74ae8863083576c4b498dbda49184a44725981, which forgot to
remove the "in".

Signed-off-by: aszlig <aszlig@nix.build>

machines/default.nix
@@ -1,6 +1,5 @@
 with import ../lib;
 
-in
 {
   aszlig = {
     dnyarri   = callMachine ./aszlig/dnyarri.nix {};