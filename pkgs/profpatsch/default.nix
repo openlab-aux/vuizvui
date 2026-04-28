cb90cfe5fe0adb8db7bd167a68ce58723926b523
sternenseemann <sternenseemann@systemli.org>
2026-04-28 22:48:11 +0200

    pkgs/profpatsch: vendor drvSeqL nixperiments
    This reintroduces (an up to date version of) the testing library
    which was previously _not_ used for drvSeqL and thus briefly
    removed in d41a04f329d1b6de48e3703b27660e586eed4637.

--- pkgs/profpatsch/default.nix
+++ pkgs/profpatsch/default.nix
@@ -1,19 +1,12 @@
 { stdenv, lib, pkgs, sternenseemann, lazy-packages }:
 
 let
-  inherit (pkgs) callPackage;
-
   getBins = import ./getBins.nix { inherit lib; };
 
-  # various nix utils and fun experiments
-  nixperiments = import (pkgs.fetchFromGitHub {
-    owner = "Profpatsch"; repo = "nixperiments";
-    rev = "e04abced1a4fef7ad63f67ec621f6484226ed104";
-    sha256 = "1if9szjj1g1l5n31pmbiyp5f3llwl6xlqxklc6g2xf4rq59d2d6w";
-  }) { nixpkgs = pkgs; };
+  testing = import ./testing { inherit (pkgs) runCommand; inherit lib; };
 
   writeRust = import ./write-rust.nix {
-    inherit pkgs; inherit (nixperiments) drvSeqL;
+    inherit pkgs; inherit (testing) drvSeqL;
   };
 
   nman = import ./nman {
